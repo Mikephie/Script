dc02879e050c9116246b427000ce429145175f83
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-08-19 11:45:23 +0800

    Update mike_gd6.js

--- mike_gd6.js
+++ mike_gd6.js
@@ -2,15 +2,15 @@
 
 项目名称：Goodnotes6
 下载地址：https://t.cn/A6K6ZSZS
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
+脚本作者：mikephie
+电报频道：https://t.me/mikephie
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
 
 [rewrite_local]
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/goodnotes6.js
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/goodnotes6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/mike_gd6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/mike_gd6.js
 
 [mitm]
 hostname = isi.csan.goodnotes.com
@@ -18,8 +18,8 @@ hostname = isi.csan.goodnotes.com
 *************************************/
 
 
-const chxm1023 = {};
-const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const mikephie = {};
+const mikephie76 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
 const namea = "apple_access";
 const nameb = "crossplatform_access";
@@ -29,28 +29,28 @@ const jsid = "com.goodnotes.gn6_one_time_unlock_3999";
 if (typeof $response == "undefined") {
   delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
-  chxm1023.headers = $request.headers;
-} else if (chxm1024 && chxm1024.subscriber) {
+  mikephie.headers = $request.headers;
+} else if (mikephie76 && mikephie76.subscriber) {
   data = {
- "Author": "chxm1023",
- "Telegram" : "https://t.me/chxm1023",
+ "Author": "mikephie",
+ "Telegram" : "https://t.me/mikephie",
  "warning": "仅供学习，禁止转载或售卖",
  "purchase_date": "2022-09-09T09:09:09Z"
  };
-  chxm1024.subscriber.subscriptions[(jsid)] = {
- "Author": "chxm1023",
- "Telegram" : "https://t.me/chxm1023",
+  mikephie76.subscriber.subscriptions[(jsid)] = {
+ "Author": "mikephie",
+ "Telegram" : "https://t.me/mikephie",
  "warning": "仅供学习，禁止转载或售卖",
  "original_purchase_date": "2022-09-09T09:09:09Z",
  "purchase_date": "2022-09-09T09:09:09Z",
  "store" : "app_store",
  "ownership_type": "PURCHASED"
  };
-  chxm1024.subscriber.entitlements[(namea)] = JSON.parse(JSON.stringify(data));
-  chxm1024.subscriber.entitlements[(nameb)] = JSON.parse(JSON.stringify(data));
-  chxm1024.subscriber.entitlements[(namea)].product_identifier = (jsid);
-    chxm1024.subscriber.entitlements[(nameb)].product_identifier = (jsid);
-  chxm1023.body = JSON.stringify(chxm1024);
+  mikephie76.subscriber.entitlements[(namea)] = JSON.parse(JSON.stringify(data));
+  mikephie76.subscriber.entitlements[(nameb)] = JSON.parse(JSON.stringify(data));
+  mikephie76.subscriber.entitlements[(namea)].product_identifier = (jsid);
+    mikephie76.subscriber.entitlements[(nameb)].product_identifier = (jsid);
+  mikephie.body = JSON.stringify(mikephie76);
 }
 
-$done(chxm1023);
+$done(mikephie);
