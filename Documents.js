4e67a04d7f3dd13f67e7f26e1a92117e4769740f
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-08-20 11:02:02 +0800

    Update Documents.js

--- Documents.js
+++ Documents.js
@@ -8,7 +8,7 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/documents\/subscription\/refresh url script-request-body Documents.js
+^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/documents\/subscription\/refresh url script-request-body https://raw.githubusercontent.com/Mikephie/Script/main/Documents.js
 
 [mitm]
 hostname = license.pdfexpert.com
