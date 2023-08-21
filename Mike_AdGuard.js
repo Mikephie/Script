1f493c25c708fb339438b30571bf7fd5a25c90c2
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-08-21 21:59:56 +0800

    Update Mike_AdGuard.js

--- Mike_AdGuard.js
+++ Mike_AdGuard.js
@@ -2,14 +2,14 @@
 
 项目名称：AdGuard-Safari浏览器扩展
 下载地址：https://t.cn/A6xe1oaK
-脚本作者：chxm1023
+脚本作者：mikephie
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
 
 [rewrite_local]
 
-^https:\/\/mobile-api\.adguard\.org\/api\/.+\/ios_validate_receipt\/(.*?) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/AdGuard.js
+^https:\/\/mobile-api\.adguard\.org\/api\/.+\/ios_validate_receipt\/(.*?) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/Mike_AdGuard.js
 
 [mitm]
 hostname = mobile-api.adguard.org
@@ -17,8 +17,8 @@ hostname = mobile-api.adguard.org
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
+var mikephie = JSON.parse($response.body);
 
-chxm1023 = {"products":[{"premium_status":"ACTIVE","product_id":"com.adguard.lifetimePurchase"}]}
+mikephie = {"products":[{"premium_status":"ACTIVE","product_id":"com.adguard.lifetimePurchase"}]}
 
-$done({body : JSON.stringify(chxm1023)});
+$done({body : JSON.stringify(mikephie)});
