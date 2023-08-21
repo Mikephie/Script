7c42c21ad8662dac235684f7e8f163b3bcbe61c5
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-08-21 22:01:34 +0800

    Update Mike_gd6.js

--- Mike_gd6.js
+++ Mike_gd6.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/mike_gd6.js
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/mike_gd6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/Mike_gd6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/Mike_gd6.js
 
 [mitm]
 hostname = isi.csan.goodnotes.com
