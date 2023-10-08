f679f1eeaae3778c518e26c57783cd97a21ed5ae
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-10-08 18:46:24 +0800

    更新 cycc.js

--- cycc.js
+++ cycc.js
@@ -1,9 +1,9 @@
 /******************************
 
-脚本名称: cycc.js
+脚本名称:   cycc
 下载地址：商店
 脚本作者：Mikephie
-更新时间：2023年10月7日 16:25
+更新时间：2023年10月8日 18:40
 问题反馈：TG
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
@@ -11,55 +11,38 @@
 
 [rewrite_local]
 
-^https:\/\/ea\.blademaster\.club\/api\/log url script-response-body cycc.js
-
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/cycc.js
 [mitm] 
 
-hostname = ea.blademaster.club
+hostname = buy.itunes.apple.com
 
 *******************************/
 
 var mikephie = JSON.parse($response.body);
 
-    objc = {
-  "behavior" : {
-    "coin_get" : 144,
-    "ad_reward_energy_times" : 0,
-    "coin_consume" : 0,
-    "int_ad_suc" : 0,
-    "int_ad_fail" : 0,
-    "coin_random_hint" : 0,
-    "challenge_resurrection_times" : 0,
-    "ad_reward_coin_times" : 0,
-    "coin_specify_hint" : 0,
-    "energy_consume" : 2,
-    "no_ad_consume" : 0,
-    "duration" : 81,
-    "no_ad_get" : 0,
-    "energy_get" : 2,
-    "rv_ad_fail" : 0,
-    "rv_ad_suc" : 0
+    mikephie = {
+  "receipt": {
+    "receipt_type": "Production",
+    "adam_id": 1618185567,
+    "app_item_id": 1618185567,
+    "bundle_id": "com.agedstudio.word.puzzle.chengyucaicai",
+    "application_version": "1.0",
+    "download_id": 502845682460838640,
+    "version_external_identifier": 859171021,
+    "receipt_creation_date": "2023-10-07 08:10:02 Etc/GMT",
+    "receipt_creation_date_ms": "1696666202000",
+    "receipt_creation_date_pst": "2023-10-07 01:10:02 America/Los_Angeles",
+    "request_date": "2023-10-08 10:24:21 Etc/GMT",
+    "request_date_ms": "1696760661478",
+    "request_date_pst": "2023-10-08 03:24:21 America/Los_Angeles",
+    "original_purchase_date": "2023-10-07 08:09:54 Etc/GMT",
+    "original_purchase_date_ms": "1696666194000",
+    "original_purchase_date_pst": "2023-10-07 01:09:54 America/Los_Angeles",
+    "original_application_version": "1.0",
+    "in_app": [3]
   },
-  "user" : {
-    "id" : "1696666273297839",
-    "platform" : "app",
-    "model" : "iPhone16,2",
-    "operating_system" : "ios 17.0.3",
-    "all_duration" : 81,
-    "brand" : "apple",
-    "version" : "1.0.027"
-  },
-  "snapshot" : {
-    "energy" : 99925,
-    "no_ad" : 1,
-    "challenge" : "000000",
-    "social_id" : "",
-    "coin" : 999344,
-    "name" : "",
-    "stage" : 3
-  }
+  "environment": "Production",
+  "status": 0
 }
 
-
-$done({body : JSON.stringify(mikephie)});
-
+$done({ body: JSON.stringify(mikephie) });
