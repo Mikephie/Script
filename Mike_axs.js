f4168f258c925634233e5370fb29b1fbbb461b74
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-10-11 13:17:28 +0800

    Update Mike_axs.js

--- Mike_axs.js
+++ Mike_axs.js
@@ -13,31 +13,6 @@
 ^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/InfoPage url reject dict
 ^https:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/highlight url reject-200
 ^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/highlight url reject-200
-^https:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/InfoPage url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/Mike_axs.js
-^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/InfoPage url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/Mike_axs.js
 
 [mitm]
 hostname = m-station.axs.com.sg, m-station2.axs.com.sg
-
-*************************************/
-
-
-var body = $response.body;
-var Mikephie= JSON.parse(body);
-
-function recursiveRemoval(Mikephie) {
-    for (let key in Mikephie) {
-        if (typeof(Mikephie[key]) === 'object') {
-            if ('layout' in Mikephie[key] && Mikephie[key]['layout'] === 'advert_self') {
-                delete Mikephie[key];
-            } else {
-                recursiveRemoval(Mikephie[key]);
-            }
-        }
-    }
-}
-
-recursiveRemoval(Mikephie);
-
-body = JSON.stringify(Mikephie);
-$done({body});
