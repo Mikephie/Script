a5536914a213cda91d1167a80e79f9f4d9f66575
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-10-11 13:45:12 +0800

    Update Mike_axs.js

--- Mike_axs.js
+++ Mike_axs.js
@@ -1,20 +1,20 @@
-/*************************************
+
 
 项目名称：AXS-去广告
-下载地址：
+下载地址：https://raw.githubusercontent.com/Mikephie/Script/main/Mike_axs.js
 脚本作者：Mikephie
 电报频道：
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
-**************************************
+
 
 [rewrite_local]
+
 ^https:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/InfoPage url reject dict
 ^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/InfoPage url reject dict
 ^https:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/highlight url reject-200
-^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/highlight url reject-200 url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/Mike_axs.js
+^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/highlight url reject-200
 
 [mitm]
-hostname = m-station.axs.com.sg, m-station2.axs.com.sg
 
-*************************************/
+hostname = m-station.axs.com.sg, m-station2.axs.com.sg
