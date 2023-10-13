2d9c6d8f73bcaebc0675b7dedae43595ff7190d3
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-10-13 11:04:30 +0800

    更新 Mike_aiyufa.js

--- Mike_aiyufa.js
+++ Mike_aiyufa.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/revenuecat-cn\.fastools\.cn\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/Mike_aiyufa.js
-^https?:\/\/revenuecat-cn\.fastools\.cn\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/Mike_aiyufa.js 
+
+^https?:\/\/revenuecat-cn\.fastools\.cn\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/Mike_aiyufa.js 
 
 [mitm]
 hostname = revenuecat-cn.fastools.cn
@@ -18,27 +18,31 @@ hostname = revenuecat-cn.fastools.cn
 *************************************/
 
 
-const mikephie = {};
-const mikephie76 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+var mikephie = JSON.parse($response.body);
 
-const name = "pro";
-const appid = "igrammar_199_1y_v1";
+    objc = {
+  "request_date_ms" : 1697112590937,
+  "request_date" : "2023-10-12T12:09:50Z",
+  "subscriber" : {
+    "non_subscriptions" : {
 
-if (typeof $response == "undefined") {
-  delete $request.headers["x-revenuecat-etag"];
-  delete $request.headers["X-RevenueCat-ETag"];
-  mikephie.headers = $request.headers;
-} else if (mikephie76 && mikephie76.subscriber) {
-  mikephie76.subscriber.subscriptions = mikephie76.subscriber.subscriptions || {};
-  mikephie76.subscriber.entitlements = mikephie76.subscriber.entitlements || {};
-  const data = {
-	"product_identifier": (appid),
-	"expires_date": "2088-08-08T08:08:08Z",
-	"purchase_date": "2023-08-08T08:08:08Z"
-	};
-  mikephie76.subscriber.entitlements[(name)] = (data);
-  mikephie76.subscriber.subscriptions[(appid)] = {  ...data,	"Author": "mikephie",	"Telegram": "https://t.me/mikephie",	"warning": "仅供学习，禁止转载或售卖",	"original_purchase_date": "2023-08-08T08:08:08Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
-  mikephie.body = JSON.stringify(mikephie76);
+    },
+    "first_seen" : "2023-10-12T12:09:21Z",
+    "original_application_version" : null,
+    "other_purchases" : {
+
+    },
+    "management_url" : null,
+    "subscriptions" : {igrammar_199_1y_v1
+
+    },
+    "entitlements" : {Pro
+
+    },
+    "original_purchase_date" : null,
+    "original_app_user_id" : "$RCAnonymousID:f670374782da4d8ba2b9edb07cda243c",
+    "last_seen" : "2023-10-12T12:09:21Z"
+  }
 }
 
-$done(mikephie);
+$done({body : JSON.stringify(mikephie)});
