2a3e3229f72ccb025cc7e10d2122d8a0018e00df
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-10-20 11:38:13 +0800

    更新 Mike_axs.js

--- Mike_axs.js
+++ Mike_axs.js
@@ -16,8 +16,7 @@
 
 # ======= 去广告 ======= #
 
-^https?:\/\/.*\.sg\/AXSMobile\/InfoPage\/.+ url reject-img
-#^https?:\/\/*\.sg\/AXSMobile\/InfoPage\/.+ url reject-img
+^https?:\/\/*\.sg\/AXSMobile\/InfoPage\/.+ url reject-img
 ^https:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/highlight url reject-img
 
 
