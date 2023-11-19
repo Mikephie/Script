7b80d00b99117d48b270825a4f5104b8174ddd5d
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-11-19 18:27:41 +0800

    更新 bizi.js

--- bizi.js
+++ bizi.js
@@ -1,35 +1,39 @@
-/*
-壁纸 4.5.3
+/*************************************
+
+项目名称：壁纸
+下载地址：https://t.cn/A6WmzYvC
+更新日期：2023-11-18
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
 
 [rewrite_local]
+^https?:\/\/leancloud\.emotionwp\.com\/.+\/classes\/wpf_account url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/bizi.js
 
-
-^https?:\/\/leancloud\.emotionwp\.com\/.+\/classes\/wpf_accoun url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/Mike_bizi.js 
-
-[mitm] 
-
+[mitm]
 hostname = leancloud.emotionwp.com
 
-**/
+*************************************/
 
-var mikephie = JSON.parse($response.body);
 
-mikephie.results = [
-  {
-    ...mikephie.results,
-    "updatedAt": "2023-11-19T09:29:18.937Z",
-    "vipEndTime": 4092599349,
-    "sex": "1",
-    "isSVIP": 1,
-    "userId": "000213.21970f036fd0454a99d29d8cebe086cd.0929",
-    "loginType": 1,
-    "nickName": "Mikephie",
-    "isVIP": 1,
-    "headImageUrl": "https://raw.githubusercontent.com/chxm1023/Script_X/main/icon/ddm.png",
-    "objectId": "6559d56eb87b3b5ada6602a7",
-    "createdAt": "2023-11-19T09:29:18.937Z",
-    "coin": 0
-  }
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023.results = [ {...chxm1023.results,
+   "updatedAt" : "2023-11-17T23:52:58.783Z",
+   "vipEndTime" : 4092599349,
+   "sex" : "1",
+   "isSVIP" : 1,
+   "userId" : "A66666666_B6666666666666-C66",
+   "loginType" : 1,
+   "nickName" : "叮当猫の分享频道",
+   "isVIP" : 1,
+   "headImageUrl" : "https://raw.githubusercontent.com/chxm1023/Script_X/main/icon/ddm.png",
+   "objectId" : "6666fcda5a17e66bd666c666",
+   "createdAt" : "2023-11-17T23:52:58.783Z",
+   "coin" : 0
+ }
 ];
 
-$done({ body: JSON.stringify(mikephie) });
+$done({body : JSON.stringify(chxm1023)});
