69c9efcdf7b693520957a7bbaead467aa3d8c910
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-11-30 19:29:40 +0800

    更新 Mike_reddit.js

--- Mike_reddit.js
+++ Mike_reddit.js
@@ -1,15 +1,18 @@
 #Surge #模块 #Reddit 过滤推广, 关 NSFW 提示(暴力替换版 有点 bug)
 
+let body;
+try {
+  if (/"(isNsfw|AdPost|AdMetadataCell|adPayload)"/.test($response.body)) {
+    body = $response.body.replace(
+      /(")(isNsfw|AdPost|AdMetadataCell|adPayload)(")/gi,
+      '$1_$2$3'
+    );
+  }
+} catch (e) {
+  console.log(e);
+} finally {
+  $done(body ? { body } : {});
+}
 
-① reddit.sgmodule
-#!name=Reddit
-#!desc=过滤推广, 关 NSFW 提示 @xream
-
-[General]
-
-force-http-engine-hosts = %APPEND% gql.reddit.com, gql-fed.reddit.com
-
-[Script]
-Reddit = type=http-response,pattern=^https?:\/\/gql(-fed)?\.reddit\.com,requires-body=1,max-size=0,timeout=30,script-path=reddit.js
 [MITM]
 hostname = %APPEND%, gql.reddit.com, gql-fed.reddit.com
