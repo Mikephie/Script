ca7afb8916a7de16511cffb12a3e426ef067686d
Mikephie <131840160+Mikephie@users.noreply.github.com>
2023-11-30 12:29:42 +0800

    Update Mike_reddit.js

--- Mike_reddit.js
+++ Mike_reddit.js
@@ -13,7 +13,7 @@
 force-http-engine-hosts = %APPEND% gql.reddit.com, gql-fed.reddit.com
 
 [Script]
-Reddit = type=http-response,pattern=^https?:\/\/gql(-fed)?\.reddit\.com,requires-body=1,max-size=0,timeout=30,script-path=reddit.js
+Reddit = type=http-response,pattern=^https?:\/\/gql(-fed)?\.reddit\.com,requires-body=1,max-size=0,timeout=30 url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/Mike_reddit.js
 [MITM]
 hostname = %APPEND%, gql.reddit.com, gql-fed.reddit.com
 ② reddit.js
