302633c2fa65b15ec22708ff9f15883d020b613e
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-02-14 05:32:52 +0800

    Update Mike_axs.js

--- Mike_axs.js
+++ Mike_axs.js
@@ -1,30 +1,21 @@
-/***********************************
+#!name=AXS Payment
 
-> 应用名称：AXS Payment
-> 下载地址：http://t.cn/Air8PM5k
-> 脚本作者：@Mikephie
-> 解锁说明：去广告+去水印
-> 更新时间：2023年10月11日 23:20
-> 特别提醒：如需转载请注明出处，谢谢合作！
-> 重写地址：https://raw.githubusercontent.com/Mikephie/Script/main/Mike_axs.js
-> 特别说明：⚠️⚠️⚠️
-          本脚本仅供学习交流使用，禁止转载售卖
-          ⚠️⚠️⚠️
-          
-          
-[rewrite_local]
+#!desc=AXS Payment 去广告
 
-# ======= 去广告 ======= #
+#!homepage=https://mike
+
+#!author=mike
+
+#!icon=> 重写地址：https://raw.githubusercontent.com/Mikephie/Script/main/Mike_axs.js
+
+
+
+
+
+hostname=m-station2.axs.com.sg
+
+
+
+
 
 ^https?:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/(InfoPage\/.*|highlight\/.*) url reject-200
-
-
-
-
-
-[mitm]
- 
-
-hostname = m-station2.axs.com.sg
-
-***********************************/
