6f7601efd801f6620afa0584fe436d5602098035
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-02-23 19:18:24 +0800

    Add files via upload

---
+++ qx/flightradar24.js
@@ -0,0 +1,18 @@
+> 应用名称：Flightradar24
+> 下载地址：http://t.cn/Air8PM5k
+> 脚本作者：@Mikephie
+> 解锁说明：去广告+去水印
+> 更新时间：17 Feb 2024 at 18:13
+> 特别提醒：如需转载请注明出处，谢谢合作！
+> 重写地址：https://raw.githubusercontent.com/Mikephie/Script/main/Flightradar24.js
+> 特别说明：⚠️⚠️⚠️本脚本仅供学习交流使用，禁止转载售卖⚠️⚠️⚠️
+          
+          
+[rewrite_local]
+
+# > Flightradar24@ddgksf2013
+^https?:\/\/mobile\.flightradar24\.com\/mobile\/(user-session|\w{9}) url script-echo-response https://github.com/ddgksf2013/dev/raw/main/flightradar24.js
+
+[mitm]
+hostname = mobile.flightradar24.com
+
