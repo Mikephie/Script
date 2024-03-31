0234e0e15aa449832e4568e654a104a5cab1e2c1
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-03-31 15:13:42 +0800

    Update revddm.js

--- qx/revddm.js
+++ qx/revddm.js
@@ -10,13 +10,13 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/revddm.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/revddm.js
 ^https?:\/\/app-measurement\.com\/config\/app url reject
 ^https?:\/\/firestore\.googleapis\.com url reject
 
 [mitm]
-hostname = api.revenuecat.com, app-measurement.com, firestore.googleapis.com
+hostname = api.revenuecat.com, app-measurement.com
 
 *************************************/
 
