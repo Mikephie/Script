6a3de943429a76fc136b74a5ec96bb3734de2011
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-04-03 16:41:08 +0800

    Update revenuebaby.js

--- qx/revenuebaby.js
+++ qx/revenuebaby.js
@@ -85,7 +85,7 @@ if (typeof $response == "undefined") {
     'Currency':{ name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.pro.crossgrade'},
     
         //以下内容来自叮当猫（@chxm1023）
-    'mizframa': { name: 'premium', id: 'mf_20_lifetime2', cm: 'sjb' },  //Mizframa v2.2
+    'mizframa': { name: 'premium', id: 'mf_20_lifetime2', cm: 'sjb' },  //Mizframa v2.0
     'ShellBoxKit': { name: 'pro', id: 'ShellBoxKit.Lifetime', cm: 'sjb' },  //CareServer-服务器监控
     'ShellBean':{ name: 'pro', id: 'com.ningle.shellbean.subscription.year', cm: 'sja' },  //Shellbean
     'PhotoRoom': { name: 'pro', id: 'com.background.pro.yearly', cm: 'sja' },  //PhotoRoom
