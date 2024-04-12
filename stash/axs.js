16be709b21a1227178c573ef11cb82ab20c12888
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-04-12 11:18:19 +0800

    脚本

---
+++ stash/axs.js
@@ -0,0 +1,21 @@
+#!name=AXS Payment
+
+#!desc=AXS Payment 去广告
+
+#!homepage=https://mike
+
+#!author=mike
+
+#!icon=https://raw.githubusercontent.com/Mikephie/Script/main/Mike_axs.js
+
+
+
+
+
+hostname=m-station2.axs.com.sg
+
+
+
+
+
+^https?:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/(InfoPage\/.*|highlight\/.*) url reject-200
