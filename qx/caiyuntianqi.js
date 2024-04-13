08e2d078b1f9761c4909f0342c8f28483ca5299e
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-04-13 21:54:53 +0800

    Vault updates: caiyuntianqi

--- qx/caiyuntianqi.js
+++ qx/caiyuntianqi.js
@@ -10,8 +10,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/(biz|wrapper|starplucker)\.(cyapi|caiyunapp)\.(cn|com)\/(.+\/(user\?app_name|activity\?app_name|visitors|operation\/banners|operation\/homefeatures|config)|p\/v\d\/(vip_info|user_info|entries|privileges|trial_card\/info)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
-^https?:\/\/(api|wrapper)\.(cyapi|caiyunapp)\.(cn|com)\/v\d\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
+^https?:\/\/(biz|wrapper|starplucker)\.(cyapi|caiyunapp)\.(cn|com)\/(.+\/(user\?app_name|activity\?app_name|visitors|operation\/banners|operation\/homefeatures|config)|p\/v\d\/(vip_info|user_info|entries|privileges|trial_card\/info)) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/caiyuntianqi.js
+^https?:\/\/(api|wrapper)\.(cyapi|caiyunapp)\.(cn|com)\/v\d\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/caiyuntianqi.js
 
 [mitm]
 hostname = *.cyapi.cn, *.caiyunapp.com
