b67d6bfc045fb2415a6998e6e86ae93baaa89dba
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-04-22 18:39:38 +0800

    Auto-commit

--- qx/revenueddm.js
+++ qx/revenueddm.js
@@ -249,8 +249,8 @@ const list = {
   'totowallet': { name: 'all', id: 'com.ziheng.totowallet.onetimepurchase', cm: 'sjb' },  //图图记账
   '1Blocker': { name: 'premium', id: 'blocker.ios.iap.lifetime', cm: 'sjb' },  //1Blocker-广告拦截
   'VSCO': { name: 'pro', id: 'vscopro_global_5999_annual_7D_free', cm: 'sja' }  //VSCO-照片与视频编辑
-  'ShellBean':{ name: 'pro', id: 'com.ningle.shellbean.subscription.year'},
-  
+  'ShellBean':{ name: 'pro', id: 'com.ningle.shellbean.subscription.year'},  //Shellbean
+  'Spark':{ name: 'premium', id: 'spark_5999_1y_1w0',  //Spark
 };
 
 if (typeof $response == "undefined") {
