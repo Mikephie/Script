857725e8a5a183c78b36cc064033971986695ea7
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-04-25 22:58:37 +0800

    ok

--- qx/test.js
+++ qx/test.js
@@ -172,7 +172,7 @@ if (typeof $response == "undefined") {
 
     //以下内容来自卑鄙的Baby（@Yu9191）
     'Spark':{ name: 'premium', id: 'spark_5999_1y_1w0'},
-    'ScreenRecordCase':{ name: 'Pro', id: 'me.fandong.ScreenRecordCase.Ultra'},
+    'ScreenRecordCase':{ name: 'Premium', id: 'me.fandong.ScreenRecordCase.Pro'},
     'WeeklyNote':{ name: 'org.zrey.weeklynote', id: 'org.zrey.weeklynote.flash_lifetime'},//周周记
     'Text%20Workflow':{ name: 'pro', id: 'tw_99_1m'},
     'FruitMinder':{ name: 'Premium', id: 'com.bartozo.FruitMinder.lifetime'},  //FruitMinder-水果提醒
