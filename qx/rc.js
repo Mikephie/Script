7a73d342ed39795a5725dba59168b1680b028038
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-04-29 05:42:41 +0800

    ok

--- qx/rc.js
+++ qx/rc.js
@@ -12,7 +12,7 @@ https://appraven.net/collection/77299969
 
 # ～ RevenueCat@mikephie
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/rc.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/mikephie/Scripts/master/deleteHeader.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/ddgksf2013/Scripts/master/deleteHeader.js
 
 [mitm]
 
@@ -23,6 +23,8 @@ hostname=api.revenuecat.com
 
 // ========= 动态 ID ========= //
 const mapping = {
+  'CountDuck': ['premium'],
+  'ScreenRecordCase': ['Premium'],
   'OneBox': ['all'],
   'Spark': ['premium'],
 
