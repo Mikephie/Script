15cda734b53fa072e1ff100b83ece6ab56bbb736
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-01 07:32:49 +0800

    ok

--- qx/rc.js
+++ qx/rc.js
@@ -31,7 +31,8 @@ if (forbiddenAppFound) {
 
 // ========= 动态 ID ========= //
 const mapping = {
-  'CountDuck': ['premium', 'Lifetime'],
+  //'CountDuck': ['premium', 'Lifetime'],
+  'CountDuck': { name: 'premium', id: 'Lifetime' }
   'IDM': ['premium'],
   'Whisper': ['all_features'],
   'Airmail': ['Airmail Premium'],
