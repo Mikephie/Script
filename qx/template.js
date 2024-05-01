2c7c98294ffb64140759621c95e0fd97e6743d7d
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-01 16:43:30 +0800

    Auto-commit

--- qx/template.js
+++ qx/template.js
@@ -1,7 +1,8 @@
 /*************************************
 
-项目名称：车票票
-下载地址：https://t.cn/A60ydwfR
+项目名称：Revenuecat 系列解锁合集
+下载地址：https://too.st/CollectionsAPP
+更新日期：2024-04-27
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -9,40 +10,80 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/chepiaopiao.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/chepiaopiao.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+
 
 [mitm]
-hostname = api.revenuecat.com
+hostname = api.revenuecat.com,
 
 *************************************/
 
+const chxm1024 = {};
+const headers = $request.headers;
+const chxm1023 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const ua = headers['User-Agent'] || headers['user-agent'];
+const bundle_id = headers['X-Client-Bundle-ID'] || headers['x-client-bundle-id'];
+
+//排除已禁止MITM的APP
+const forbiddenApps = ['Fileball', 'APTV'];
+const forbiddenAppFound = forbiddenApps.find(appName => (ua && ua.includes(appName)) || ($request.body && $request.body.includes(appName)));
+if (forbiddenAppFound) {
+  console.log(`发现禁止MITM的APP: ${forbiddenAppFound}，已停止运行脚本！\n叮当猫の分享频道: https://t.me/chxm1023`);
+  $done({});
+}
+//识别bundle_id
+const bundle = {
+  'io.innerpeace.yiye': { name: 'Premium', id: 'io.innerpeace.yiye.lifetime.forYearly', cm: 'sja' },  //言外笔记
+  'com.skysoft.removalfree': { name: 'Pro', id: 'com.skysoft.removalfree.subscription.newyearly', cm: 'sja' }  //图片消除
+}
+
+//识别UA
+const list = {
+  'MagicTiles3': { name: 'VIP', id: 'com.pianoidols.vipsub.year.06', cm: 'sja' },  //MagicTiles3-音乐游戏
+  'ShellBoxKit': { name: 'pro', id: 'ShellBoxKit.Lifetime', cm: 'sjb' },  //CareServer-服务器监控
 
 const chxm1023 = {};
 const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 if (typeof $response == "undefined") {
-  delete $request.headers["x-revenuecat-etag"];
-  delete $request.headers["X-RevenueCat-ETag"];
-  chxm1023.headers = $request.headers;
-} else if (chxm1024 && chxm1024.subscriber) {
-  chxm1024.subscriber.subscriptions = chxm1024.subscriber.subscriptions || {};
-  chxm1024.subscriber.entitlement = chxm1024.subscriber.entitlement || {};
-  const app = 'cm';const list = {'cm':{name: 'vip', id: 'eticket_3_life'}};
-  const data = {
-   "Author": "chxm1023",
-   "Telegram" : "https://t.me/chxm1023",
-   "warning": "仅供学习，禁止转载或售卖",
-   "original_purchase_date": "2022-09-09T09:09:09Z",
-   "purchase_date": "2022-09-09T09:09:09Z",
-   "ownership_type": "PURCHASED"};
-for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
-  chxm1024.subscriber.subscriptions[list[i].id] = data;
-  chxm1024.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
-  chxm1024.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
-                break;
-          }
+  delete headers["x-revenuecat-etag"];
+  delete headers["X-RevenueCat-ETag"];
+  chxm1024.headers = headers;
+} else if (chxm1023 && chxm1023.subscriber) {
+  chxm1023.subscriber.subscriptions = chxm1023.subscriber.subscriptions || {};
+  chxm1023.subscriber.entitlements = chxm1023.subscriber.entitlements || {};
+  let name, ids, idb, data;
+  for (const src of [list, bundle]) {
+    for (const i in src) {
+      const test = src === list ? ua : bundle_id;
+      if (new RegExp(`^${i}`, `i`).test(test)) {
+        data = { "purchase_date": "2023-09-09T09:09:09Z", "expires_date": "2099-09-09T09:09:09Z" };
+        ids = src[i].id; name = src[i].name; idb = src[i].idb;
+        break;
+      }
     }
-    chxm1023.body = JSON.stringify(chxm1024);
+  }
+  if (!name || !ids) {
+    data = { "purchase_date": "2023-09-09T09:09:09Z", "expires_date": "2099-09-09T09:09:09Z" };
+    name = 'pro';
+    ids = 'com.chxm1023.pro';
+  }
+  chxm1023.subscriber.entitlements[name] = Object.assign({}, data, { product_identifier: ids });
+  if (typeof idb !== 'undefined' && idb !== null) {
+    chxm1023.subscriber.entitlements[name] = Object.assign({}, data, { product_identifier: idb });
+  }
+  const subData = Object.assign({}, data, {
+    "Author": "chxm1023", "Telegram": "https://t.me/chxm1023",
+    "warning": "仅供学习，禁止转载或售卖", "original_purchase_date": "2023-09-09T09:09:09Z",
+    "store_transaction_id": "4900066666666666", "period_type": "trial", "store": "app_store",
+    "ownership_type": "PURCHASED"
+  });
+  chxm1023.subscriber.subscriptions[ids] = subData;
+  if (typeof idb !== 'undefined' && idb !== null) {
+    chxm1023.subscriber.subscriptions[idb] = subData;
+  }
+  chxm1024.body = JSON.stringify(chxm1023);
+  console.log('已操作成功🎉🎉🎉\n叮当猫の分享频道: https://t.me/chxm1023');
 }
 
-$done(chxm1023);
+$done(chxm1024);
