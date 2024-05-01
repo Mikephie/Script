60d00e255bc4de69cb89b9c4bfc0da7f46a075b4
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-01 12:53:06 +0800

    okok

--- qx/template-3.js
+++ qx/template-3.js
@@ -11,8 +11,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/AnimeArt.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/AnimeArt.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/名template-3.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/名template-3.js
 
 [mitm]
 hostname = api.revenuecat.com
@@ -34,10 +34,10 @@ if (typeof $response == "undefined") {
   Mikephie76.subscriber.entitlements = Mikephie76.subscriber.entitlements || {};
   const data = {
     "product_identifier": (appid),
-    "purchase_date": "2022-09-09T09:09:09Z"
+    "purchase_date": "2024-04-04T04:04:04Z"
   };
   Mikephie76.subscriber.entitlements[(name)] = (data);
-  Mikephie76.subscriber.subscriptions[(appid)] = { ...data,  "Author": "Mikephie",  "Telegram": "https://t.me/Mikephie",  "warning": "仅供学习，禁止转载或售卖",  "original_purchase_date": "2022-09-09T09:09:09Z",  "store": "app_store",  "ownership_type": "PURCHASED" };
+  Mikephie76.subscriber.subscriptions[(appid)] = { ...data,  "Author": "Mikephie",  "Telegram": "https://t.me/Mikephie",  "warning": "仅供学习，禁止转载或售卖",  "original_purchase_date": "2024-04-04T04:04:04Z",  "store": "app_store",  "ownership_type": "PURCHASED" };
   Mikephie.body = JSON.stringify(Mikephie76);
 }
 
