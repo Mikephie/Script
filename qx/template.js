073e43a151c6102256423123e98b676c19d35afc
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-01 18:48:06 +0800

    Auto-commit

--- qx/template.js
+++ qx/template.js
@@ -3,25 +3,27 @@
 项目名称：Revenuecat 系列解锁合集
 下载地址：https://too.st/CollectionsAPP
 更新日期：2024-04-27
-脚本作者：chxm1023
+脚本作者：mikephie
 电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/template.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/template.js
-
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https?:\/\/app-measurement\.com\/config\/app url reject
+^https?:\/\/firestore\.googleapis\.com url reject
 
 [mitm]
-hostname = api.revenuecat.com,
+hostname = api.revenuecat.com, app-measurement.com, firestore.googleapis.com
 
 *************************************/
 
-const chxm1024 = {};
+
+const mikephie = {};
 const headers = $request.headers;
-const chxm1023 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const mikephie8 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 const ua = headers['User-Agent'] || headers['user-agent'];
 const bundle_id = headers['X-Client-Bundle-ID'] || headers['x-client-bundle-id'];
 
@@ -32,6 +34,7 @@ if (forbiddenAppFound) {
   console.log(`发现禁止MITM的APP: ${forbiddenAppFound}，已停止运行脚本！\n叮当猫の分享频道: https://t.me/chxm1023`);
   $done({});
 }
+
 //识别bundle_id
 const bundle = {
   'io.innerpeace.yiye': { name: 'Premium', id: 'io.innerpeace.yiye.lifetime.forYearly', cm: 'sja' },  //言外笔记
@@ -40,51 +43,45 @@ const bundle = {
 
 //识别UA
 const list = {
-  'MagicTiles3': { name: 'VIP', id: 'com.pianoidols.vipsub.year.06', cm: 'sja' },  //MagicTiles3-音乐游戏
-  //'ShellBoxKit': { name: 'pro', id: 'ShellBoxKit.Lifetime' },  //CareServer-服务器监控
   'ShellBoxKit': { name: 'pro', id: 'ShellBoxKit.Lifetime', cm: 'sjb' },  //CareServer-服务器监控
+  'IDM': { name: 'premium', id: 'sub_yearly_idm', cm: 'sja' },  //IDM-下载
+  'Whisper': { name: 'all_features', id: 'whisperai_80_y', cm: 'sja' },  //Whisper
+};
 
-const chxm1023 = {};
-const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 if (typeof $response == "undefined") {
   delete headers["x-revenuecat-etag"];
   delete headers["X-RevenueCat-ETag"];
-  chxm1024.headers = headers;
-} else if (chxm1023 && chxm1023.subscriber) {
-  chxm1023.subscriber.subscriptions = chxm1023.subscriber.subscriptions || {};
-  chxm1023.subscriber.entitlements = chxm1023.subscriber.entitlements || {};
-  let name, ids, idb, data;
+  mikephie.headers = headers;
+} else if (mikephie8 && mikephie8.subscriber) {
+  mikephie8.subscriber.subscriptions = mikephie8.subscriber.subscriptions || {};
+  mikephie8.subscriber.entitlements = mikephie8.subscriber.entitlements || {};
+  let name,nameb,ids,idb,data;
   for (const src of [list, bundle]) {
     for (const i in src) {
       const test = src === list ? ua : bundle_id;
       if (new RegExp(`^${i}`, `i`).test(test)) {
-        data = { "purchase_date": "2023-09-09T09:09:09Z", "expires_date": "2099-09-09T09:09:09Z" };
-        ids = src[i].id; name = src[i].name; idb = src[i].idb;
-        break;
+      if (src[i].cm.indexOf('sja') != -1) { data = {  "purchase_date" : "2023-09-09T09:09:09Z",  "expires_date" : "2099-09-09T09:09:09Z" };  } else if (src[i].cm.indexOf('sjb') != -1) { data = {  "purchase_date" : "2023-09-09T09:09:09Z" }; }
+      ids = src[i].id;name = src[i].name;idb = src[i].idb;nameb = src[i].nameb;
+      break;
       }
     }
   }
   if (!name || !ids) {
-    data = { "purchase_date": "2023-09-09T09:09:09Z", "expires_date": "2099-09-09T09:09:09Z" };
+    data = {  "purchase_date" : "2023-09-09T09:09:09Z",  "expires_date" : "2099-09-09T09:09:09Z" };
     name = 'pro';
     ids = 'com.chxm1023.pro';
   }
-  chxm1023.subscriber.entitlements[name] = Object.assign({}, data, { product_identifier: ids });
-  if (typeof idb !== 'undefined' && idb !== null) {
-    chxm1023.subscriber.entitlements[name] = Object.assign({}, data, { product_identifier: idb });
+  mikephie8.subscriber.entitlements[name] = Object.assign({}, data, { product_identifier: ids });
+  if (typeof nameb !== 'undefined' && nameb !== null) {
+    mikephie8.subscriber.entitlements[nameb] = Object.assign({}, data, { product_identifier: idb });
   }
-  const subData = Object.assign({}, data, {
-    "Author": "chxm1023", "Telegram": "https://t.me/chxm1023",
-    "warning": "仅供学习，禁止转载或售卖", "original_purchase_date": "2023-09-09T09:09:09Z",
-    "store_transaction_id": "4900066666666666", "period_type": "trial", "store": "app_store",
-    "ownership_type": "PURCHASED"
-  });
-  chxm1023.subscriber.subscriptions[ids] = subData;
+  const subData = Object.assign({},data,{  "Author": "mikephie",  "Telegram": "https://t.me/chxm1023",  "warning": "仅供学习，禁止转载或售卖",  "original_purchase_date": "2023-09-09T09:09:09Z",  "store_transaction_id" : "4900066666666666",  "period_type" : "trial",  "store": "app_store",  "ownership_type": "PURCHASED"  });
+  mikephie8.subscriber.subscriptions[ids] = subData;
   if (typeof idb !== 'undefined' && idb !== null) {
-    chxm1023.subscriber.subscriptions[idb] = subData;
+    mikephie8.subscriber.subscriptions[idb] = subData;
   }
-  chxm1024.body = JSON.stringify(chxm1023);
+  mikephie.body = JSON.stringify(mikephie8);
   console.log('已操作成功🎉🎉🎉\n叮当猫の分享频道: https://t.me/chxm1023');
 }
 
-$done(chxm1024);
+$done(mikephie);
