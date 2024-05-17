e6e3bdeb19281b250afbf40db2d73e380828f499
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-17 14:43:58 +0800

    o

---
+++ qx/DuetDisplay 2.js
@@ -0,0 +1,34 @@
+/*
+‎Duet Display 2.18
+DuetAir 1.2.12
+
+需登录
+
+[rewrite_local]
+
+https://rdp.duetdisplay.com/v1/users/validateReceipt url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/DuetDisplay.js
+
+[mitm]
+hostname = rdp.duetdisplay.com
+*/
+
+
+var objc = JSON.parse($response.body);
+
+    objc = {
+  "success": true,
+  "products": [
+    {
+      "vendor": "stripe",
+      "product": "duetProAnnual",
+      "subscriptionId": 2193526,
+      "purchaseDate": "2024-05-14T02:41:27Z",
+      "cancelled": false,
+      "expiresDate": "2088-08-08T19:47:22Z",
+      "inTrial": true
+    }
+  ],
+  "hasStripeAccount": false
+}
+
+$done({body : JSON.stringify(objc)});
