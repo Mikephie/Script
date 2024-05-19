30a30de7d88b02ea2b5569a0f6ae47207581195e
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-19 21:54:12 +0800

    delete

--- qx/template-2.js
+++ qx/template-2.js
@@ -11,35 +11,34 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/AnimeArt.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/AnimeArt.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/template-3.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/template-3.js
 
 [mitm]
 hostname = api.revenuecat.com
 
 *************************************/
 
+const Mikephie = {};
+const Mikephie76 = JSON.parse(typeof $response!= "undefined" && $response.body || null);
 
-const chxm1023 = {};
-const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
-
-const name = "AnimeArt.Gold";
-const appid = "WaifuArt.Lifetime";
+const name = "Lifetime";
+const appid = "ShellBoxKit.Lifetime";
 
 if (typeof $response == "undefined") {
   delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
-  chxm1023.headers = $request.headers;
-} else if (chxm1024 && chxm1024.subscriber) {
-  chxm1024.subscriber.subscriptions = chxm1024.subscriber.subscriptions || {};
-  chxm1024.subscriber.entitlements = chxm1024.subscriber.entitlements || {};
+  Mikephie.headers = $request.headers;
+} else if (Mikephie76 && Mikephie76.subscriber) {
+  Mikephie76.subscriber.subscriptions = Mikephie76.subscriber.subscriptions || {};
+  Mikephie76.subscriber.entitlements = Mikephie76.subscriber.entitlements || {};
   const data = {
-	"product_identifier": (appid),
-	"purchase_date": "2022-09-09T09:09:09Z"
-	};
-  chxm1024.subscriber.entitlements[(name)] = (data);
-  chxm1024.subscriber.subscriptions[(appid)] = {  ...data,	"Author": "chxm1023",	"Telegram": "https://t.me/chxm1023",	"warning": "仅供学习，禁止转载或售卖",	"original_purchase_date": "2022-09-09T09:09:09Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
-  chxm1023.body = JSON.stringify(chxm1024);
+    "product_identifier": (appid),
+    "purchase_date": "2024-04-04T04:04:04Z"
+  };
+  Mikephie76.subscriber.entitlements[(name)] = (data);
+  Mikephie76.subscriber.subscriptions[(appid)] = { ...data,  "Author": "Mikephie",  "Telegram": "https://t.me/Mikephie",  "warning": "仅供学习，禁止转载或售卖",  "original_purchase_date": "2024-04-04T04:04:04Z",  "store": "app_store",  "ownership_type": "PURCHASED" };
+  Mikephie.body = JSON.stringify(Mikephie76);
 }
 
-$done(chxm1023);
+$done(Mikephie);
