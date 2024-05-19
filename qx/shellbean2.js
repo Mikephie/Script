b60d98077343ff3f35243b4c787ebae50d4e0ad7
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-19 11:49:21 +0800

    Update and rename shellbean1.js to shellbean2.js

--- qx/shellbean2.js
+++ qx/shellbean2.js
@@ -11,7 +11,7 @@
 
 [rewrite_local]
 
-^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/shellbean.js
+^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/shellbean2.js
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/revenueheader.js
 
 [mitm] 
