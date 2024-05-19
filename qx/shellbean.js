4fa0f8e789e3f23f105adb888abe7cc2ffdbb9b0
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-19 11:00:41 +0800

    o

--- qx/shellbean.js
+++ qx/shellbean.js
@@ -11,7 +11,7 @@
 
 [rewrite_local]
 
-https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/spark.js
+https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/shellbean.js
 
 [mitm] 
 
