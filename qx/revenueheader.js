99d11bbc2ac1d0867fdb62ed6ef6fd7476f6ff05
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-19 13:42:36 +0800

    o

--- qx/revenueheader.js
+++ qx/revenueheader.js
@@ -37,7 +37,7 @@ const list = {
   'Currency': { name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.plus' },  //Currency-汇率查询
   'TripMemo': { name: 'pro', id: 'com.ningle.dailytracker.lifetime' },  //旅行迹
   'ShellBean': { name: 'pro', id: 'com.ningle.shellbean.iap.forever' },  //ShellBean-SSH终端服/Linux监控
-
+  'ShellBoxKit': { name: 'pro', id: 'ShellBoxKit.Lifetime' },  //CareServer-服务器监控
 
 }
 
