a896463850dde6509e79bcd34e45374c11c2c447
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-19 14:32:20 +0800

    Update careserver.js

--- qx/careserver.js
+++ qx/careserver.js
@@ -1,9 +1,9 @@
 /******************************
 
-脚本名称: Drops
-下载地址：商店 38.0
-脚本作者：ios151
-更新时间：2023年10月5日 20:47
+脚本名称: Careserver
+下载地址：商店
+脚本作者：Mikephie
+更新时间：10 May 2024 at 14:25
 问题反馈：TG
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
@@ -11,9 +11,8 @@
 
 [rewrite_local]
 
-https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/careserver.js
-#清理
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/Revenuecat.js
+^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/careaerver.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/revenueheader.js
 
 [mitm] 
 
@@ -21,48 +20,50 @@ hostname = api.revenuecat.com
 
 *******************************/
 
-var objc = JSON.parse($response.body);
+var mikephie = JSON.parse($response.body);
 
-    objc = {
-  "request_date_ms": 1714686943375,
-  "request_date": "2024-05-02T21:55:43Z",
-  "subscriber": {
-    "non_subscriptions": {
+    mikephie = {
+  "request_date_ms" : 1716098977241,
+  "request_date" : "2024-05-19T06:09:37Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+
     },
-    "first_seen": "2024-05-02T21:53:23Z",
-    "original_application_version" : null,
-    "other_purchases": {
+    "first_seen" : "2024-05-01T01:54:39Z",
+    "original_application_version" : "6",
+    "other_purchases" : {
+
     },
-    "management_url": "https:\/\/apps.apple.com\/account\/subscriptions",
-    "subscriptions": {
-      "ShellBoxKit.Lifetime": {
-        "original_purchase_date": "2024-05-02T21:53:23Z",
-        "expires_date": "2099-10-12T12:43:00Z",
-        "is_sandbox": false,
-        "refunded_at": null,
-        "store_transaction_id": "190001746507030",
-        "unsubscribe_detected_at": null,
-        "grace_period_expires_date": null,
-        "period_type": "trial",
-        "purchase_date": "2024-05-02T21:53:23Z",
-        "billing_issues_detected_at": null,
-        "ownership_type": "PURCHASED",
-        "store": "app_store",
-        "auto_resume_date": null
+    "management_url" : "https://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "ShellBoxKit.Year" : {
+        "original_purchase_date" : "2024-05-19T06:06:26Z",
+        "expires_date" : "2024-06-02T06:06:25Z",
+        "is_sandbox" : false,
+        "refunded_at" : null,
+        "store_transaction_id" : "150001791650125",
+        "unsubscribe_detected_at" : null,
+        "grace_period_expires_date" : null,
+        "period_type" : "trial",
+        "purchase_date" : "2024-05-19T06:06:25Z",
+        "billing_issues_detected_at" : null,
+        "ownership_type" : "PURCHASED",
+        "store" : "app_store",
+        "auto_resume_date" : null
       }
     },
-    "entitlements": {
-      "pro": {
-        "grace_period_expires_date": null,
-        "purchase_date": "2024-05-02T21:53:23Z",
-        "product_identifier": "ShellBoxKit.Lifetime",
-        "expires_date": "2099-10-12T12:43:00Z"
+    "entitlements" : {
+      "ssh_pro" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2024-05-19T06:06:25Z",
+        "product_identifier" : "ShellBoxKit.Year",
+        "expires_date" : "2024-06-02T06:06:25Z"
       }
     },
-    "original_purchase_date": "2024-05-02T21:53:23Z",
-    "original_app_user_id": "$$RCAnonymousID:27e47831e5e7431fb7d9e76384938a42",
-    "last_seen": "2024-05-02T21:53:23Z"
+    "original_purchase_date" : "2024-04-03T13:08:46Z",
+    "original_app_user_id" : "$RCAnonymousID:20771c8af6754cc9b71681c91b3b00de",
+    "last_seen" : "2024-05-18T16:12:06Z"
   }
 }
 
-$done({body : JSON.stringify(objc)});
+$done({body : JSON.stringify(mikephie)});
