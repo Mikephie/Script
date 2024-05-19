341c2d8155a5987134358c706131625ea5eb1d54
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-19 18:09:42 +0800

    Update careserver1.js

--- qx/careserver1.js
+++ qx/careserver1.js
@@ -36,9 +36,9 @@ var mikephie = JSON.parse($response.body);
     },
     "management_url" : "https://apps.apple.com/account/subscriptions",
     "subscriptions" : {
-      "ShellBoxKit.Lifetime" : {
+      "ShellBoxKit.Year" : {
         "original_purchase_date" : "2024-05-19T06:06:26Z",
-        "expires_date" : "2088-08-08T08:08:08Z",
+        "expires_date" : "2088-06-02T06:06:25Z",
         "is_sandbox" : false,
         "refunded_at" : null,
         "store_transaction_id" : "150001791650125",
@@ -53,11 +53,11 @@ var mikephie = JSON.parse($response.body);
       }
     },
     "entitlements" : {
-      "pro" : {
+      "ssh_pro" : {
         "grace_period_expires_date" : null,
         "purchase_date" : "2024-05-19T06:06:25Z",
-        "product_identifier" : "ShellBoxKit.Lifetime",
-        "expires_date" : "2088-08-08T08:08:08Z"
+        "product_identifier" : "ShellBoxKit.Year",
+        "expires_date" : "2088-06-02T06:06:25Z"
       }
     },
     "original_purchase_date" : "2024-04-03T13:08:46Z",
