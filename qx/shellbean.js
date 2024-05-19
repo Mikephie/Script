62cd9b1370fff1f61bd11ca7bc18fa355bc0671c
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-19 11:41:26 +0800

    o

--- qx/shellbean.js
+++ qx/shellbean.js
@@ -11,7 +11,8 @@
 
 [rewrite_local]
 
-https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/shellbean.js
+^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/shellbean.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
 
 [mitm] 
 
