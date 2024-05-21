4064b8a6abbf8eed6cebb996518ec01e60383d1c
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-05-21 19:47:03 +0800

    Update mrevenue.js

--- qx/mrevenue.js
+++ qx/mrevenue.js
@@ -3,8 +3,8 @@
 Revenuecat解锁合集
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body mrevenue.js
-//^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header mrevenue.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/mrevenue.js
+//^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/mrevenue.js
 
 [MITM]
 hostname = api.revenuecat.com
