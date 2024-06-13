7b5728ef736a80f517c0b383e084c45d3d249b30
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-13 18:45:37 +0800

    up

--- qx/filmix-2.js
+++ qx/filmix-2.js
@@ -26,7 +26,7 @@ var mikephie = JSON.parse($response.body);
   "created_at" : "2024-03-08T12:27:47.759205+08:00",
   "userprofile_id" : "_d371b7f768661f4ccaca1e6eac2f1278",
   "vip_start_time" : "2024-06-13T17:57:17+08:00",
-  "year_reset_time" : "2088-06-14T08:00:00+08:00",
+  "year_reset_time" : "2088-08-08T08:08:08+08:00",
   "token" : "aaff0c12a53257b29d6e1cbab698447672b7d846",
   "year_total_match_max" : 10084,
   "updated_at" : "2024-06-13T18:05:56.543849+08:00",
@@ -41,7 +41,7 @@ var mikephie = JSON.parse($response.body);
     "email" : "",
     "is_active" : true
   },
-  "vip_end_time" : "2088-06-13T17:57:16+08:00"
+  "vip_end_time" : "2088-08-08T08:08:08+08:00" 
 }
-
+ 
 $done({body : JSON.stringify(mikephie)});
