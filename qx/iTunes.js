259936235eca109b3b10f0506b0d0eb2147dfa69
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-15 10:30:43 +0800

    Auto-commit

--- qx/iTunes.js
+++ qx/iTunes.js
@@ -49,6 +49,8 @@ var uaProductMapping = {
   'Subscriptions': {product_id: 'com.touchbits.subscriptions.iap.pro.yearly'},  //订阅通
   'TimeCut': {product_id: 'com.floatcamellia.hfrslowmotion.forevervip'},  //TimeCut
   'imgplay': {product_id: 'me.imgbase.imgplay.subscriptionYearly'},  //imgPlay
+  'Ever%20Play': {product_id: 'me.imgbase.imgplay.subscriptionYearly'},  //imgPlay
+
 
 
 };
