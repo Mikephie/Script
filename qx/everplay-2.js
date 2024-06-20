ab6bea35737c68af6535ec3a4a817ab1a901fab4
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-21 07:45:37 +0800

    new

--- qx/everplay-2.js
+++ qx/everplay-2.js
@@ -1,6 +1,6 @@
 /******************************
 
-脚本名称: itunestest
+脚本名称: 66铃声 解锁VIP
 下载地址：商店
 脚本作者：Mikephie
 更新时间：2024年6月15日 15:56
@@ -10,11 +10,9 @@
 *******************************
 
 [rewrite_local]
-
-^https?:\/\/kkyun\.com\/api\/accounts\/user? url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/66LS.js
+^https?:\/\/kkyun\.com\/api\/accounts\/user? url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/66ringtones.js
 
 [mitm] 
-
 hostname = kkyun.com
 
 *******************************/
