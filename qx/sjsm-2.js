720f0ac08f694d5d13543af2a7a3b460d72df952
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-22 07:40:11 +0800

    Auto-commit

--- qx/sjsm-2.js
+++ qx/sjsm-2.js
@@ -12,7 +12,7 @@
 
 [rewrite_local]
 # >试卷扫描-拍照清除笔迹，还原空白试卷，错题标记重组（永久会员）
-^https?:\/\/appss.baomingding.com\/\/app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/sjsm.js
+^https?:\/\/appss.baomingding.com\/\/app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/cdsp.js
 
 [mitm] 
 hostname = appss.baomingding.com
@@ -25,29 +25,23 @@ var mikephie = JSON.parse($response.body);
     mikephie = {
   "success" : true,
   "result" : {
-    "dataId" : "10171672668831233491",
-    "remainTimeSeconds" : 88888,
     "realnameStatus" : "NO",
     "appleUserEmail" : "mikephiemy@gmail.com",
-    "wordage" : 88888888,
     "freeFlag" : "NO",
-    "inviteCode" : "QPHMUFX8",
+    "wordage" : 0,
+    "inviteCode" : "uP2rEjVh",
     "vipGroupInfos" : [
-      {
-        "groupType" : "TYPE_ONE",
-        "vipType" : "VIP",
-        "autoPay" : "NO"
-      }
+
     ],
     "autoPay" : "NO",
-    "type" : "VIP",
-    "vipExpireTime" : "2088-08-08 08:08:08",
-    "vipExpireDays" : 88888888,
-    "registerTime" : "2024-05-26 20:31:28",
+    "type" : "NORMAL",
+    "times" : 0,
+    "dataId" : "10171615389361166950",
+    "registerTime" : "2024-05-20 05:24:54",
     "nickname" : "Mikephie",
     "email" : "mikephiemy@gmail.com",
     "headImg" : "https://boniuapp.rhinox.cn/common/head/1599546812784.png",
-    "times" : 88888888
+    "remainTimeSeconds" : 0
   },
   "returnCode" : "200",
   "timeOut" : false
