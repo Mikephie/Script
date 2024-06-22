ec9b240e821f52ed8bcd07616213bd08266b6579
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-22 21:44:12 +0800

    Auto-commit

--- qx/66ringtones-1.js
+++ qx/66ringtones-1.js
@@ -20,35 +20,76 @@ hostname = msg.umengcloud.com
 var mikephie = JSON.parse($response.body);
 
     mikephie = {
-  "header" : {
-    "sdk_version" : "4.0.2",
-    "is_pirated" : "NO",
-    "language" : "zh-Hans-SG",
-    "country" : "SG",
-    "app_version" : "0",
-    "sdk_type" : "iOS",
-    "oid" : "142823e6fabeac37d23d675e90b97f9ae0c10437",
-    "channel" : "App Store",
-    "resolution" : "2796*1290",
-    "app_store_version" : "5.1.10",
-    "os" : "iOS",
-    "short_version" : "5.1.10",
-    "package_name" : "com.zhangchao.AudioPlayer",
-    "req_time" : "1719061634.475868",
-    "access" : "WiFi",
-    "carrier" : "--",
-    "idfv" : "5F866B54-5E88-48A0-855A-7F1C00123221",
-    "timezone" : "8",
-    "os_version" : "17.5.1",
-    "device_model" : "iPhone16,2",
-    "is_jailbroken" : "NO",
-    "display_name" : "",
-    "push_switch" : "true",
-    "appkey" : "5a325412f29d984dba000167",
-    "umid" : "f73a7447a887db8df5106f342aac52"
-  },
-  "tags" : "subscribed_purchased",
-  "device_token" : "40a757d4bb3a05201b4bfb3781ebe8878c30a727ea9ea4039e9d8e3a94d310b8"
+  "data" : {
+    "me" : {
+      "login" : "Rnik666🎖",
+      "is_admin" : false,
+      "can_view_p_f_s" : true,
+      "next_onboarding_step" : "goals",
+      "created_at" : "2024-06-22 04:45:24 -0700",
+      "id" : "31684150",
+      "is_newsletter_subscriber" : true,
+      "is_platform_developer" : false,
+      "normalized_user_tier" : "Pro",
+      "prompt_first_time_feedback" : "not_yet",
+      "profile_username" : null,
+      "profile_image_url" : null,
+      "applet_quota_slots_total" : 2,
+      "profile_provider" : null,
+      "marketing_push_notification_setting" : true,
+      "user_subscriptions" : [
+
+      ],
+      "website" : null,
+      "is_promotions_subscriber" : false,
+      "permissions" : {
+        "multi_action" : {
+          "minimum_tier" : "intermediate_pro",
+          "permitted" : true
+        },
+        "multi_service_account" : {
+          "minimum_tier" : "pro",
+          "permitted" : true
+        },
+        "queries" : {
+          "minimum_tier" : "pro",
+          "permitted" : true
+        },
+        "filter_code" : {
+          "minimum_tier" : "pro",
+          "permitted" : true
+        }
+      },
+      "email" : "rnik666@gmail.com",
+      "alerts_push_notification_setting" : true,
+      "applet_quota_slots_remaining" : 999,
+      "is_platform_partner_subscriber" : true,
+      "send_alerts" : true,
+      "unlimited_applet_enables" : true,
+      "two_factor_auth" : null,
+      "home_screen_preference" : "",
+      "bio" : null,
+      "organizations" : [
+
+      ],
+      "time_zone" : {
+        "id" : "Asia/Shanghai",
+        "formatted_offset" : "+08:00",
+        "name" : "Beijing"
+      },
+      "is_beta_subscriber" : true,
+      "tier" : "pro",
+      "user_login_providers" : [
+
+      ]
+    },
+    "live_applets" : [
+
+    ],
+    "applets" : [
+
+    ]
+  }
 }
 
 
