80c01c076fbcf64ca9eb865093c1ae4f4f80aec7
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-24 14:11:55 +0800

    Auto-commit

--- qx/ds2.js
+++ qx/ds2.js
@@ -12,3 +12,20 @@
 
 [MITM]
 hostname = photoby.hasmash.com
+
+*******************************/
+
+var mikephie = JSON.parse($response.body);
+
+    mikephie = {
+  "message": "成功!",
+  "result": {
+    "uid": "efe376d39b4f4385a45cb19ae0c8c13b",
+    "phone": "18888888888",
+    "memberExpire": 4092610661000
+  },
+  "code": "00000"
+}
+  
+
+$done({body : JSON.stringify(mikephie)});
