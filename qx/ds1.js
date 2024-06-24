8bfe8736d5ee8f5ab261325b97d1b37af79210f9
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-24 13:49:14 +0800

    Auto-commit

--- qx/ds1.js
+++ qx/ds1.js
@@ -1,1 +1,29 @@
-var Rnik=JSON['parse']($response['body']);Rnik={'message':'成功!','result':{'id':0xd1d733,'pageName':_0x11a6('0x0','fv6R'),'elementId':'付费点击','uid':_0x11a6('0x1','!Cq8'),'iosOrAnd':_0x11a6('0x2','pv!W'),'version':_0x11a6('0x3','ikj['),'isVip':0x1,'isCN':0x1,'deviceId':'26242316-0D4E-4F0F-A1B2-1FE5A99E68BC','deviceType':_0x11a6('0x4','aU*B'),'pageTitle':_0x11a6('0x5','BXd]'),'elementPosition':'','elementContent':'付费页面','ip':_0x11a6('0x6','(EA4')},'code':'00000'};$done({'body':JSON['stringify'](Rnik)});;
+// Parse the JSON response
+var Rnik = JSON.parse($response.body);
+
+// Modify the parsed object
+Rnik = {
+    'message': '成功!',
+    'result': {
+        'id': 13706739,
+        'pageName': 'decoded value for pageName',
+        'elementId': '付费点击',
+        'uid': 'decoded value for uid',
+        'iosOrAnd': 'decoded value for iosOrAnd',
+        'version': 'decoded value for version',
+        'isVip': 1,
+        'isCN': 1,
+        'deviceId': '26242316-0D4E-4F0F-A1B2-1FE5A99E68BC',
+        'deviceType': 'decoded value for deviceType',
+        'pageTitle': 'decoded value for pageTitle',
+        'elementPosition': '',
+        'elementContent': '付费页面',
+        'ip': 'decoded value for ip'
+    },
+    'code': '00000'
+};
+
+// Stringify the modified object and send it as the response
+$done({
+    'body': JSON.stringify(Rnik)
+});
