5570f9ba89db2522061b4824d08825f7cd529217
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-25 17:17:07 +0800

    Auto-commit

--- qx/hbzzds.js
+++ qx/hbzzds.js
@@ -1,17 +1,11 @@
-/******************************
-
-脚本名称: 海报制作大师 解锁VIP
-下载地址：商店
-脚本作者：Mikephie
-更新时间：2024年6月15日 15:56
-问题反馈：TG
-使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-
-*******************************
+/*
+海报制作大师
+解锁永久VIP
+@Rnik666
 
 [rewrite_local] 
- 
-^https:\/\/poster\.leminet\.cn\/v01\/profile url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/hbzzds.js
+
+^https?:\/\/poster\.leminet\.cn\/v01\/profile url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/HBZZDS.js
 
 [MITM]
 hostname = poster.leminet.cn
