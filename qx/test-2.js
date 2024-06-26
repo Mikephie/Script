ce383278621205fdcd68876fd90e6f0705b15d72
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-26 18:23:53 +0800

    new

--- qx/test-2.js
+++ qx/test-2.js
@@ -8,7 +8,7 @@
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
 [rewrite_local] 
-^https:\/\/www\.lovewith\.me\/golove\/v05\/profile url script-response-body
+^https:\/\/www\.lovewith\.me\/golove\/v05\/profile url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/hljx.js
 
 
 [MITM]
@@ -20,73 +20,28 @@ hostname = www.lovewith.me
 var mikephie = JSON.parse($response.body);
 
     mikephie = {
-  "request_date_ms": 1715290303654,
-  "request_date": "2024-05-09T21:31:43Z",
-  "subscriber": {
-    "non_subscriptions": {
-      "spark_openai_tokens_4xt": [
-        {
-          "id": "80b88cbbc5",
-          "is_sandbox": false,
-          "purchase_date": "2024-05-08T08:08:10Z",
-          "original_purchase_date": "2024-05-08T08:08:10Z",
-          "store": "app_store",
-          "store_transaction_id": "150001777189185"
-        }
-      ]
-    },
-    "first_seen": "2023-10-19T10:37:33Z",
-    "original_application_version": "3.9.3.303",
-    "other_purchases": {
-      "spark_openai_tokens_4xt": {
-        "purchase_date": "2024-05-08T08:08:10Z"
-      }
-    },
-    "management_url": null,
-    "subscriptions": {
-      "rc_promo_free-sws_lifetime": {
-        "original_purchase_date": "2023-10-19T10:37:33Z",
-        "expires_date": "2223-09-01T10:37:33Z",
-        "is_sandbox": false,
-        "refunded_at": null,
-        "store_transaction_id": "7d3eac6aefd0ed38806eecaf0140dbbc",
-        "unsubscribe_detected_at": null,
-        "grace_period_expires_date": null,
-        "period_type": "normal",
-        "purchase_date": "2023-10-19T10:37:33Z",
-        "billing_issues_detected_at": null,
-        "store": "promotional",
-        "auto_resume_date": null
-      },
-      "spark_5999_1y_1w0": {
-        "Author": "mikephie",
-        "store_transaction_id": "4900066666666666",
-        "period_type": "trial",
-        "store": "app_store",
-        "ownership_type": "PURCHASED",
-        "expires_date": "2088-08-08T08:08:08Z",
-        "warning": "仅供学习，禁止转载或售卖",
-        "original_purchase_date": "2023-09-09T09:09:09Z",
-        "purchase_date": "2023-09-09T09:09:09Z",
-        "Telegram": "https:\/\/t.me\/mikephie"
-      }
-    },
-    "entitlements": {
-      "premium": {
-        "expires_date": "2088-08-08T08:08:08Z",
-        "product_identifier": "spark_5999_1y_1w0",
-        "purchase_date": "2023-09-09T09:09:09Z"
-      },
-      "free-sws": {
-        "grace_period_expires_date": null,
-        "purchase_date": "2023-10-19T10:37:33Z",
-        "product_identifier": "rc_promo_free-sws_lifetime",
-        "expires_date": "2223-09-01T10:37:33Z"
-      }
-    },
-    "original_purchase_date": "2024-04-22T13:51:50Z",
-    "original_app_user_id": "8PNqxsNs34SUe82",
-    "last_seen": "2024-05-09T09:14:54Z"
+  "success" : true,
+  "data" : {
+    "guest" : false,
+    "admin" : false,
+    "role" : 0,
+    "join_at" : 1716719701,
+    "endpoint" : "oss-us-west-1",
+    "no" : 23110111,
+    "id" : 1163222,
+    "phone" : "",
+    "balance" : 888888,
+    "is_guest" : 0,
+    "avatar" : "https://i.ibb.co/f1cgnGT/IMG-1215.jpg",
+    "city_id" : 0,
+    "group_id" : 1,
+    "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjE5ODY3MjQsInN0YXR1cyI6MSwidXNlcl9pZCI6MTE2MzIyMn0.QcqjpzCLTJ3f1bP_TH1yzxqHqRTCTAOoamnsoyUuOIk",
+    "username" : "Mikephie",
+    "is_admin" : 0,
+    "email" : "mikephiemy@gmail.com",
+    "untried" : false,
+    "vip" : true,
+    "vip_expire" : 3742762088000
   }
 }
 
