65521ccf9461ee6f07e86271a24d314d6eca13e3
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-26 18:20:39 +0800

    Auto-commit

---
+++ qx/test-2.js
@@ -0,0 +1,17 @@
+/*        
+        ➪：脚本名称: 海报制作大师 （永久会员）
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https:\/\/poster\.leminet\.cn\/v01\/login url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/zypg-3.js
+^https:\/\/poster\.leminet\.cn\/v01\/profile url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/zypg-4.js
+
+
+[MITM]
+hostname = poster.leminet.cn
