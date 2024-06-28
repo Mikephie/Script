23d485ca5893c7bcbe6776f2dcf6a1ecceb9f15b
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-28 21:36:15 +0800

    Auto-commit

--- qx/wshb.js
+++ qx/wshb.js
@@ -6,32 +6,13 @@
         𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
 
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+
 [rewrite_local] 
-^https?:\/\/api-poster\.ycase\.cn\/api\/user\/info url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/WS1.js
-^https?:\/\/api-poster\.ycase\.cn\/api\/User\/home url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/WS1.js 
-^https?:\/\/api-poster\.ycase\.cn\/web\/display url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/WS2.js
+^https?:\/\/api-poster\.ycase\.cn\/api\/user\/info url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/wshb1.js
+^https?:\/\/api-poster\.ycase\.cn\/api\/User\/home url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/wshb2.js 
+#^https?:\/\/api-poster\.ycase\.cn\/web\/display url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/WS2.js
+
+
 [MITM]
 hostname = api-poster.ycase.cn
-
-
-*******************************/
-
-
-var mikephie = JSON.parse($response.body);
-
-    mikephie = {
-  "status" : "1",
-  "info" : "成功",
-  "data" : {
-    "sex" : "未知",
-    "phone" : "1",
-    "username" : "Mikephie🎖",
-    "user_id" : "539279",
-    "headimgurl" : "https://i.ibb.co/G2zzXBr/IMG-1275.jpg",
-    "vip_status" : "1",
-    "vip_expiration" : "2088-08-08",
-    "token" : "rk36l7e9ptanK4KkcmmO8UBHmqdLQouH"
-  }
-}
-
-$done({body : JSON.stringify(mikephie)});
