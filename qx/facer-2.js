f2fd393c207d91a3df5eff28ae98da736dcc65c0
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-29 16:40:56 +0800

    Auto-commit

--- qx/facer-2.js
+++ qx/facer-2.js
@@ -20,27 +20,39 @@ hostname = api.facer.io
 var mikephie = JSON.parse($response.body);
 
     mikephie = {
-  "year_total_match" : 9,
-  "hash_version" : 0,
-  "created_at" : "2024-03-08T12:27:47.759205+08:00",
-  "userprofile_id" : "_d371b7f768661f4ccaca1e6eac2f1278",
-  "vip_start_time" : "2024-06-13T17:57:17+08:00",
-  "year_reset_time" : "2088-08-08T08:08:08+08:00",
-  "token" : "aaff0c12a53257b29d6e1cbab698447672b7d846",
-  "year_total_match_max" : 88888,
-  "updated_at" : "2024-06-13T18:05:56.543849+08:00",
-  "app_account_token" : "a4c5606d-15ab-49a8-b1c6-33c918ffcbc0",
-  "week_total_match" : 33,
-  "week_reset_time" : "2024-06-14T17:00:02.800985+08:00",
-  "is_need_refresh" : false,
-  "is_vip" : true,
-  "user" : {
-    "username" : "Mikephie",
-    "id" : 1265,
-    "email" : "",
-    "is_active" : true
-  },
-  "vip_end_time" : "2088-08-08T08:08:08+08:00" 
+  "result" : {
+    "appleId" : "000213.8fc1454c279c424b85965abf30adce0c.1143",
+    "createdAt" : "2024-06-27T11:43:49.756Z",
+    "updatedAt" : "2024-06-29T08:06:12.282Z",
+    "file" : {
+      "__type" : "File",
+      "name" : "30bf8d4c173349f99b90cf4c2e579a0e_avatar.jpg",
+      "url" : "https://cdn.facer.io/original/30bf8d4c173349f99b90cf4c2e579a0e_avatar.jpg"
+    },
+    "lastLoginType" : "apple",
+    "fileName" : "IMG_1287.jpeg",
+    "__type" : "Object",
+    "className" : "_User",
+    "rolesArray" : [
+      "All Access"
+    ],
+    "followingCount" : 3,
+    "username" : "mikephiemy@gmail.com",
+    "ACL" : {
+      "HMEOUskSHv" : {
+        "write" : true,
+        "read" : true
+      }
+    },
+    "objectId" : "HMEOUskSHv",
+    "watchfaces" : {
+      "__type" : "Relation",
+      "className" : "Watchface"
+    },
+    "email" : "mikephiemy@gmail.com",
+    "displayName" : "mikephiemy",
+    "ssoOnly" : true
+  }
 }
  
 $done({body : JSON.stringify(mikephie)});
