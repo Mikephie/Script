a29b65748ed3b772435aaecb822318761b79253a
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-06-29 16:26:43 +0800

    Auto-commit

--- qx/test.js
+++ qx/test.js
@@ -1,5 +1,5 @@
 /*        
-        ➪：脚本名称: 海报制作大师 （永久会员）
+        ➪：脚本名称: 微商海报大师 （永久会员）
 
         ★：解锁永久🆅🅸🅿
 
@@ -7,11 +7,33 @@
 
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
-[rewrite_local] 
-
-^https:\/\/poster\.leminet\.cn\/v01\/login url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/zypg-3.js
-^https:\/\/poster\.leminet\.cn\/v01\/profile url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/zypg-4.js
-
+[rewrite_local]
+^https?:\/\/api-poster\.ycase\.cn\/web\/display url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/WS2.js
 
 [MITM]
-hostname = poster.leminet.cn
+hostname = api-poster.ycase.cn
+
+
+*******************************/
+
+
+var mikephie = JSON.parse($response.body);
+
+    mikephie = {
+  "success" : true,
+  "data" : {
+    "guest" : false,
+    "admin" : false,
+    "id" : 1770111,
+    "role" : 0,
+    "join_at" : 1716719701,
+    "username" : "Mikephie",
+    "endpoint" : "oss-us-west-1",
+    "no" : 23110111,
+    "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbmRwb2ludElkIjoxLCJleHAiOjE3MjE5ODIwNTYsInJvbGUiOjAsInVpZCI6MTc3MDExMX0.fuOgfmp7T_liHQq_6iWYdENuYJLSX5VxHlts6t7GHbo",
+    "vip" : true,
+    "vip_expire" : 3742762088000
+  }
+}
+
+$done({body : JSON.stringify(mikephie)});
