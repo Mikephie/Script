a3657caae35f7fa31d831428595b924b394f5bac
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-02 12:53:15 +0800

    Auto-commit

--- qx/mrevenue.js
+++ qx/mrevenue.js
@@ -59,7 +59,7 @@ if (typeof $response == "undefined") {
     'Yosum': { name: 'Premium', id: 'yosum_999_1year' },  //Yosum
     'iplayTV': { name: 'com.ll.btplayer.12', id: 'com.ll.btplayer.12' },  //ntplayer
     'TQBrowser': { name: 'pro_lt', id: 'com.tk.client.lifetime' },  //Teak浏览器
-    'TQBrowser': { name: 'pro_lt', id: 'mf_20_1m_0_0' },  //Teak浏览器
+    'Mizframe': { name: 'premium', id: 'mf_20_lifetime2' },  //Mizframe
     
   };
   const data = {
