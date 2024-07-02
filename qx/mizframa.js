5bf77993e7b3a51b09e6824af9d372889d010cf7
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-02 13:16:10 +0800

    up

--- qx/mizframa.js
+++ qx/mizframa.js
@@ -10,6 +10,8 @@
 [rewrite_local]
 ^https:\/\/api\.revenuecat\.com\/v1\/receipts url script-request-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/mizframaq.js
 ^https:\/\/api\.revenuecat\.com\/v1\/receipts url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/mizframap.js
+^https:\/\/api\.revenuecat\.com\/v1\/subscribers\/$RCAnonymousID%3Ae277bf4d6ff0448382718ec6c5476105 url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/mizframar.js
+
 
 [mitm]
 hostname = api.revenuecat.com
