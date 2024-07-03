a2a68d1d68ac598e0326c977d37bab3c327fc629
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-03 19:16:00 +0800

    Auto-commit

--- qx/mrevenue.js
+++ qx/mrevenue.js
@@ -4,7 +4,7 @@ Revenuecat解锁合集
 
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/mrevenue.js
-//^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/mrevenue.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/mrevenue.js
 
 [MITM]
 hostname = api.revenuecat.com
