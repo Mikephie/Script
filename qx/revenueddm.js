aaba7e654d55b8ce92dbeca6db4b332a632523ea
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-04 10:57:54 +0800

    up

--- qx/revenueddm.js
+++ qx/revenueddm.js
@@ -3,8 +3,8 @@ xxx
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/revenueddm.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/revenueddm.js
 
 [mitm]
 hostname = api.revenuecat.com
@@ -28,19 +28,18 @@ if (forbiddenAppFound) {
 
 //识别bundle_id
 const bundle = {
-  'io.innerpeace.yiye': { name: 'Premium', id: 'io.innerpeace.yiye.lifetime.forYearly', cm: 'sja' },  //言外笔记
-  'com.skysoft.removalfree': { name: 'Pro', id: 'com.skysoft.removalfree.subscription.newyearly', cm: 'sja' }  //图片消除
+  'io.innerpeace.yiye': { name: 'Premium', id: 'io.innerpeace.yiye.lifetime.forYearly' },  //言外笔记
+  'com.skysoft.removalfree': { name: 'Pro', id: 'com.skysoft.removalfree.subscription.newyearly' }  //图片消除
 }
 
 //识别UA
 const list = {
-  'PwDrawingPad': { name: 'pro', id: 'com.s132.app.supaintexchange.year', cm: 'sja' },  //全能画板2
-  'OneGrow': { name: 'pro', id: 'com.onenicetech.OneGrow.Lifetime', cm: 'sjb' },  //OneGrow-儿童身高成长测量
-  '%E6%97%B6%E9%97%B4%E8%AE%B0%E5%BD%95': { name: 'pro', id: 'com.bapaws.Hours.lifetime', cm: 'sjb' },  //时间记录
-  'PianoTrainer': { name: 'pro_subscription', id: 'pianotrainer.sub.yearly.pro', cm: 'sja' },  //Pianolytics-学习钢琴
-  'FretTrainer': { name: 'pro_subscription', id: 'frettrainer.sub.yearly.pro', cm: 'sja' },  //Fretonomy-学习指板
-  'Currency': { name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.plus', cm: 'sja' },  //Currency-汇率查询
-
+  'PwDrawingPad': { name: 'pro', id: 'com.s132.app.supaintexchange.year' },  //全能画板2
+  'OneGrow': { name: 'pro', id: 'com.onenicetech.OneGrow.Lifetime' },  //OneGrow-儿童身高成长测量
+  '%E6%97%B6%E9%97%B4%E8%AE%B0%E5%BD%95': { name: 'pro', id: 'com.bapaws.Hours.lifetime' },  //时间记录
+  'PianoTrainer': { name: 'pro_subscription', id: 'pianotrainer.sub.yearly.pro' },  //Pianolytics-学习钢琴
+  'mizframa': { name: 'premium', id: 'mf_20_lifetime2' },  //Mizframa
+  'Currency': { name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.plus' },  //Currency-汇率查询
 };
 
 if (typeof $response == "undefined") {
@@ -50,31 +49,35 @@ if (typeof $response == "undefined") {
 } else if (chxm1023 && chxm1023.subscriber) {
   chxm1023.subscriber.subscriptions = chxm1023.subscriber.subscriptions || {};
   chxm1023.subscriber.entitlements = chxm1023.subscriber.entitlements || {};
-  let name,nameb,ids,idb,data;
+  let name, ids, data;
   for (const src of [list, bundle]) {
     for (const i in src) {
       const test = src === list ? ua : bundle_id;
       if (new RegExp(`^${i}`, `i`).test(test)) {
-      if (src[i].cm.indexOf('sja') != -1) { data = {  "purchase_date" : "2023-09-09T09:09:09Z",  "expires_date" : "2099-09-09T09:09:09Z" };  } else if (src[i].cm.indexOf('sjb') != -1) { data = {  "purchase_date" : "2023-09-09T09:09:09Z" }; }
-      ids = src[i].id;name = src[i].name;idb = src[i].idb;nameb = src[i].nameb;
-      break;
+        data = { "purchase_date": "2023-09-09T09:09:09Z", "expires_date": "2099-09-09T09:09:09Z" };
+        ids = src[i].id;
+        name = src[i].name;
+        break;
       }
     }
   }
   if (!name || !ids) {
-    data = {  "purchase_date" : "2023-09-09T09:09:09Z",  "expires_date" : "2099-09-09T09:09:09Z" };
+    data = { "purchase_date": "2023-09-09T09:09:09Z", "expires_date": "2099-09-09T09:09:09Z" };
     name = 'pro';
     ids = 'com.chxm1023.pro';
   }
   chxm1023.subscriber.entitlements[name] = Object.assign({}, data, { product_identifier: ids });
-  if (typeof nameb !== 'undefined' && nameb !== null) {
-    chxm1023.subscriber.entitlements[nameb] = Object.assign({}, data, { product_identifier: idb });
-  }
-  const subData = Object.assign({},data,{  "Author": "chxm1023",  "Telegram": "https://t.me/chxm1023",  "warning": "仅供学习，禁止转载或售卖",  "original_purchase_date": "2023-09-09T09:09:09Z",  "store_transaction_id" : "4900066666666666",  "period_type" : "trial",  "store": "app_store",  "ownership_type": "PURCHASED"  });
+  const subData = Object.assign({}, data, {
+    "Author": "chxm1023",
+    "Telegram": "https://t.me/chxm1023",
+    "warning": "仅供学习，禁止转载或售卖",
+    "original_purchase_date": "2023-09-09T09:09:09Z",
+    "store_transaction_id": "4900066666666666",
+    "period_type": "trial",
+    "store": "app_store",
+    "ownership_type": "PURCHASED"
+  });
   chxm1023.subscriber.subscriptions[ids] = subData;
-  if (typeof idb !== 'undefined' && idb !== null) {
-    chxm1023.subscriber.subscriptions[idb] = subData;
-  }
   chxm1024.body = JSON.stringify(chxm1023);
   console.log('已操作成功🎉🎉🎉\n叮当猫の分享频道: https://t.me/chxm1023');
 }
