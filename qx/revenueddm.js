301afcf4f397be0dc4cc391e636c8ebe463f79c5
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-04 12:53:37 +0800

    Auto-commit

--- qx/revenueddm.js
+++ qx/revenueddm.js
@@ -28,18 +28,25 @@ if (forbiddenAppFound) {
 
 //识别bundle_id
 const bundle = {
-  'io.innerpeace.yiye': { name: 'Premium', id: 'io.innerpeace.yiye.lifetime.forYearly' },  //言外笔记
-  'com.skysoft.removalfree': { name: 'Pro', id: 'com.skysoft.removalfree.subscription.newyearly' }  //图片消除
-}
-
-//识别UA
-const list = {
-  'PwDrawingPad': { name: 'pro', id: 'com.s132.app.supaintexchange.year' },  //全能画板2
-  'OneGrow': { name: 'pro', id: 'com.onenicetech.OneGrow.Lifetime' },  //OneGrow-儿童身高成长测量
-  '%E6%97%B6%E9%97%B4%E8%AE%B0%E5%BD%95': { name: 'pro', id: 'com.bapaws.Hours.lifetime' },  //时间记录
-  'PianoTrainer': { name: 'pro_subscription', id: 'pianotrainer.sub.yearly.pro' },  //Pianolytics-学习钢琴
-  'FretTrainer': { name: 'pro_subscription', id: 'frettrainer.sub.yearly.pro' },  //Fretonomy-学习指板
-  'Currency': { name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.plus' },  //Currency-汇率查询
+    'ShellBean': { name: 'pro', id: 'com.ningle.shellbean.iap.forever' },  //服务器监控
+    'CountDuck': { name: 'premium', id: 'Lifetime' },  //倒数鸭
+    'ScreenRecordCase': { name: 'Premium', id: 'me.fandong.ScreenRecordCase.Ultra' },  //手机壳套图
+    'Currency': { name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.plus' },  //汇率转换
+    'Spark': { name: 'premium', id: 'spark_5999_1y_1w0' },  //邮箱
+    'ShellBoxKit': { name: 'pro', id: 'ShellBoxKit.Lifetime' },  //服务器监控
+    'IDM': { name: 'premium', id: 'sub_yearly_idm' },  //IDM-下载
+    'Whisper': { name: 'all_features', id: 'whisperai_80_y' },  //Whisper AI
+    'PhotoRoom': { name: 'business', id: 'com.background.business.yearly' },  //图像编辑
+    'TouchRetouchBasic': { name: 'premium', id: 'tr5_yearlysubsc_20dlrs_1' },  //图像移除
+    'Drops': { name: 'premium', id: 'forever_unlimited_time_discounted_80_int' },  //Drops外语
+    'UTC': { name: 'Entitlement.Pro', id: 'tech.miidii.MDClock.subscription.month' },  //花样文字
+    'EraseIt': { name: 'ProVersionLifeTime', id: 'com.uzero.cn.eraseit.premium1.fromyear' },  //Smoothrase-AI擦除照片 
+    'DHWaterMarkManager': { name: 'Vip', id: 'lifetimeVIP_001' },  //水印熊
+    '%E8%B5%84%E6%BA%90%E6%90%AC%E8%BF%90%E5%A4%A7%E5%B8%88': { name: 'SaveTikYoutu_common', id: 'LifetimeSubscription' },  //资源搬运大师
+    'Yosum': { name: 'Premium', id: 'yosum_999_1year' },  //Yosum
+    'iplayTV': { name: 'com.ll.btplayer.12', id: 'com.ll.btplayer.12' },  //ntplayer
+    'TQBrowser': { name: 'pro_lt', id: 'com.tk.client.lifetime' },  //Teak浏览器
+    'mizframa': { name: 'premium', id: 'mf_20_1m_0_0' },  //Mizframe
 };
 
 if (typeof $response == "undefined") {
