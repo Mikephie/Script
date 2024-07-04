ecb58baf43ce0fa4a6ec831187cd5f6ce2967379
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-04 12:48:20 +0800

    Auto-commit

--- qx/revenueddm.js
+++ qx/revenueddm.js
@@ -12,9 +12,9 @@ hostname = api.revenuecat.com
 *************************************/
 
 
-const chxm1024 = {};
+const mikephie76 = {};
 const headers = $request.headers;
-const chxm1023 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const mikephie = JSON.parse(typeof $response != "undefined" && $response.body || null);
 const ua = headers['User-Agent'] || headers['user-agent'];
 const bundle_id = headers['X-Client-Bundle-ID'] || headers['x-client-bundle-id'];
 
@@ -22,7 +22,7 @@ const bundle_id = headers['X-Client-Bundle-ID'] || headers['x-client-bundle-id']
 const forbiddenApps = ['Fileball', 'APTV'];
 const forbiddenAppFound = forbiddenApps.find(appName => (ua && ua.includes(appName)) || ($request.body && $request.body.includes(appName)));
 if (forbiddenAppFound) {
-  console.log(`发现禁止MITM的APP: ${forbiddenAppFound}，已停止运行脚本！\n叮当猫の分享频道: https://t.me/chxm1023`);
+  console.log(`发现禁止MITM的APP: ${forbiddenAppFound}，已停止运行脚本！\nMikephieの分享频道: https://t.me/mikephie`);
   $done({});
 }
 
@@ -38,17 +38,17 @@ const list = {
   'OneGrow': { name: 'pro', id: 'com.onenicetech.OneGrow.Lifetime' },  //OneGrow-儿童身高成长测量
   '%E6%97%B6%E9%97%B4%E8%AE%B0%E5%BD%95': { name: 'pro', id: 'com.bapaws.Hours.lifetime' },  //时间记录
   'PianoTrainer': { name: 'pro_subscription', id: 'pianotrainer.sub.yearly.pro' },  //Pianolytics-学习钢琴
-  'mizframa': { name: 'premium', id: 'mf_20_1m_0_0' },  //Mizframa
+  'FretTrainer': { name: 'pro_subscription', id: 'frettrainer.sub.yearly.pro' },  //Fretonomy-学习指板
   'Currency': { name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.plus' },  //Currency-汇率查询
 };
 
 if (typeof $response == "undefined") {
   delete headers["x-revenuecat-etag"];
   delete headers["X-RevenueCat-ETag"];
-  chxm1024.headers = headers;
-} else if (chxm1023 && chxm1023.subscriber) {
-  chxm1023.subscriber.subscriptions = chxm1023.subscriber.subscriptions || {};
-  chxm1023.subscriber.entitlements = chxm1023.subscriber.entitlements || {};
+  mikephie76.headers = headers;
+} else if (mikephie && mikephie.subscriber) {
+  mikephie.subscriber.subscriptions = mikephie.subscriber.subscriptions || {};
+  mikephie.subscriber.entitlements = mikephie.subscriber.entitlements || {};
   let name, ids, data;
   for (const src of [list, bundle]) {
     for (const i in src) {
@@ -64,12 +64,12 @@ if (typeof $response == "undefined") {
   if (!name || !ids) {
     data = { "purchase_date": "2023-09-09T09:09:09Z", "expires_date": "2099-09-09T09:09:09Z" };
     name = 'pro';
-    ids = 'com.chxm1023.pro';
+    ids = 'com.mikephie.pro';
   }
-  chxm1023.subscriber.entitlements[name] = Object.assign({}, data, { product_identifier: ids });
+  mikephie.subscriber.entitlements[name] = Object.assign({}, data, { product_identifier: ids });
   const subData = Object.assign({}, data, {
-    "Author": "chxm1023",
-    "Telegram": "https://t.me/chxm1023",
+    "Author": "mikephie",
+    "Telegram": "https://t.me/mikephie",
     "warning": "仅供学习，禁止转载或售卖",
     "original_purchase_date": "2023-09-09T09:09:09Z",
     "store_transaction_id": "4900066666666666",
@@ -77,9 +77,9 @@ if (typeof $response == "undefined") {
     "store": "app_store",
     "ownership_type": "PURCHASED"
   });
-  chxm1023.subscriber.subscriptions[ids] = subData;
-  chxm1024.body = JSON.stringify(chxm1023);
-  console.log('已操作成功🎉🎉🎉\n叮当猫の分享频道: https://t.me/chxm1023');
+  mikephie.subscriber.subscriptions[ids] = subData;
+  mikephie76.body = JSON.stringify(mikephie);
+  console.log('已操作成功🎉🎉🎉\nMikephieの分享频道: https://t.me/mikephie');
 }
 
-$done(chxm1024);
+$done(mikephie76);
