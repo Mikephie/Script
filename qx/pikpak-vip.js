babe30d29db2876ffa35bd68bbb4d569428b6304
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-05 22:15:32 +0800

    up

--- qx/pikpak-vip.js
+++ qx/pikpak-vip.js
@@ -1,14 +1,11 @@
-/******************************
-脚本功能：PikPak（永久会员）
-软件版本：1.1.0 
-下载地址： 
-脚本作者：
-更新时间：2024年
-电报频道：https://t.me/GieGie777
-问题反馈：
-使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
+/*        
+        ➪：脚本名称: PikPak （永久会员）
 
-*******************************
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
 [rewrite_local]
 ^https:\/\/api-drive\.mypikpak\.com\/vip\/v1\/vip\/info url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/pikpak-vip.js
