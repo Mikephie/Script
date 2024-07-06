8b2efe4fdee03a0fd6dde1f478c1ab2650595dcf
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-06 08:30:15 +0800

    up

--- qx/ccc.js
+++ qx/ccc.js
@@ -14,33 +14,33 @@ hostname = api-drive.mypikpak.com
 var mikephie = JSON.parse($response.body);
 
     mikephie = {
-  "success" : true,
-  "result" : {
-    "headImg" : "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
-    "dataId" : "00000000000000000000",
-    "appleUserEmail" : "mikephiemy@gmail.com",
-    "wordage" : 7777777,
-    "mobile" : "Mikephie",
-    "inviteCode" : "000000",
-    "vipGroupInfos" : [
-      {
-        "groupType" : "TYPE_ONE",
-        "vipType" : "VIP",
-        "autoPay" : "NO"
-      }
-    ],
-    "type" : "VIP", 
-    "vipExpireTime" : "2088-08-08 08:08:08",
-    "vipExpireDays" : 99999999,
-    "registerTime" : "2022-09-09 03:20:32",
-    "nickname" : "Mikephie",
-    "email" : "mikephiemy@gmail.com",
-    "remainTimeSeconds" : 99999,
-    "realnameStatus" : "NO",
-    "times" : 77777777
-  },
-  "returnCode" : "200",
-  "timeOut" : false
+  "space": [
+  ],
+  "has_more": false,
+  "base": {
+    "sub_status": true,
+    "assets": "10T",
+    "upload": {
+      "assets": 0,
+      "total_assets": 0,
+      "size": 0
+    },
+    "expire_time": "2024-11-29T10:58:16+08:00",
+    "download": {
+      "assets": 0,
+      "total_assets": 0,
+      "size": 0
+    },
+    "size": 10995116277760,
+    "vip_status": "ok",
+    "user_id": "ZUNcGmFMxgvtLSD8",
+    "offline": {
+      "assets": 0,
+      "total_assets": 0,
+      "size": 0
+    },
+    "info": ""
+  }
 }
 
 $done({body : JSON.stringify(mikephie)});
