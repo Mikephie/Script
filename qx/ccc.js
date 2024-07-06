0ec5e08266715cfa746834d7c26cbbaf4e006a3b
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-06 08:29:36 +0800

    Auto-commit

--- qx/ccc.js
+++ qx/ccc.js
@@ -9,9 +9,38 @@
 [mitm] 
 hostname = api-drive.mypikpak.com
 
-#"expire": "2088-08-08T08:08:08+08:00",
-        "type": "regional",
-        "description": "区域会员",
-        "surplus_day": 88888
-
 */
+
+var mikephie = JSON.parse($response.body);
+
+    mikephie = {
+  "success" : true,
+  "result" : {
+    "headImg" : "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
+    "dataId" : "00000000000000000000",
+    "appleUserEmail" : "mikephiemy@gmail.com",
+    "wordage" : 7777777,
+    "mobile" : "Mikephie",
+    "inviteCode" : "000000",
+    "vipGroupInfos" : [
+      {
+        "groupType" : "TYPE_ONE",
+        "vipType" : "VIP",
+        "autoPay" : "NO"
+      }
+    ],
+    "type" : "VIP", 
+    "vipExpireTime" : "2088-08-08 08:08:08",
+    "vipExpireDays" : 99999999,
+    "registerTime" : "2022-09-09 03:20:32",
+    "nickname" : "Mikephie",
+    "email" : "mikephiemy@gmail.com",
+    "remainTimeSeconds" : 99999,
+    "realnameStatus" : "NO",
+    "times" : 77777777
+  },
+  "returnCode" : "200",
+  "timeOut" : false
+}
+
+$done({body : JSON.stringify(mikephie)});
