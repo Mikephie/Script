716437f095e19a08d0e0a9f7110cb091b6f520a5
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-06 20:48:04 +0800

    Auto-commit

---
+++ qx/pikpak-vip-2.js
@@ -0,0 +1,44 @@
+/*        
+        ➪：脚本名称: PikPak （永久会员）
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local]
+^https:\/\/api-drive\.mypikpak\.com\/vip\/v1\/vip\/info url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/pikpak-vip.js
+
+[mitm] 
+hostname = api-drive.mypikpak.com
+
+*******************************/
+
+
+var mikephie = JSON.parse($response.body);
+
+    mikephie = {
+  "status" : "ACTIVE",
+  "password" : "SET",
+  "sub" : "ZUNcGmFMxgvtLSD8",
+  "created_at" : "2023-11-02T08:21:46.572570191Z",
+  "email" : "4pbhqkfbzx@privaterelay.appleid.com",
+  "providers" : [
+    {
+      "id" : "apple.com",
+      "provider_user_id" : "000213.d5b3683911654463854ae0ebab8c11fa.0821",
+      "name" : "MikephieMY"
+    },
+    {
+      "id" : "telegram.com",
+      "provider_user_id" : "812526739",
+      "name" : "ⓂⓘⓚⓔⓅⓗⓘⓔ"
+    }
+  ],
+  "phone_number" : "+6587***88",
+  "name" : "MikephieMY",
+  "password_updated_at" : "2023-11-23T06:50:56.310544839Z"
+}
+
+$done({body : JSON.stringify(mikephie)});
