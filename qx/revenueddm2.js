2b3832f997d63dac9c724b8ab15cae40f849307c
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-08 15:10:07 +0800

    u

--- qx/revenueddm2.js
+++ qx/revenueddm2.js
@@ -5,8 +5,8 @@ Revenuecat解锁合集
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/revenueddm1.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/revenueddm1.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/revenueddm2.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/revenueddm2.js
 
 [mitm]
 hostname = api.revenuecat.com
