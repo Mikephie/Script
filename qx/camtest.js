ac35e17f8963c8174d3521bb60c9363356262dc9
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-09 20:52:00 +0800

    u

--- qx/camtest.js
+++ qx/camtest.js
@@ -5,7 +5,7 @@ CamScanner 解锁部分高级特权
 Quantumult X:
 
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/camscanner.js
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/camtest.js
 
 [mitm]
 hostname = ap*.intsig.net
