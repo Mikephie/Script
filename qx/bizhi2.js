17c7e7fb4b2bddc2285c4c5507e4aa00230c2515
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-11 20:01:44 +0800

    Auto-commit

--- qx/bizhi2.js
+++ qx/bizhi2.js
@@ -17,8 +17,8 @@ var body = $response.body;
 
 body = body.replace(/"isSVIP" : 0, '"isSVIP" : 1');
 body = body.replace(/"isVIP" : 0, '"isVIP" : 1');
-body = body.replace(/"youOwn":false/g,
-'"youOwn":true');
+body = body.replace(/"vipEndTimeFormat" : "2024-08-11",
+'"vipEndTimeFormat" : "2088-08-08",');
 body = body.replace(/"arcade":false/g,
 '"arcade":true');
 
