94a125f8a7a47ca71184f3429320b004548f1234
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-12 10:09:34 +0800

    Auto-commit

--- qx/flightradar24.js
+++ qx/flightradar24.js
@@ -1,14 +1,11 @@
-> 应用名称：Flightradar24
-> 下载地址：http://t.cn/Air8PM5k
-> 脚本作者：@Mikephie
-> 解锁说明：去广告+去水印
-> 更新时间：17 Feb 2024 at 18:13
-> 特别提醒：如需转载请注明出处，谢谢合作！
-> 重写地址：https://raw.githubusercontent.com/Mikephie/Script/main/Flightradar24.js
-> 特别说明：⚠️⚠️⚠️本脚本仅供学习交流使用，禁止转载售卖⚠️⚠️⚠️
-          
-          
-[rewrite_local]
+/*        
+        ➪：脚本名称: 婚礼精选 （永久会员）
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
 # > Flightradar24@ddgksf2013
 ^https?:\/\/mobile\.flightradar24\.com\/mobile\/(user-session|\w{9}) url script-echo-response https://github.com/ddgksf2013/dev/raw/main/flightradar24.js
@@ -16,3 +13,28 @@
 [mitm]
 hostname = mobile.flightradar24.com
 
+*******************************/
+
+
+var mikephie = JSON.parse($response.body);
+
+    mikephie = {
+  "success" : true,
+  "data" : {
+    "id" : 1163222,
+    "phone" : "",
+    "balance" : 888888,
+    "is_guest" : 0,
+    "avatar" : "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
+    "city_id" : 0,
+    "group_id" : 1,
+    "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjE5ODY3MjQsInN0YXR1cyI6MSwidXNlcl9pZCI6MTE2MzIyMn0.QcqjpzCLTJ3f1bP_TH1yzxqHqRTCTAOoamnsoyUuOIk",
+    "username" : "Mikephie",
+    "is_admin" : 0,
+    "email" : "mikephiemy@gmail.com",
+    "untried" : false,
+    "vip_expire" : 3742762088000
+  }
+}
+
+$done({body : JSON.stringify(mikephie)});
