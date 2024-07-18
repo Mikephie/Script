26ad472a40940c81b67e6b6cd4e26389d4995761
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-19 05:53:22 +0800

    Auto-commit

---
+++ qx/mumu.js
@@ -0,0 +1,15 @@
+const modifyResponse = (response) => {
+  try {
+    let data = JSON.parse(response.body);
+    if (data && data.data && data.data.current_device) {
+      data.data.current_device.trial_end_at = 1742436947;
+      data.data.current_device.trial_status = 1;
+      response.body = JSON.stringify(data);
+    }
+  } catch (e) {
+    console.error("Error modifying response", e);
+  }
+  return response;
+};
+
+$done(modifyResponse($response));
