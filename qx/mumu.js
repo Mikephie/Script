9ba16498806e2fa0fbd790b5fc09b07db3ae7ef2
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-19 05:54:02 +0800

    u

--- qx/mumu.js
+++ qx/mumu.js
@@ -2,7 +2,7 @@ const modifyResponse = (response) => {
   try {
     let data = JSON.parse(response.body);
     if (data && data.data && data.data.current_device) {
-      data.data.current_device.trial_end_at = 1742436947;
+      data.data.current_device.trial_end_at = 2764454888;
       data.data.current_device.trial_status = 1;
       response.body = JSON.stringify(data);
     }
