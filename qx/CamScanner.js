fd2887c6e625679b4def419698b147c92a8e4bb5
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-30 06:02:34 +0800

    u

--- qx/CamScanner.js
+++ qx/CamScanner.js
@@ -26,36 +26,46 @@ const tqzx = '/getPrivilegeItem';
 const vip = {
       "group1_paid" : 1,
       "ms_first_pay" : 0,
-      "vip_type" : "svip",
-      "auto_renewal" : true,
+      "vip_type" : "vip",
+      "auto_renewal" : false,
       "in_trial" : 0,
-      "members_page" : 0,
-      "pc_vip" : 1,
-      "renew_type" : "year",
+      "pc_vip" : 0,
       "renew_method" : "appstore",
-      "ys_first_pay" : 0,
-      "initial_tm" : "4092599349",
-      "product_id" : "com.intsig.camscanner.premiums.oneyear.autorenewable.free.test1",
+      "is_trial_guide" : "1",
+      "ys_first_pay" : 1,
+      "initial_tm" : "1721815096",
       "vip_level_info" : {
         "score" : 9,
         "level" : 1,
-        "next_score" : 600,
+        "create_time" : 1722228783,
         "start_score" : 1,
-        "create_time" : 1722228783
+        "next_score" : 600
       },
-      "nxt_renew_tm" : "4092599349",
-      "last_payment_method" : "appstore",
-      "grade" : 4,
-      "svip" : 1,
-      "expiry" : 4092599349,
-      "pending" : 0,
       "level_info" : {
         "level" : 1,
-        "end_days" : 30,
-        "days" : 14
+        "days" : 14,
+        "end_days" : 30
       },
+      "grade" : 4,
+      "last_payment_method" : "webshop",
+      "nxt_renew_tm" : 0,
+      "svip" : 0,
+      "expiry" : 1722765490,
+      "pending" : 0,
       "inherited_flag" : 0,
       "group2_paid" : 0
+    },
+    "used_points" : "0",
+    "fax_balance" : "0",
+    "login_ocr_balance" : 4,
+    "points" : "0",
+    "pdfword_balance" : "100000",
+    "bookmode_balance" : 100000,
+    "immt_expy_points" : "0",
+    "no_login_ocr_balance" : 2,
+    "ocr_balance" : 991,
+    "server_time" : "1722254811",
+    "CamScanner_RoadMap" : 499
     };
 
 if ($request.url.indexOf(vipa) != -1){
