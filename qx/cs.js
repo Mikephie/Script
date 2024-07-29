8ba1a46493e970706ebd608c8f4f66b140b39a5d
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-29 20:46:23 +0800

    u

---
+++ qx/cs.js
@@ -0,0 +1,21 @@
+/*        
+        ➪：脚本名称: CamScanner （永久会员）
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local]
+
+^https:\/\/api-cs\.intsig\.net\/purchase\/cs\/query_property\?app_type=CamScanner_IP_FREE&client_app=CamScanner_IP_FREE%406\.69\.1\.2407252158&country=sg&cs_ept_d=YgvDZ8SY8LGPoiikDxBTdw8vs13ojhJgyORVGX4X2RkOppl012e79Q8AeopjFre1&first_install_version=6\.50\.0&language=zh-cn&property_id=cs_vip%7Cfax%7Cpoints%7Cocr_count%7CCamScanner_BookMode%7Cpdfword_count%7CCamScanner_RoadMap&sign url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/cs1.js
+
+^https:\/\/api-cs\.intsig\.net\/purchase\/cs\/query_property\?app_type=CamScanner_IP_FREE&client_app=CamScanner_IP_FREE%406\.69\.1\.2407252158&country=sg&cs_ept_d=YgvDZ8SY8LGPoiikDxBTdw8vs13ojhJgyORVGX4X2RkOppl012e79Q8AeopjFre1&first_install_version=6\.50\.0&language=zh-cn&property_id=cs_vip%7Cfax%7Cpoints%7Cgreetcard%7Cpoints_exchange_cfg%7Ctrans_count%7Cocr_count%7Cremovead%7Cupload_pdf%7Cpatting_count%7Cexcel_count%7Cpdfword_count%7Cwatchad_vip%7Cwatermarks_count%7CCamScanner_BookMode%7CCamScanner_ImageRestore%7CCamScanner_DirNum%7CCamScanner_Recolor%7CCamScanner_RoadMap%7CCamScanner_Pdfword%EF%BD%9CCamScanner_Erase%7CCamScanner_Demoire%EF%BD%9CCamScanner_CertMode%7CCamScanner_AIImageFilter%7CCamScanner_Intellect_Erase%7CCamScanner_ADD_Watermarks%7CCamScanner_SharePdf&sign url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/cs2.js
+
+^https:\/\/api-cs\.intsig\.net\/purchase\/cs\/query_gift_log\?country url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/cs3.js
+
+
+[mitm]
+
+hostname = api-cs.intsig.net
--- qx/cs3.js
+++ qx/cs3.js
@@ -20,53 +20,145 @@ hostname = api-cs.intsig.net
 
 var mikephie = JSON.parse($response.body);
 
-    mikephie = {
-  "ret" : "0",
-  "data" : {
-    "psnl_vip_property" : {
-      "group1_paid" : 1,
-      "ms_first_pay" : 0,
-      "vip_type" : "vip",
-      "auto_renewal" : false,
-      "in_trial" : 0,
-      "pc_vip" : 0,
-      "renew_method" : "appstore",
-      "is_trial_guide" : "1",
-      "ys_first_pay" : 1,
-      "initial_tm" : "1721815096",
-      "vip_level_info" : {
-        "score" : 9,
-        "level" : 1,
-        "create_time" : 1722228783,
-        "start_score" : 1,
-        "next_score" : 600
-      },
-      "level_info" : {
-        "level" : 1,
-        "days" : 14,
-        "end_days" : 30
-      },
-      "grade" : 4,
-      "last_payment_method" : "webshop",
-      "nxt_renew_tm" : 0,
-      "svip" : 0,
-      "expiry" : 1722765490,
-      "pending" : 0,
-      "inherited_flag" : 0,
-      "group2_paid" : 0
-    },
-    "used_points" : "0",
-    "fax_balance" : "0",
-    "login_ocr_balance" : 4,
-    "points" : "0",
-    "pdfword_balance" : "100000",
-    "bookmode_balance" : 100000,
-    "immt_expy_points" : "0",
-    "no_login_ocr_balance" : 2,
-    "ocr_balance" : 991,
-    "server_time" : "1722254811",
-    "CamScanner_RoadMap" : 499
+    mikephie = [
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_9",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_10",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_11",
+    "each_add" : "52428800",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_12",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_13",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_14",
+    "each_add" : "209715200",
+    "max" : "100"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_15",
+    "each_add" : "209715200",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_16",
+    "each_add" : "52428800",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_web_1",
+    "each_add" : "209715200",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_web_2",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_2",
+    "each_add" : "209715200",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_24",
+    "each_add" : "209715200",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_25",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_30",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_34",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_35",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_36",
+    "each_add" : "524288000",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_1",
+    "each_add" : "1073741824",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_3",
+    "each_add" : "104857600",
+    "max" : "10"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_4",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_6",
+    "each_add" : "52428800",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_7",
+    "each_add" : "104857600",
+    "max" : "1"
+  },
+  {
+    "done" : "0",
+    "act_id" : "cs_storage_8",
+    "each_add" : "104857600",
+    "max" : "1"
   }
-}
+]
 
 $done({body : JSON.stringify(mikephie)});
