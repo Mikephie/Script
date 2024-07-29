938061c319771a9fee4a8f5878b76631a27e7278
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-07-29 20:39:12 +0800

    Auto-commit

---
+++ qx/cs1.js
@@ -0,0 +1,52 @@
+/*        
+        ➪：脚本名称: CamScanner （永久会员）
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local]
+# >彩豆视频水印宝 – 视频加水印制作视频剪辑（永久会员）
+^https?:\/\/appss.baomingding.com\/app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/cdsp.js
+
+[mitm] 
+hostname = appss.baomingding.com
+
+*******************************/
+
+
+var mikephie = JSON.parse($response.body);
+
+    mikephie = {
+  "success" : true,
+  "result" : {
+    "headImg" : "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
+    "dataId" : "00000000000000000000",
+    "appleUserEmail" : "mikephiemy@gmail.com",
+    "wordage" : 7777777,
+    "mobile" : "Mikephie",
+    "inviteCode" : "000000",
+    "vipGroupInfos" : [
+      {
+        "groupType" : "TYPE_ONE",
+        "vipType" : "VIP",
+        "autoPay" : "NO"
+      }
+    ],
+    "type" : "VIP", 
+    "vipExpireTime" : "2088-08-08 08:08:08",
+    "vipExpireDays" : 99999999,
+    "registerTime" : "2022-09-09 03:20:32",
+    "nickname" : "Mikephie",
+    "email" : "mikephiemy@gmail.com",
+    "remainTimeSeconds" : 99999,
+    "realnameStatus" : "NO",
+    "times" : 77777777
+  },
+  "returnCode" : "200",
+  "timeOut" : false
+}
+
+$done({body : JSON.stringify(mikephie)});
