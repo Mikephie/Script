b1cbedce0e6877291217538b3f6f02b13134c0b3
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-01 21:05:14 +0800

    Auto-commit

--- qx/updf1.js
+++ qx/updf1.js
@@ -43,7 +43,7 @@ var mikephie = JSON.parse($response.body);
         25644,
         25643
       ],
-      "goodsId": 65908,
+      "goodsId": 65909,
       "isRecommend": 0,
       "platformGoodsType": "inapp",
       "subscriptionType": "lifetime"
