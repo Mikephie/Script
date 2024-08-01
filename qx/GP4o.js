267e016f0eb6b6dc9c8bd694b3a0fe1882e2f8e9
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-01 13:00:35 +0800

    Auto-commit

--- qx/GP4o.js
+++ qx/GP4o.js
@@ -17,25 +17,10 @@ hostname = www.lovewith.me
 *******************************/
 
 
-var mikephie = JSON.parse($response.body);
-
-    mikephie = {
-  "success" : true,
-  "data" : {
-    "id" : 1163222,
-    "phone" : "",
-    "balance" : 888888,
-    "is_guest" : 0,
-    "avatar" : "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
-    "city_id" : 0,
-    "group_id" : 1,
-    "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjE5ODY3MjQsInN0YXR1cyI6MSwidXNlcl9pZCI6MTE2MzIyMn0.QcqjpzCLTJ3f1bP_TH1yzxqHqRTCTAOoamnsoyUuOIk",
-    "username" : "Mikephie",
-    "is_admin" : 0,
-    "email" : "mikephiemy@gmail.com",
-    "untried" : false,
-    "vip_expire" : 3742762088000
-  }
+if ($request.url.includes('api/app/user/getCurrentInfo')) {
+    let body = JSON.parse($response.body);
+    Object.assign(body.data, { vipStatus: "1", nickname: "Baby", vipLabel: "1", imgNum: 9999999 });
+    $done({ body: JSON.stringify(body) });
+} else {
+    $done({});
 }
-
-$done({body : JSON.stringify(mikephie)});
