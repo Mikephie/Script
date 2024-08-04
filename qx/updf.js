bdb84f5d2fd6ab4f30fe55fde665d00128917321
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-04 13:22:20 +0800

    u

--- qx/updf.js
+++ qx/updf.js
@@ -1,29 +1,21 @@
-/*        
-        ➪：脚本名称: UPDF （永久会员）
+/*
+^https:\/\/api\.updf\.com\/v1\/user\/planList
 
-        ★：解锁永久🆅🅸🅿
-
-        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
-
-𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
-
-[rewrite_local]
-^https:\/\/api\.updf\.com\/v1\/user\/planList url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/updf1.js
-
-[mitm]
-
-hostname = api.updf.com
+api.updf.com
+*/
 
 
-var mikephie = JSON.parse($response.body);
 
-mikephie = {
+
+let body = JSON.parse($response.body);
+
+function modifyObject(obj) {
     for (let key in obj) {
         if (obj.hasOwnProperty(key)) {
             if (typeof obj[key] === 'object' && obj[key] !== null) {
                 modifyObject(obj[key]);
             } else {
-                if (key === 'expireTime') {
+                if (key === "expireTime") {
                     obj[key] = 3742762088000,
                 }
                 
@@ -35,5 +27,5 @@ mikephie = {
 
 
 modifyObject(body);
- 
-$done({body : JSON.stringify(mikephie)});
+
+$done({ body: JSON.stringify(body) });
