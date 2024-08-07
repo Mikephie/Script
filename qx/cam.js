fd3d4f28af47653df1ae9f4942ba2de64699ff42
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-07 15:57:29 +0800

    Auto-commit

--- qx/cam.js
+++ qx/cam.js
@@ -11,11 +11,11 @@
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
 *******************************
 
-[rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/cam.js
+^https:\/\/v3\.camscanner\.com\/app\/getPrivilegeItem url script-response-body
 
 [mitm]
-hostname = ap*.intsig.net
+
+hostname = v3.camscanner.com
 
 *
 *
