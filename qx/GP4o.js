4c1c879b0cf074c5c096a4e8cac40f1d61c94db4
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-10 20:41:38 +0800

    u

--- qx/GP4o.js
+++ qx/GP4o.js
@@ -8,7 +8,7 @@
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
 [rewrite_local] 
-https://nichousha.sjrjyffs.top/api/img/aiSketch url script-request-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/GTP4ohd.js
+https://nichousha.sjrjyffs.top/api/img/aiSketch url script-request-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/GP4ohd.js
 https://nichousha.sjrjyffs.top/api/app/user/getCurrentInfo url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/GP4o.js
 
 [mitm]
