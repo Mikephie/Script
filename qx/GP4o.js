fe07e2e5cf6a720421341766006e8fe2a7414b18
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-10 08:52:26 +0800

    u

--- qx/GP4o.js
+++ qx/GP4o.js
@@ -20,27 +20,16 @@ hostname = nichousha.sjrjyffs.top
 var mikephie = JSON.parse($response.body);
 
     mikephie = {
-  "year_total_match" : 9,
-  "hash_version" : 0,
-  "created_at" : "2024-03-08T12:27:47.759205+08:00",
-  "userprofile_id" : "_d371b7f768661f4ccaca1e6eac2f1278",
-  "vip_start_time" : "2024-06-13T17:57:17+08:00",
-  "year_reset_time" : "2088-08-08T08:08:08+08:00",
-  "token" : "aaff0c12a53257b29d6e1cbab698447672b7d846",
-  "year_total_match_max" : 88888,
-  "updated_at" : "2024-06-13T18:05:56.543849+08:00",
-  "app_account_token" : "a4c5606d-15ab-49a8-b1c6-33c918ffcbc0",
-  "week_total_match" : 33,
-  "week_reset_time" : "2024-06-14T17:00:02.800985+08:00",
-  "is_need_refresh" : false,
-  "is_vip" : true,
-  "user" : {
-    "username" : "Mikephie",
-    "id" : 1265,
-    "email" : "",
-    "is_active" : true
+  "msg": "ok",
+  "data": {
+    "vipStatus": "1",
+    "id": "1818946794591789058",
+    "vipLabel": "1",
+    "nickname": "尾号tAI_Mikephie",
+    "imgNum": 888888,
+    "avatar": "https://i.ibb.co/wM5z10N/IMG-1287.jpg"
   },
-  "vip_end_time" : "2088-08-08T08:08:08+08:00" 
+  "code": 200
 }
- 
+  
 $done({body : JSON.stringify(mikephie)});
