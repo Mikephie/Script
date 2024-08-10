e9cb38f5c993b99499c0d0db53d9eb75a66bd927
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-10 21:01:01 +0800

    u

--- qx/adblockpro.js
+++ qx/adblockpro.js
@@ -3,7 +3,7 @@
 adblockpro 10.7.5
 
 [rewrite_local]
-^https:\/\/api\.adblockpro\.app\/verify url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/adblockpro.js
+^https:\/\/api\.adblockpro\.app\/verify url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/adblockpro.js
 [mitm]
 hostname = api.adblockpro.app
 */
