96387fe3521c3471a28761bedc86ba90f984d62e
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-15 16:39:24 +0800

    Auto-commit

---
+++ qx/aloha.js
@@ -0,0 +1,30 @@
+/*        
+             ➪：脚本名称:   aloha 
+            
+     ꫛꫀꪝ  ：2024年8月15日 16:37
+
+            ★：解锁永久🆅🅸🅿
+
+           𖣘：🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+ 
+
+[rewrite_local]
+^https:\/\/api\.weilaizhushou\.com\/base\/user\/vip\/getUserVip url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/aloha.js
+
+[mitm]
+hostname = api.weilaizhushou.com
+
+*************************************/
+
+var mikephie = JSON.parse($response.body);
+
+mikephie.data = {
+  ...mikephie.data,
+    "end_of_premium": 3742762088000,
+    "is_premium": true,
+    "_end_of_premium": "2088-08-08 08:08:08",
+};
+
+$done({body : JSON.stringify(mikephie)});
