e6cd42806271f153d340f3fbd1516a9132d66ea4
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-18 15:08:02 +0800

    u

--- qx/iTunes.js
+++ qx/iTunes.js
@@ -60,7 +60,7 @@ var uaProductMapping = {
   'VideoHelper': {product_id: 'vip_service'},  //媒关系
   'qxwp%20copy': {product_id: 'com.chowjoe.wp2free.year.pro'},  //WP壁纸
   'PhimCiaj': {product_id: 'com.jiancent.calligraphymaster.lifetime'},  //WP壁纸
-  'PhimCiaj': {product_id: 'wiseart.ai.ios.week.nofree'},  //WP壁纸
+  'WiseMate': {product_id: 'wiseart.ai.ios.week.nofree'},  //WiseMate AI
   
   
   
