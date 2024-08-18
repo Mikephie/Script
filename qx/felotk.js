1de31c15e3bfb9470d8be6707d4a1995a8e1ef6c
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-18 20:41:45 +0800

    Auto-commit

--- qx/felotk.js
+++ qx/felotk.js
@@ -20,37 +20,9 @@ var mikephie = JSON.parse($response.body);
     mikephie = {
   "status": 200,
   "data": {
-    "now": "2024-08-18T11:57:20+0000",
-    "type": "SUBSCRIPTION",
-    "plan": {
-      "app_id": "translator",
-      "charge_time": "2088-08-08T11:14:50+0000",
-      "subscription_id_third": "300001970562827",
-      "reset_time": "2088-08-08T11:14:50+0000",
-      "count": 1,
-      "subscription_type": "ANNUALLY",
-      "subscribe_time": "2024-08-18T11:14:50+0000",
-      "product_id": "translator_pro_annual_trial",
-      "subscription_channel": "IAP",
-      "reset_period": "ANNUALLY",
-      "subscription_id": "1825129251802910724",
-      "status": "TRIAL",
-      "subscriber": "1823835292198371329"
-    },
-    "user_product_total": [
-      {
-        "freeze": 0,
-        "total_limit": "UNLIMITED",
-        "deduction_type": "DURATIONS",
-        "user_id": "1823835292198371329",
-        "product_item_type": "TRANSLATOR_DURATIONS",
-        "total": 5999999995,
-        "balance": 5999999995,
-        "user_product_total_id": "1823835294030647298"
-      }
-    ]
+    "token": "eyJhbGciOiJFUzI1NiIsImtpZCI6ImtleTEiLCJ0eXAiOiJKV1QifQ.eyJyZWdpb24iOiJqYXBhbmVhc3QiLCJzdWJzY3JpcHRpb24taWQiOiI1OWVkMGQzYjY3ZTI0NzczYmM5YzViY2EyNDA1MmNmMyIsInByb2R1Y3QtaWQiOiJTcGVlY2hTZXJ2aWNlcy5TMCIsImNvZ25pdGl2ZS1zZXJ2aWNlcy1lbmRwb2ludCI6Imh0dHBzOi8vYXBpLmNvZ25pdGl2ZS5taWNyb3NvZnQuY29tL2ludGVybmFsL3YxLjAvIiwiYXp1cmUtcmVzb3VyY2UtaWQiOiIvc3Vic2NyaXB0aW9ucy82Y2JhMWFhYi0wNTRkLTRlNzgtOWY3NS1jZjBhZWUyMjc0ZDAvcmVzb3VyY2VHcm91cHMvdHJhbnNsYXRvci9wcm92aWRlcnMvTWljcm9zb2Z0LkNvZ25pdGl2ZVNlcnZpY2VzL2FjY291bnRzL3RyYW5zbGF0b3ItcHJkIiwic2NvcGUiOiJzcGVlY2hzZXJ2aWNlcyIsImF1ZCI6InVybjptcy5zcGVlY2hzZXJ2aWNlcy5qYXBhbmVhc3QiLCJleHAiOjE3MjM5ODI4MzIsImlzcyI6InVybjptcy5jb2duaXRpdmVzZXJ2aWNlcyJ9.qx3YVEFe2f1OHr11pyJ8qX_LIwIR2tinI4unXWwDV95bzgRgzUZ8ls-658ZWNrcmbBncOCyN_gzDA21Ax8gNfw"
   },
   "code": "OK"
 }
-
+ 
 $done({body: JSON.stringify(mikephie)});
