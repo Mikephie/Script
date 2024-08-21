9557f445f5932deb05966e642b6215a283f47ef1
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-22 07:13:26 +0800

    Auto-commit

--- qx/felotk-r.js
+++ qx/felotk-r.js
@@ -7,16 +7,26 @@
 
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
+
 [rewrite_local]
+
 # >felo实时翻译-同传与语音字幕
+#^https?:\/\/(translator|accounts).felo.me\/api\/user.*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/felo.js
+
 ^https?:\/\/translator.felo.me\/api\/translation\/thirdToken\?token_channel url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/felotk.js
- 
+
 [mitm] 
 hostname = accounts.felo.me,translator.felo.me
+ 
+*
+*
+*/
 
-*******************************/
 
-var modifiedHeaders = $response.headers;
-modifiedHeaders["Authorization"] = "Bearer eyJhbGciOiJFUzI1NiIsImtpZCI6ImtleTEiLCJ0eXAiOiJKV1QifQ.eyJyZWdpb24iOiJqYXBhbmVhc3QiLCJzdWJzY3JpcHRpb24taWQiOiI1OWVkMGQzYjY3ZTI0NzczYmM5YzViY2EyNDA1MmNmMyIsInByb2R1Y3QtaWQiOiJTcGVlY2hTZXJ2aWNlcy5TMCIsImNvZ25pdGl2ZS1zZXJ2aWNlcy1lbmRwb2ludCI6Imh0dHBzOi8vYXBpLmNvZ25pdGl2ZS5taWNyb3NvZnQuY29tL2ludGVybmFsL3YxLjAvIiwiYXp1cmUtcmVzb3VyY2UtaWQiOiIvc3Vic2NyaXB0aW9ucy82Y2JhMWFhYi0wNTRkLTRlNzgtOWY3NS1jZjBhZWUyMjc0ZDAvcmVzb3VyY2VHcm91cHMvdHJhbnNsYXRvci9wcm92aWRlcnMvTWljcm9zb2Z0LkNvZ25pdGl2ZVNlcnZpY2VzL2FjY291bnRzL3RyYW5zbGF0b3ItcHJkIiwic2NvcGUiOiJzcGVlY2hzZXJ2aWNlcyIsImF1ZCI6InVybjptcy5zcGVlY2hzZXJ2aWNlcy5qYXBhbmVhc3QiLCJleHAiOjE3MjM5ODI4MzIsImlzcyI6InVybjptcy5jb2duaXRpdmVzZXJ2aWNlcyJ9.qx3YVEFe2f1OHr11pyJ8qX_LIwIR2tinI4unXWwDV95bzgRgzUZ8ls-658ZWNrcmbBncOCyN_gzDA21Ax8gNfw";
-
-$done({headers: modifiedHeaders});
+let headers = $request.headers;
+headers.cookie = "_clsk=1e3ji69%7C1723606564421%7C1%7C1%7Cw.clarity.ms%2Fcollect; _ga_70F8QP140X=GS1.1.1723606552.4.1.1723606558.0.0.0; _ga=GA1.1.347029008.1723476322; _clck=8lm7fs%7C2%7Cfob%7C0%7C1685";
+headers.authorization = "qq_60cabdacf608480aa95663ab8f0f1ab5__";
+headers.deviceid = "902C611A-A12A-4D48-9DEF-562EF8155908";
+$done({
+  "headers": headers
+});
