737e24fe804d34564321f48594efbac152081ff7
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-23 22:10:43 +0800

    u

--- qx/pikpaktk.js
+++ qx/pikpaktk.js
@@ -1,3 +1,24 @@
+/*        
+        ➪：脚本名称: PikPak （永久会员）
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+*******************************/
+
+[rewrite_local]
+#TK
+^https:\/\/(api-drive|user)\.mypikpak\.com\/(vip\/v1\/(allSubscriptionStatus|vip\/info|space\/list\?type=space)|v1\/user\/me)$ url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/pikpaktk.js
+
+[mitm] 
+hostname = *.mypikpak.com
+
+
+
+
 let obj = JSON.parse($response.body); 
 
   obj.expire = "2088-08-08T08:08:08+08:00";  
