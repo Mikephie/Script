14398fd48eb43afb57dca21e06b647244168aeac
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-23 22:08:46 +0800

    u

--- qx/pikpaktk.js
+++ qx/pikpaktk.js
@@ -1,13 +1,9 @@
 let obj = JSON.parse($response.body); 
 
-  obj.expire = "2024-11-29T10:58:16+08:00";  
+  obj.expire = "2088-08-08T08:08:08+08:00";  
   
-  obj.expire_time = true;
-  obj.teamPro = true;
- obj.activeTeamUser =  true;
-  obj.teamUser = true;
- obj.freetrail = true;
+  obj.expire_time = 2088-08-08T08:08:08+08:00;
+
   
 $done({body: JSON.stringify(obj)});
 
-https:\/\/ticktick\.com\/api\/v2\/user\/status
