6fe513e39d17884e105f6457bc763a56f222743c
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-24 07:23:05 +0800

    Auto-commit

--- qx/pikpak-vipdrive.js
+++ qx/pikpak-vipdrive.js
@@ -25,7 +25,7 @@ var mikephie = JSON.parse($response.body);
   "has_more" : true,
   "base" : {
     "sub_status" : true,
-    "assets" : "20T",
+    "assets" : "15T",
     "upload" : {
       "assets" : 0,
       "total_assets" : 0,
@@ -37,7 +37,7 @@ var mikephie = JSON.parse($response.body);
       "total_assets" : 0,
       "size" : 0
     },
-    "size" : 20995116277760,
+    "size" : 10995116277760,
     "vip_status" : "ok",
     "user_id" : "ZUNcGmFMxgvtLSD8",
     "offline" : {
