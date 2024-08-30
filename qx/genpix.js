0d6dc0244a82aa44d0534d051e6aedec31677f87
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-31 07:24:52 +0900

    Auto-commit

--- qx/genpix.js
+++ qx/genpix.js
@@ -17,7 +17,8 @@ hostname = apiv2.vivipic.com
 var body = $response.body;
 
 body = body.replace(/"ai_credit":d/g, '"ai_credit":8888888888');
-body = body.replace(/"next_billing_time":false/g,'"next_billing_time":true');
+body = body.replace(/"next_billing_time":```regex
+\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/g,'"next_billing_time":true');
 body = body.replace(/"youOwn":false/g,
 '"youOwn":true');
 body = body.replace(/"arcade":false/g,
