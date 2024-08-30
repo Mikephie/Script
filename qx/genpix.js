80d6c8b78a85d18fc1fd1d9252bab23217de2933
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-31 07:25:49 +0900

    Auto-commit

--- qx/genpix.js
+++ qx/genpix.js
@@ -13,18 +13,13 @@ AppRaven
 hostname = apiv2.vivipic.com
 
 **/
-
+2024-09-02T21:56:51.000Z
+2088-08-08 08:08:08.000Z
 var body = $response.body;
 
 body = body.replace(/"ai_credit":d/g, '"ai_credit":8888888888');
 body = body.replace(/"next_billing_time":```regex
-\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/g,'"next_billing_time":true');
-body = body.replace(/"youOwn":false/g,
-'"youOwn":true');
-body = body.replace(/"arcade":false/g,
-'"arcade":true');
+\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/g,'"next_billing_time":2088-08-08 08:08:08.000Z');
 
-body = body.replace(/"preorder":false/g,
-'"preorder":true');
 
 $done({ body });
