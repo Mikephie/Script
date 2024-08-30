52797f30e49804b0e70e4416eb20eadea5e2a08f
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-31 07:27:54 +0900

    u

--- qx/genpix.js
+++ qx/genpix.js
@@ -6,18 +6,16 @@ AppRaven
 [rewrite_local]
 
 
-^https:\/\/apiv2\.vivipic\.com\/get-tx-info url script-response-body
+^https:\/\/apiv2\.vivipic\.com\/get-tx-info url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/genpix.js
 
-[mitm]
-
+[mitm] 
 hostname = apiv2.vivipic.com
 
 **/
-2024-09-02T21:56:51.000Z
-2088-08-08 08:08:08.000Z
+
 var body = $response.body;
 
-body = body.replace(/"ai_credit":d/g, '"ai_credit":8888888888');
+body = body.replace(/"ai_credit":29988/g, '"ai_credit":8888888888');
 body = body.replace(/"next_billing_time":```regex
 \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/g,'"next_billing_time":2088-08-08 08:08:08.000Z');
 
