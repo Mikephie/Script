e80346a980c1b4abe1e7c76183330f38b65982b8
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-08-31 18:38:09 +0900

    u

--- qx/iTunes.js
+++ qx/iTunes.js
@@ -67,7 +67,7 @@ var uaProductMapping = {
   'AllMyBatteries': {product_id: 'AllMyBatteries_Ultimate'},  //AllMyBatteries 电池监控🔋
   'Packet': {product_id: 'com.aaaalab.nepacket.iap.full'},  //  HTTPS抓包
   'EarthSpirit': {product_id: 'Facey_Forever'},  //  Facey-专业彩妆P图神器
-  'com.alphamobiletech.bodyApp': {product_id: 'Facey_Forever'},  //  Facey-专业彩妆P图神器
+  'com.alphamobiletech.bodyApp': {product_id: 'Bodyapp_Forever'},  //  Bodyapp-身材修图软件
 };
 var receipt = {
   "quantity": "1",
