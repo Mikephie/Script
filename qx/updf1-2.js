4f3f186ccf8fd2419d9532117227d4bca7dccea9
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-09-03 08:03:14 +0900

    Auto-commit

--- qx/updf1-2.js
+++ qx/updf1-2.js
@@ -8,7 +8,7 @@
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
 [rewrite_local]
-^https:\/\/api\.updf\.com\/v1\/user\/planList url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/updf1-2.js
+https://apis\.updf\.com/v1/(drive/user/info|ai/chat/usage|appleInnerPay/checkReceipt) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/updf1-2.js
 
 [mitm]
 
