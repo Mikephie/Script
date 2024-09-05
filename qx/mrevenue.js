2028fe3b9d10b63e9ae9b0983b56fc5b3387dad7
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-09-05 17:28:12 +0800

    u

--- qx/mrevenue.js
+++ qx/mrevenue.js
@@ -66,7 +66,7 @@ if (typeof $response == "undefined") {
     'Python3IDE': { name: 'pro', id: 'python3ide_six_month' },  // Python3IDE
     'CallAnnie': { name: 'ai.animato.callannie.entitlement.pro0', id: 'ai.animato.callannie.proyearly1' },  //CallAnnie
     'VideoToLive':{ name: 'Premium', id: 'video_to_live_premium'},  //VideoToLive
-    'ElonAI':{ name: 'Premium', id: 'video_to_live_premium'},  //ElonAI
+    'ElonAI':{ name: 'Premium', id: 'elongpt.yearly_1'},  //ElonAI
   };
   const data = {
     "expires_date": "2088-08-08T08:08:08Z",
