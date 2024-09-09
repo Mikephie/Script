508c6cd43e4dd8782c71dfd67944c7315d3c08db
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-09-09 22:26:21 +0800

    Auto-commit

--- qx/updfdevicelist.js
+++ qx/updfdevicelist.js
@@ -1,3 +1,23 @@
+/*        
+             ➪：脚本名称:   updfdevicelist 
+            
+     ꫛꫀꪝ  ：9 Sep 2024 at 22:24
+
+            ★：解锁永久🆅🅸🅿
+
+           𖣘：🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+ 
+
+[rewrite_local]
+^https:\/\/api\.weilaizhushou\.com\/base\/user\/vip\/getUserVip url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/updfdevicelist.js
+
+[mitm]
+hostname = api.weilaizhushou.com
+
+*************************************/
+
 let headers = $request.headers;
 delete headers['Device-Id'];
 $done({ headers });
