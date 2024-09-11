e1cdf9f59b172b64326a4557353a2ad1da2f73d5
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-09-11 22:44:49 +0800

    Auto-commit

---
+++ qx/aimirror.js
@@ -0,0 +1,32 @@
+/
+let body = $response.body;
+let url = $request.url;
+
+function sendNotification(title, subtitle, message) {
+    if (typeof $notification != 'undefined') {
+        // Surge
+        $notification.post(title, subtitle, message);
+    } else if (typeof $notify != 'undefined') {
+        // Quantumult X
+        $notify(title, subtitle, message);
+    }
+}
+
+if (url.includes("/query_is_vip")) {
+    if (body === 'false') {
+        body = 'true';
+    }
+} else if (url.includes("/draw")) {
+    body = body.replace(/"is_vip"\s*:\s*false/g, '"is_vip":true');
+} else if (url.includes("/users/video_render_count")) {
+    if (body === '0') {
+        body = '这个没解锁别用了';
+        sendNotification("提示", "", "这个视频ai无法解锁，请返回上一个界面");
+    }
+} else if (url.includes("/query_consumable_quota")) {
+    body = body.replace(/"has_quota"\s*:\s*false/g, '"has_quota":true');
+} else if (url.includes("/users/discount")) {
+    body = body.replace(/"discount"\s*:\s*false/g, '"discount":true');
+}
+
+$done({ body });
