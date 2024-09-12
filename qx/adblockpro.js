bc9c48d40c563327326cad5fa1272f3a68af693b
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-09-12 22:28:13 +0800

    Auto-commit

--- qx/adblockpro.js
+++ qx/adblockpro.js
@@ -16,48 +16,25 @@ hostname = api.adblockpro.app
 
 *******************************/
 
-function transformJSON(originalJSON) {
-  // Parse the original JSON string if it's not already an object
-  const original = typeof originalJSON === 'string' ? JSON.parse(originalJSON) : originalJSON;
-
-  // Create the new object with desired modifications
-  const transformed = {
-    p: 1,
-    a: "",
-    i: 1,
-    x: 1,
-    b: original.b,
-    r: "",
-    c: original.c,
-    s: 1,
-    l: 1,
-    t: 1,
-    e: 1,
-    m: 1,
-    f: 1,
-    v: original.v
-  };
-
-  return transformed;
-}
-
-// Example usage:
-const originalData = {
+let jsonObj = {
   "p": 0,
-  "a": "",
-  "i": 1,
-  "x": 1,
-  "b": 274,
-  "r": "",
-  "c": "F8DABB6D",
   "s": 0,
   "l": 0,
-  "t": 1,
+  "t": 0,
   "e": 0,
   "m": 0,
-  "f": 0,
-  "v": true
+  "f": 0
 };
 
-const result = transformJSON(originalData);
-console.log(JSON.stringify(result, null, 2));
+// 修改值
+jsonObj.p = 1;
+jsonObj.s = 1;
+jsonObj.l = 1;
+jsonObj.t = 1;
+jsonObj.e = 1;
+jsonObj.m = 1;
+jsonObj.f = 1;
+
+// 转换为JSON字符串
+let jsonString = JSON.stringify(jsonObj);
+console.log(jsonString);
