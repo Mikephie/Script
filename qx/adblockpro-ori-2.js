9fb83893a3a90a2f947860aed1bb29e3f5ec5c43
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-09-17 07:45:51 +0800

    Auto-commit

---
+++ qx/adblockpro-ori-2.js
@@ -0,0 +1,9 @@
+/*
+
+adblockpro 10.7.5
+
+[rewrite_local]
+^https:\/\/api\.adblockpro\.app\/verify url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/adblockpro.js
+[mitm]
+hostname = api.adblockpro.app
+*/
