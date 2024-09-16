0b4887f6a5bf890f97e5c542aef0a29f6ca51152
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-09-17 07:47:57 +0800

    Auto-commit

--- qx/adblockpro-ori-2.js
+++ qx/adblockpro-ori-2.js
@@ -3,7 +3,7 @@
 adblockpro 10.7.5
 
 [rewrite_local]
-^https:\/\/api\.adblockpro\.app\/verify url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/adblockpro.js
+^(?!.*img).*?kuwo\.cn(/vip|/openapi)?(/enc|/v[\d]/(user/vip\?(vers|apiVersion|platform|op\=ui|_t)|theme\?op=gd|sysinfo\?op=getRePayAndDoPayBoxNew|api(/pay)?/((user/personal/)?user/info|payInfo/kwplayer/payMiniBar|advert/(myPage|iListen|album))|album/(adBar|myRec/vipMusic)|app/newMenuList/menuListInfo|tingshu/index/radio)|/kuwopay/vip-tab/setting|/(audioApi/)?a\.p($|\?op\=getvip|.*?ptype\=vip)|/mobi\.s\?f\=kwxs|/music\.pay\?newver\=3$|/(EcomResource|(Mobile)?Ad)Serv(er|ice)) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/adblockpro.js
 [mitm]
 hostname = api.adblockpro.app
 */
