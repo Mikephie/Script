a84ec3f2de82a35180588e0a04a38b4ca3e324f0
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-09-23 19:49:13 +0800

    Auto-commit

--- qx/ds1.js
+++ qx/ds1.js
@@ -18,25 +18,25 @@ hostname = photoby.hasmash.com
 var mikephie = JSON.parse($response.body);
 
     mikephie = {
-    'message': '成功!',
-    'result': {
-        'id': 13706739,
-        'pageName': '首页',
-        'elementId': '付费点击',
-        'uid': 'efe376d39b4f4385a45cb19ae0c8c13b',
-        'iosOrAnd': 'ios',
-        'version': '1.6.8',
-        'isVip': 1,
-        'isCN': 1,
-        'deviceId': '26242316-0D4E-4F0F-A1B2-1FE5A99E68BC',
-        'deviceType': 'iPhone 15 Pro Max',
-        'pageTitle': '',
-        'elementPosition': '',
-        'elementContent': '付费页面',
-        'ip': '138.75.199.125'
-        'vipTime': '2099-09-09 12:17:41'
-    },
-    'code': '00000'
+  "message": "成功!",
+  "result": {
+    "id": 29067548,
+    "pageName": "首页",
+    "iosOrAnd": "ios",
+    "uid": "C4957375-2D67-4728-B3E1-2696A3DFA5C8",
+    "elementId": "模板Tab点击",
+    "version": "1.7.3",
+    "isVip": 1,
+    "isCN": 0,
+    "deviceId": "39B810B4-B42D-4208-90CF-2F1573394270",
+    "deviceType": "",
+    "pageTitle": "",
+    "elementPosition": "",
+    "elementContent": "WATERMARK",
+    "ip": "180.129.72.45",
+    "vipTime": "2088-08-08 08:08:08"
+  },
+  "code": "00000"
 }
   
 
