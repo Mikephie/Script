4426daf7619beb0501cfb7793371d74f7bd1f203
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-06 20:11:13 +0800

    u

--- qx/dreamface.js
+++ qx/dreamface.js
@@ -24,21 +24,21 @@ mikephie = {
       "renewal": false,
       "vip_type": "TRY_YEAR_PACKAGE",
       "vip_label": true,
-      "vip_remainder_day": 88888888,
-      "expires_date": 3742762088000,
+      "vip_remainder_day": 99999999,
+      "expires_date": 33403938000000,
       "have_trial": false,
-      "expires_date_format": "2088-08-08 08:08:08.000",
+      "expires_date_format": "2099-09-09 00:00:00.000",
       "vip_product_id": "96"
     },
     "device_name": "iPhone17,2",
-    "system_version": "18.0",
+    "system_version": "18.0.1",
     "app_version": "4.6.1",
     "app_package_name": "DreamFace",
     "user_id": "1D71B429-E97D-4232-B94D-AA87804D9C62",
     "device_system": "iOS",
     "country_code": "my",
     "repeat_subscribe": false,
-    "ip_country_code": "cn"
+    "ip_country_code": "sg"
   },
   "status_code": "THS12140000000",
   "extend": {
