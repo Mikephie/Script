370702a79d4f9da4e16e1851a4f924b753821e77
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-06 21:52:27 +0800

    Auto-commit

--- qx/dreamface-2.js
+++ qx/dreamface-2.js
@@ -18,18 +18,32 @@ hostname = dreamfaceapp.com
 
 var mikephie = JSON.parse($response.body);
 
-var Data = {
-  'renewal': false,
-  'vip_type': 'TRY_YEAR_PACKAGE',
-  'vip_label': true,
-  'vip_remainder_day': 88888888,
-  'expires_date': 33403938000000,
-  'have_trial': false,
-  'expires_date_format': '2099-09-09 00:00:00.000',
-  'vip_product_id': '96'
-};
-
-// Merge Data into mikephie or update mikephie as necessary
-Object.assign(mikephie, Data);
+Mikephie = {
+  "data": {
+    "rights": {
+      "renewal": false,
+      "vip_type": "TRY_YEAR_PACKAGE",
+      "vip_label": true,
+      "vip_remainder_day": 99999999,
+      "expires_date": 33403938000000,
+      "have_trial": false,
+      "expires_date_format": "2099-09-09 00:00:00.000",
+      "vip_product_id": "96"
+    },
+    "device_name": "iPhone17,2",
+    "system_version": "18.0.1",
+    "app_version": "4.6.1",
+    "app_package_name": "DreamFace",
+    "user_id": "DB2E50A2-FFAA-4640-B61D-906E8EBF2834",
+    "device_system": "iOS",
+    "country_code": "my",
+    "repeat_subscribe": false,
+    "ip_country_code": "cn"
+  },
+  "status_code": "THS12140000000",
+  "extend": {
+  },
+  "status_msg": "Success"
+}
 
 $done({body: JSON.stringify(mikephie)});
