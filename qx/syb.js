0cb8baeab648a27d683bc834c052e699c5528376
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-10 10:51:23 +0800

    u

--- qx/syb.js
+++ qx/syb.js
@@ -1,12 +1,16 @@
-/*******************************
-脚本功能：水印宝-解锁会员
-脚本作者：afengye
-注意事项：无
-频道地址：https://t.me/afengye
-使用声明：️仅供学习交流, 🈲️商业用途
-********************************
+/*        
+        ➪：脚本名称: 水印宝-解锁会员
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
 [rewrite_local]
-^https:\/\/newappapi\.fntmob\.com\/api\/v1\/qsy\/user-info(.?)+ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/syb.js
+^https:\/\/newappapi\.fntmob\.com\/api\/v1\/qsy\/user-info(.?)+ url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/syb.js
+
+
 [mitm] 
 hostname = newappapi.fntmob.com
 *******************************/
@@ -15,6 +19,8 @@ let obj =  JSON.parse($response.body);
 
 obj.data = {
     ...obj.data,
+    "avatar": "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
+    "nickname": "Mikephie",
     "is_vip" : 1,
     "level_expire" : 32472115200
 }
