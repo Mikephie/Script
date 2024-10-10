a7dcf85a410b3e957ec1a851d42c11175eb6a1e1
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-10 17:21:01 +0800

    u

---
+++ qx/shunlink.js
@@ -0,0 +1,10 @@
+瞬连vpn无限注册白嫖节点定时任务，无需安装APP，支持surge和loon，quantumults
+
+订阅链接:https://sl.eric.vpn/query/sub
+/*
+[rewrite_local]
+https:\/\/sl\.eric\.vpn url script-analyze-echo-response https://raw.githubusercontent.com/Alex0510/Eric/refs/heads/master/surge/Script/SLVPN.js
+
+[MITM]
+hostname = sl.eric.vpn
+*/
