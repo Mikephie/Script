95e2b01a4808ccbd1801e83267f9e781fab0e9be
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-11 14:38:33 +0800

    u

--- qx/mrevenue.js
+++ qx/mrevenue.js
@@ -34,7 +34,7 @@ const bundle = {
 const list = {
   'ShellBean': { name: 'pro', id: 'com.ningle.shellbean.iap.forever', cm: 'sjb' },  //ShellBean-SSH终端服/Linux监控
   'ChatLLM': { name: 'Pro', id: 'com.curiouscreatorsco.ChatLLM.pro.lifetime.notrial.150_00', cm: 'sjb' },  //AItText
-  //'ShellBean': { name: 'pro', id: 'com.ningle.shellbean.subscription.year', cm: 'sja' },  //ShellBean-SSH终端服/Linux监控
+  'Pixelmator': { name: 'pixelmator_photo_pro_access', id: 'pixelmator_photo_lifetime_v1', cm: 'sjb' },  //ShellBean-SSH终端服/Linux监控
   'CountDuck': { name: 'premium', id: 'Lifetime', cm: 'sjb' },  // 倒数鸭
   'ScreenRecordCase': { name: 'Premium', id: 'me.fandong.ScreenRecordCase.Ultra', cm: 'sjb' },  // 手机壳套图
   'Currency': { name: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.plus', cm: 'sja' },  // 汇率转换
