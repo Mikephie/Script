7b0a2d252a361a1111a950d58a923cd50b57292b
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-18 06:04:10 +0800

    Auto-commit

--- qx/chatonai.js
+++ qx/chatonai.js
@@ -16,34 +16,16 @@ hostname = billing.chaton.ai
 
 var mikephie = JSON.parse($response.body);
 
-    mikephie = {
-  "success" : true,
-  "result" : {
-    "headImg" : "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
-    "dataId" : "00000000000000000000",
-    "appleUserEmail" : "mikephiemy@gmail.com",
-    "wordage" : 7777777,
-    "mobile" : "Mikephie",
-    "inviteCode" : "000000",
-    "vipGroupInfos" : [
-      {
-        "groupType" : "TYPE_ONE",
-        "vipType" : "VIP",
-        "autoPay" : "NO"
-      }
-    ],
-    "type" : "VIP", 
-    "vipExpireTime" : "2088-08-08 08:08:08",
-    "vipExpireDays" : 99999999,
-    "registerTime" : "2022-09-09 03:20:32",
-    "nickname" : "Mikephie",
-    "email" : "mikephiemy@gmail.com",
-    "remainTimeSeconds" : 99999,
-    "realnameStatus" : "NO",
-    "times" : 77777777
-  },
-  "returnCode" : "200",
-  "timeOut" : false
-}
+    mikephie =  {
+    subscriptions: [
+        {
+            id: 'your_subscription_id',
+            provider: 'apple',
+            status: 'active',
+            periodEnd: 0xf4865700,
+            createdAt: 0x670d42cd
+        }
+    ]
+};
 
 $done({body : JSON.stringify(mikephie)});
