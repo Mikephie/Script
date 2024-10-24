311ced2fb8870b402eef93b3866e0c77886f92ee
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-24 21:20:07 +0800

    u

--- qx/mrevenue.js
+++ qx/mrevenue.js
@@ -32,6 +32,7 @@ const bundle = {
 
 //识别UA
 const list = {
+  'Color%20Widgets': { name: 'pro', id: 'cw_1999_1y_3d0', cm: 'sja' },  //ColorWidgets
   'ChatBot': { name: 'chatbot_annual', id: 'chatbot_annual', cm: 'sja' },  //ChatBot-AIChat
   'ASKAI': { name: 'pro', id: 'askai_pro', nameb: 'pro_plan', idb: 'token_pro_plan', cm: 'sjb' },  //ASKAI
   'Infltr': { name: 'com.Yooshr.infltr.subscriptionPremium', id: 'com.Yooshr.infltr.everythingForever', cm: 'sja' },  //樱飞无限滤镜
