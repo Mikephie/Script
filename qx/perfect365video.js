89b953b16da77cbcff4d1589052070219d3e2564
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-27 14:52:36 +0800

    👌

--- qx/perfect365video.js
+++ qx/perfect365video.js
@@ -16,18 +16,18 @@ hostname = video-svr.perfect365.com
 *******************************/
 
 
-var mikephie = JSON.parse($response.body);
+// 假设 body 已经有一些内容，先解析它
+let body = JSON.parse($response.body);
 
-    mikephie = {
-  "msg" : "success",
-  "data" : {
-    "isEligible" : true,
-    "isSubscribing" : true,
-    "productId" : "subscription_year",
-    "expireTime" : "4092599349000",
-    "isYearlyConversion" : true
-  },
-  "code" : 0
-}
+// 添加或更新字段
+body.data = {
+  ...body.data,
+  "isEligible": true,
+  "isSubscribing": true,
+  "productId": "subscription_year",
+  "expireTime": "4092599349000",
+  "isYearlyConversion": true
+};
 
-$done({body : JSON.stringify(mikephie)});
+// 将修改后的对象转换回 JSON 格式并返回
+$done({ body: JSON.stringify(body) });
