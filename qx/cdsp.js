0c4bb6022eb67fdba279a28efbd7479dd3aeaa1f
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-27 14:33:43 +0800

    👌

--- qx/cdsp.js
+++ qx/cdsp.js
@@ -21,19 +21,11 @@ var mikephie = JSON.parse($response.body);
 
 mikephie.data = {
   ...mikephie.data,
-  "vipState": true,
-  "end_time": 3742762088,
-  "allLifeVip": true,
-  "status": 2,
-  "level": 2,
-  "is_select": 1,
-  "notice": "卡券：5张",
-  "title1": "终身VIP会员特权",
-  "remark": "gerenzhongshen",
-  "cat": "geren",
-  "show": 200,
-  "wx_name": "Mikephie",
-  "headimg": "https://i.ibb.co/wM5z10N/IMG-1287.jpg"
+  "isEligible" : true,
+    "isSubscribing" : true,
+    "productId" : "subscription_year",
+    "expireTime" : "3742762088000",
+    "isYearlyConversion" : true
 };
  
 $done({body: JSON.stringify(mikephie)});
