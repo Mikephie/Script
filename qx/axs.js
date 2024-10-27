660954490823c9496959cba9c8a06f5c57fbb911
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-27 18:30:15 +0800

    👌

--- qx/axs.js
+++ qx/axs.js
@@ -1,19 +1,12 @@
 #!name=AXS Payment
-
 #!desc=AXS Payment 去广告
-
 #!homepage=https://mike
-
 #!author=mike
-
 #!icon=https://raw.githubusercontent.com/Mikephie/Script/main/Mike_axs.js
 
 [rewrite_local]
-^https?:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/(InfoPage|highlight)\/.*\.(png|jpg|jpeg|gif|js|css)(\?.*)?$ url reject-200
+^https?:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/InfoPage\/[^\/]+\/whatsnew\.php\?paramData=[A-F0-9]+&keyEnc=[A-F0-9]+&idx=\d+&APP_VERSION=\d+\.\d+\.\d+ url reject-200
+^https:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/InfoPage\/(?:InfoPageProcess\.php|(?:[^\/]+\/(?:images\/[^\/]+\.(?:png|jpg)|css\/[^\/]+\.css))(?:\?\d+)?)$ url reject-200
 
 
-hostname=m-station2.axs.com.sg
-
-
-
-#https:\/\/m-station2\.axs\.com\.sg\/AXSMobile\/(InfoPage|highlight)\/[A-Za-z0-9_\/?.=&-]+ url reject-200
+hostname = m-station2.axs.com.sg
