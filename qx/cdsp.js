de345bba7e17a8f526817cc852cc08d534f4e002
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-27 14:32:57 +0800

    👌

--- qx/cdsp.js
+++ qx/cdsp.js
@@ -19,34 +19,21 @@ hostname = appss.baomingding.com
 
 var mikephie = JSON.parse($response.body);
 
-    mikephie = {
-  "success" : true,
-  "result" : {
-    "headImg" : "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
-    "dataId" : "00000000000000000000",
-    "appleUserEmail" : "mikephiemy@gmail.com",
-    "wordage" : 7777777,
-    "mobile" : "Mikephie",
-    "inviteCode" : "000000",
-    "vipGroupInfos" : [
-      {
-        "groupType" : "TYPE_ONE",
-        "vipType" : "VIP",
-        "autoPay" : "NO"
-      }
-    ],
-    "type" : "VIP", 
-    "vipExpireTime" : "2088-08-08 08:08:08",
-    "vipExpireDays" : 99999999,
-    "registerTime" : "2022-09-09 03:20:32",
-    "nickname" : "Mikephie",
-    "email" : "mikephiemy@gmail.com",
-    "remainTimeSeconds" : 99999,
-    "realnameStatus" : "NO",
-    "times" : 77777777
-  },
-  "returnCode" : "200",
-  "timeOut" : false
-}
-
-$done({body : JSON.stringify(mikephie)});
+mikephie.data = {
+  ...mikephie.data,
+  "vipState": true,
+  "end_time": 3742762088,
+  "allLifeVip": true,
+  "status": 2,
+  "level": 2,
+  "is_select": 1,
+  "notice": "卡券：5张",
+  "title1": "终身VIP会员特权",
+  "remark": "gerenzhongshen",
+  "cat": "geren",
+  "show": 200,
+  "wx_name": "Mikephie",
+  "headimg": "https://i.ibb.co/wM5z10N/IMG-1287.jpg"
+};
+ 
+$done({body: JSON.stringify(mikephie)});
