a2e3dc5b0f436af210bcc3b879b0b0f50e7466f3
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-10-30 13:37:22 +0800

    👌

--- qx/iTunes.js
+++ qx/iTunes.js
@@ -58,6 +58,7 @@ const appList = {
   'iscreen': { method: 'v1', product_id: 'com.zerone.hidesktop.forever' },
   'pollykann': { method: 'v1', product_id: 'vip.forever.pollykann' },
   'VLLO': { method: 'v1', product_id: 'com.darinsoft.vimo.yearly' },
+  'VLLO': { method: 'v1', product_id: 'com.darinsoft.vimo.yearly' },
   
   // 第二个脚本的应用列表
   'co.vulcanlabs': { method: 'v2', cm: 'timeb', hx: 'hxpda', id: "liftetimeid", latest: "mikephie" },
