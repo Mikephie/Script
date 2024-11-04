683fceef3937f81f991476c112c35230bdb23701
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-04 13:53:06 +0800

    👌

--- qx/mrevenue.js
+++ qx/mrevenue.js
@@ -32,7 +32,7 @@ const bundle = {
 
 //识别UA
 const list = {
-  'Vocai-iOS': { name: 'premium', id: 'genius_lifetime_59', cm: 'sjb' },  // Vocai
+  'Genius%20AI': { name: 'premium', id: 'genius_lifetime_59', cm: 'sjb' },  // Genius%20AI
   'Vocai-iOS': { name: 'AI Pro', id: 'vocabAI_900_1m', cm: 'sja' },  // Vocai
   'PixImagine': { id: 'com.efsoft.piximagine_nc_lifetime', cm: 'sjc' },  //PixImagine
   '1Blocker': { name: 'premium', id: 'blocker.ios.iap.lifetime', cm: 'sjb' },  //1Blocker
