341747737bab19b583a68bbfe1fc6c4ea59d99c6
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-05 12:11:52 +0800

    👌

--- qx/mrevenue.js
+++ qx/mrevenue.js
@@ -32,6 +32,7 @@ const bundle = {
 
 //识别UA
 const list = {
+  'Forward': { name: 'pro', id: 'forward.vip.forever.vvebo', cm: 'sja' },  //Forward
   'Genius%20AI': { name: 'premium', id: 'genius_lifetime_59', cm: 'sjb' },  // Genius%20AI
   'Vocai-iOS': { name: 'AI Pro', id: 'vocabAI_900_1m', cm: 'sja' },  // Vocai
   'PixImagine': { id: 'com.efsoft.piximagine_nc_lifetime', cm: 'sjc' },  //PixImagine
