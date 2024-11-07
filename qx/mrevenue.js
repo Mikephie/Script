be00d9da8a2947a5d453c0f75b582cfe2276cced
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-07 15:13:20 +0800

    👌

--- qx/mrevenue.js
+++ qx/mrevenue.js
@@ -33,7 +33,7 @@ const bundle = {
 
 //识别UA
 const list = {
-  'WorldClock': { name: 'lifetime', id: 'com.overdesigned.worldclock.removeads1', cm: 'sjb' },  // WorldClock
+  'World%20Clock': { name: 'lifetime-unlock', id: 'com.overdesigned.worldclock.removeads1', cm: 'sjb' },  // World%20Clock
   'Genius%20AI': { name: 'premium', id: 'genius_lifetime_59', cm: 'sjb' },  // Genius%20AI
   'Vocai-iOS': { name: 'AI Pro', id: 'vocabAI_900_1m', cm: 'sja' },  // Vocai
   'PixImagine': { id: 'com.efsoft.piximagine_nc_lifetime', cm: 'sjc' },  //PixImagine
