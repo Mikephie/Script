ae0266aeaceb78c0e5543a6c7481a6ca32262682
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-15 14:32:16 +0800

    👌

--- qx/redditvip.js
+++ qx/redditvip.js
@@ -1,34 +1,34 @@
 let body = JSON.parse($response.body);
 
-function processObject(obj) {
-  if (typeof obj !== 'object' || obj === null) return obj;
-
+function processNSFW(obj) {
   if (obj.isNsfw === true) obj.isNsfw = false;
   if (obj.isNsfwMediaBlocked === true) obj.isNsfwMediaBlocked = false;
   if (obj.isNsfwContentShown === false) obj.isNsfwContentShown = true;
-  
-  if (Array.isArray(obj.commentsPageAds)) obj.commentsPageAds = [];
-  
-  if (obj.node && Array.isArray(obj.node.cells) && 
-      obj.node.cells.some(cell => cell.__typename === 'AdMetadataCell' || cell.isAdPost === true)) {
-    return undefined;
-  }
-  
-  if (obj.node && typeof obj.node.adPayload === 'object') {
-    return undefined;
-  }
-  
-  if (obj.__typename === 'AdPost') {
-    return undefined;
-  }
-
-  for (let key in obj) {
-    obj[key] = processObject(obj[key]);
-  }
-
-  return obj;
 }
 
-body = processObject(body);
+function removeAds(obj) {
+  // 清空评论页面广告
+  if (Array.isArray(obj.commentsPageAds)) {
+    obj.commentsPageAds = [];
+  }
+
+  // 处理 data 对象中的广告
+  if (obj.data && typeof obj.data === 'object') {
+    for (let key in obj.data) {
+      if (obj.data[key] && obj.data[key].elements && Array.isArray(obj.data[key].elements.edges)) {
+        obj.data[key].elements.edges = obj.data[key].elements.edges.filter(edge => {
+          if (!edge || !edge.node) return true;
+          if (edge.node.__typename === 'AdPost') return false;
+          if (edge.node.adPayload) return false;
+          if (Array.isArray(edge.node.cells) && edge.node.cells.some(cell => cell.__typename === 'AdMetadataCell' || cell.isAdPost === true)) return false;
+          return true;
+        });
+      }
+    }
+  }
+}
+
+processNSFW(body);
+removeAds(body);
 
 $response.body = JSON.stringify(body);
