630c90db92b455ce2fbe24ef46c1248dd4d3772d
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-17 22:23:45 +0800

    👌

--- qx/notetest.js
+++ qx/notetest.js
@@ -1,7 +1,7 @@
 
 /******************************
 
-脚本名称: Notebook4
+脚本名称: Notebook5
 下载地址：商店
 脚本作者：Mikephie
 更新时间：2024-06-08
@@ -11,9 +11,13 @@
 *******************************
 
 [rewrite_local]
-# VIP 订阅, 100G 空间, VIP 多项权益
+# VIP 订阅
 ^https:\/\/notebook\.zoho\.com\/api\/v1\/userprofile\/accounts\/payment\?action=get_current_plan_detail&include_expired_plans=true url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/notetest.js
+
+# 100G 空间
 ^https:\/\/notebook\.zoho\.com\/api\/v1\/payments\/feature\/consumptions url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/notetest.js
+
+# VIP 多项权益
 ^https:\/\/notebook\.zoho\.com\/api\/v1\/userprofile\/accounts\/payment\?action=get_feature_template&platform=ios url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/notetest.js
 
 
@@ -29,34 +33,39 @@ console.log("URL: " + url);
 console.log("Original body: " + body);
 
 if (url.indexOf('get_current_plan_detail') !== -1) {
-    // VIP 订阅部分
-    var obj = JSON.parse(body);
-    obj.plan_details = [{
-        "expiry_time": 3742762088000,
-        "purchase_source": "notebook",
-        "service_id": "107000",
-        "source": "PAID",
-        "plan_name": "Notebook Pro",
-        "payment_frequency": 12,
-        "service": "NoteBook",
-        "grace_period": 999160000000,
-        "notebook_plan_id": "com.zoho.notebook.pro",
-        "plan_description": "Upgrade to Notebook Pro and stay more productive",
-        "zoho_store_plan_id": 107102,
-        "purchase_time": 1717644792301
-    }];
-    body = JSON.stringify(obj);
+    // VIP 订阅
+    var mikephie = JSON.parse(body);
+    mikephie = {
+        "code": 200,
+        "status": "Success",
+        "message": "User profile fetched successfully",
+        "plan_details": [{
+            "expiry_time": 3742762088000,
+            "purchase_source": "notebook",
+            "service_id": "107000",
+            "source": "PAID",
+            "plan_name": "Notebook Pro",
+            "payment_frequency": 12,
+            "service": "NoteBook",
+            "grace_period": 999160000000,
+            "notebook_plan_id": "com.zoho.notebook.pro",
+            "plan_description": "Upgrade to Notebook Pro and stay more productive",
+            "zoho_store_plan_id": 107102,
+            "purchase_time": 1717644792301
+        }]
+    };
+    body = JSON.stringify(mikephie);
 } else if (url.indexOf('feature/consumptions') !== -1) {
-    // 100G 空间部分
-    var obj = JSON.parse(body);
-    obj = {
+    // 100G 空间
+    var mikephie = JSON.parse(body);
+    mikephie = {
         "code": 200,
         "status": "Success",
         "message": "Success",
         "feature_consumptions": [{
             "feature_id": "com.zoho.notebook.storage",
             "consumptions": [{
-                "value": "5268006",
+                "value": "107374182400",
                 "name": "SIZE",
                 "unit": "BYTES",
                 "user_type": "INDIVIDUAL_USER"
@@ -64,11 +73,11 @@ if (url.indexOf('get_current_plan_detail') !== -1) {
             "source": "PAID"
         }]
     };
-    body = JSON.stringify(obj);
+    body = JSON.stringify(mikephie);
 } else if (url.indexOf('get_feature_template') !== -1) {
-    // VIP 多项权益部分
-    var obj = JSON.parse(body);
-    obj = {
+    // VIP 多项权益
+    var mikephie = JSON.parse(body);
+    mikephie = {
         "code": 200,
         "status": "Success",
         "message": "User profile fetched successfully",
@@ -89,7 +98,7 @@ if (url.indexOf('get_current_plan_detail') !== -1) {
             }]
         }))
     };
-    body = JSON.stringify(obj);
+    body = JSON.stringify(mikephie);
 }
 
 console.log("Modified body: " + body);
