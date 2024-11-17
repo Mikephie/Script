f8ddfdcf65a7a8bd52f1b1dea2431284b04993f2
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-17 19:05:25 +0800

    👌

--- qx/notetest.js
+++ qx/notetest.js
@@ -11,9 +11,7 @@
 
 [rewrite_local]
 # VIP 订阅, 100G 空间, VIP 多项权益
-^https:\/\/notebook\.zoho\.com\/api\/v1\/userprofile\/accounts\/payment\?action=get_current_plan_detail&include_expired_plans=true url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/notetest.js
 ^https:\/\/notebook\.zoho\.com\/api\/v1\/payments\/feature\/consumptions url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/notetest.js
-^https:\/\/notebook\.zoho\.com\/api\/v1\/userprofile\/accounts\/payment\?action=get_feature_template&platform=ios url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/notetest.js
 
 
 [MITM]
@@ -28,58 +26,31 @@ console.log("Script started");
 console.log("URL: " + url);
 console.log("Original body: " + body);
 
-if (url.indexOf('get_current_plan_detail') !== -1) {
-  var obj = JSON.parse(body);
-  obj.plan_details = [{
-    "expiry_time": 3742762088000,
-    "purchase_source": "notebook",
-    "service_id": "107000",
-    "source": "PAID",
-    "plan_name": "Notebook Pro",
-    "payment_frequency": 12,
-    "service": "NoteBook",
-    "grace_period": 999160000000,
-    "notebook_plan_id": "com.zoho.notebook.pro",
-    "plan_description": "Upgraded to Notebook Pro",
-    "zoho_store_plan_id": 107102,
-    "purchase_time": 1717644792301
-  }];
-  body = JSON.stringify(obj);
-} else if (url.indexOf('feature/consumptions') !== -1) {
-  var obj = JSON.parse(body);
-  obj.feature_consumptions = [{
-    "feature_id": "com.zoho.notebook.storage",
-    "consumptions": [{
-      "value": "5268006",
-      "name": "SIZE",
-      "unit": "BYTES",
-      "user_type": "INDIVIDUAL_USER"
-    }],
-    "source": "PAID"
-  }];
-  body = JSON.stringify(obj);
-} else if (url.indexOf('get_feature_template') !== -1) {
-  var obj = JSON.parse(body);
-  obj.feature_template = [
-    "AUDIO_CARD", "OCR", "CHAT_WITH_US", "FLIGHT_CARD", "EMAIL_IN",
-    "CUSTOM_RECURRING_REMINDER", "PREMIUM_COVERS", "NOTECARD", "STORAGE",
-    "PHONE_SUPPORT", "NOTEBOOK_SHARING", "SCAN_TABLE", "TAG_SUGGESTIONS",
-    "EXPORT_AS_PDF", "BCR", "SMART_SEARCH", "FEATURE_X"
-  ].map(function(feature) {
-    return {
-      feature_name: feature,
-      feature_id: "com.zoho.notebook." + feature.toLowerCase(),
-      feature_meta_data: [{
-        end_date: 3742762088000,
-        source: (feature === "EXPORT_AS_PDF" || feature === "FEATURE_X") ? "FREE" : "PAID",
-        type: "PRIMARY",
-        start_date: 1717644792301,
-        grace_period: 999160000000
-      }]
-    };
-  });
-  body = JSON.stringify(obj);
+if (url.indexOf('feature/consumptions') !== -1) {
+  console.log("Matched URL for feature consumptions");
+  try {
+    var obj = JSON.parse(body);
+    console.log("Original obj:", JSON.stringify(obj));
+    
+    obj.feature_consumptions = [{
+      "feature_id": "com.zoho.notebook.storage",
+      "consumptions": [{
+        "value": "5268006",
+        "name": "SIZE",
+        "unit": "BYTES",
+        "user_type": "INDIVIDUAL_USER"
+      }],
+      "source": "PAID"
+    }];
+    
+    body = JSON.stringify(obj);
+    console.log("Modified obj:", JSON.stringify(obj));
+  } catch (e) {
+    console.log("Error occurred: " + e.message);
+  }
+} else {
+  console.log("URL did not match feature/consumptions");
 }
 
-console.log("Modified body: " + body);
+console.log("Final body: " + body);
 $done({body});
