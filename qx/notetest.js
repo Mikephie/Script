c78d80a904fbb3a868352c5b0244d40a817f44ac
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-17 21:41:28 +0800

    👌

--- qx/notetest.js
+++ qx/notetest.js
@@ -1,7 +1,7 @@
 
 /******************************
 
-脚本名称: Notebook
+脚本名称: Notebook1
 下载地址：商店
 脚本作者：Mikephie
 更新时间：2024-06-08
@@ -28,44 +28,44 @@ console.log("Script started");
 console.log("URL: " + url);
 console.log("Original body: " + body);
 
-function modifyResponse(obj) {
-    if (obj.plan_details) {
-        obj.plan_details.forEach(plan => {
-            plan.source = "PAID";
-            plan.is_active = true;
-            plan.is_expired = false;
-            plan.expiry_time = 3742762088000;
-            plan.grace_period = 999160000000;
-        });
-    }
-    
-    if (obj.feature_consumptions) {
-        obj.feature_consumptions.forEach(feature => {
-            if (feature.feature_id === "com.zoho.notebook.storage") {
-                feature.consumptions.forEach(consumption => {
-                    if (consumption.name === "SIZE") {
-                        consumption.value = "107374182400"; // 100 GB
-                    }
-                });
-            }
-            feature.source = "PAID";
-        });
-    }
-    
-    if (obj.feature_template) {
-        obj.feature_template = obj.feature_template.map(feature => ({
-            feature_name: feature.feature_name,
-            feature_id: feature.feature_id,
-            feature_meta_data: [{
-                end_date: 3742762088000,
-                source: "PAID",
-                type: "PRIMARY",
-                start_date: 1717644792301,
-                grace_period: 999160000000,
-                is_active: true,
-                is_enabled: true
-            }]
-        }));
+function modifyResponse(obj, url) {
+    if (url.indexOf('get_current_plan_detail') !== -1) {
+        if (obj.plan_details) {
+            obj.plan_details.forEach(plan => {
+                plan.source = "PAID";
+                plan.is_active = true;
+                plan.is_expired = false;
+                plan.expiry_time = 3742762088000;
+                plan.grace_period = 999160000000;
+            });
+        }
+    } else if (url.indexOf('feature/consumptions') !== -1) {
+        if (obj.feature_consumptions) {
+            obj.feature_consumptions.forEach(feature => {
+                if (feature.feature_id === "com.zoho.notebook.storage") {
+                    feature.consumptions.forEach(consumption => {
+                        if (consumption.name === "SIZE") {
+                            consumption.value = "107374182400"; // 100 GB
+                        }
+                    });
+                }
+                feature.source = "PAID";
+            });
+        }
+    } else if (url.indexOf('get_feature_template') !== -1) {
+        if (obj.feature_template) {
+            obj.feature_template = obj.feature_template.map(feature => ({
+                ...feature,
+                feature_meta_data: feature.feature_meta_data.map(meta => ({
+                    ...meta,
+                    end_date: 3742762088000,
+                    source: "PAID",
+                    is_active: true,
+                    is_enabled: true,
+                    grace_period: 999160000000
+                }))
+            }));
+        }
     }
     
     return obj;
@@ -75,7 +75,7 @@ try {
     var obj = JSON.parse(body);
     console.log("Parsed original object:", JSON.stringify(obj));
     
-    obj = modifyResponse(obj);
+    obj = modifyResponse(obj, url);
     
     body = JSON.stringify(obj);
     console.log("Modified body:", body);
