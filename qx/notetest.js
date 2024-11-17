398f35fa9235ab674c2955a02d64930d3e0f7965
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-17 11:09:12 +0800

    👌

--- qx/notetest.js
+++ qx/notetest.js
@@ -30,7 +30,6 @@ const URL2 = '/userprofile/accounts/payment?action=get_current_plan_detail&inclu
 const URL3 = '/payments/feature/consumptions';
 const URL4 = '/userprofile/accounts/payment?action=get_feature_template&platform=ios';
 const URL5 = '/userprofile/accounts/payment?action=get_feature_template';
-const URL6 = '/sdk/api/apptics/v1/app/bearertoken';
 
 // 第一个 URL 处理
 if (url.indexOf(URL1) != -1) {
@@ -101,13 +100,6 @@ if (url.indexOf(URL5) != -1) {
   };
 }
 
-// 第六个 URL 处理
-if (url.indexOf(URL6) != -1) {
-  mikephie.result = "success";
-  mikephie.data = {
-    "anon_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzZXNzaW9uVG9rZW4iOiJBaENKcWRmUnpVWVJwM2VFZlNmdmZ6MEV0bFlJal9ROUF1WEp0V3pqTEFHY1JpVDZNMlI3a0FnSlJGR2dLc24wY3c4S182NUZwbzdLZElldWphajdoZkFVZUVWSmxqZDFWTEtLeFI0QTFLWjdJYjdMZV94UHhFQ0xubEYtUTNpQkotQzIwN0w5MTBQMGRyb0lrWURsWWJwZmpadHZWQ2Ita213bTB0UWk5XzhPajROaHpWRDY1R2tET3IzMFIyUWJScGU3U1FkZ1RDaFNxUHE5XBCV2ZHVy1YT3d2cnN1SHpZUFhEdnJvNVJXTDRsWEtyZEFrUE5MTWcyTG11X2U4OWtFeDRyNkRQZE9kVmtESF84M1FPM0VBUjQ5SVJJb3JvTDE3d25Ma3Q3dkRuc1V5RnVoMm5VMFQ3MWl6QjBrUDVWSGtWTG9rSVUxMGdJQV9oUGciLCJpc3MiOiJhcHB0aWNzIiwiZXhwIjoxNzE3NjYwMTQzfQ.idzajvRjSlbVTs7gGr6v3oU2Pvdkl-PUap9UWVtYz9UPq33k93-q6MFtGYjwQ4v5nhZ3NHRHWRK511j45aAhMmmi1ugBmb1fsJWTctt-KIt7S_X7vt-LX4s9NHLKltjYp3NddToVc_pH10QMg430mv27vLy9rrCrXktveZFF-3mW1wRq7qgWXpSXBdK51wX_SM-_Bq7qHY6kdP_R5nt5b7Tke5KaAE3Z0o4FPgn0PusphzQw2F6t4VU0mZaXrdLNOTDp5-aYLAcZsSZ-Fm_qPJS6KtO0GFxPxLmBgiuMWMKVM4aq4Yw-HIP7acoollO1KXDPyTBGFjXAPaDWFn04ZQ"
-  };
-}
 
 // 返回 JSON 响应
 $done({body: JSON.stringify(mikephie)});
