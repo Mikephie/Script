8ca74a855e4e5379b0673b6da6428b08209a3c4b
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-18 19:04:29 +0800

    👌

--- qx/pdfexpert.js
+++ qx/pdfexpert.js
@@ -9,7 +9,7 @@
 
 [rewrite_local]
 ^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/pdfexpert6\/subscription\/refresh url script-request-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/pdfexpert.js
-
+^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/pdfexpert6\/subscription\/refresh url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/pdfexpert.js
 
 [mitm]
 hostname = license.pdfexpert.com
@@ -28,16 +28,84 @@ const requestBody = {
 
 // Expected response body structure 
 const responseBody = {
-  originalTransactionId: 30001521230668,
-  inAppStates: [
+  "originalTransactionId" : 30001521230668,
+  "inAppStates" : [
     {
-      productId: "ABC"  
+      "productId" : "com.readdle.PDFExpert5.subscription.year50BMI",
+      "isInGracePeriod" : false,
+      "subscriptionExpirationDate" : "02:17 16/03/2025",
+      "subscriptionExpirationTimestamp" : 1742091445,
+      "originalTransactionId" : 30001521230668,
+      "productName" : "subscription",
+      "isEligibleForIntroPeriod" : true,
+      "isInBillingRetryPeriod" : false,
+      "type" : "subscription",
+      "purchaseDate" : "02:17 16/03/2024",
+      "entitlements" : [
+        "ios.pe.ai-features",
+        "ios.pe.subscription.pdf-features"
+      ],
+      "transactionId" : "30001956396811",
+      "subscriptionGroupId" : "20537380",
+      "subscriptionState" : "active",
+      "subscriptionAutoRenewStatus" : "autoRenewOff",
+      "originalPurchaseDate" : "09:23 08/02/2023"
+    },
+    {
+      "productId" : "com.readdle.PDFExpert5.subscription.month10",
+      "isInGracePeriod" : false,
+      "subscriptionExpirationDate" : "01:40 16/03/2023",
+      "subscriptionExpirationTimestamp" : 1678930812,
+      "originalTransactionId" : 30001521230668,
+      "productName" : "subscription",
+      "isEligibleForIntroPeriod" : true,
+      "isInBillingRetryPeriod" : false,
+      "type" : "subscription",
+      "purchaseDate" : "02:40 16/02/2023",
+      "entitlements" : [
+        "ios.pe.ai-features",
+        "ios.pe.subscription.pdf-features"
+      ],
+      "transactionId" : "30001529433342",
+      "subscriptionGroupId" : "20537380",
+      "subscriptionState" : "lapsed",
+      "subscriptionAutoRenewStatus" : "autoRenewOff",
+      "originalPurchaseDate" : "09:23 08/02/2023"
+    },
+    {
+      "productId" : "com.readdle.PDFExpert5.subscription.year80",
+      "isInGracePeriod" : false,
+      "subscriptionExpirationDate" : "09:23 15/02/2023",
+      "subscriptionExpirationTimestamp" : 1676453033,
+      "originalTransactionId" : 30001521230668,
+      "productName" : "subscription",
+      "isEligibleForIntroPeriod" : false,
+      "isInBillingRetryPeriod" : false,
+      "type" : "subscription",
+      "purchaseDate" : "09:23 08/02/2023",
+      "entitlements" : [
+        "ios.pe.ai-features",
+        "ios.pe.subscription.pdf-features",
+        "macos.pe.subscription.pdf-features",
+        "macos.pe.ai-features"
+      ],
+      "transactionId" : "30001521230668",
+      "subscriptionGroupId" : "20537380",
+      "subscriptionState" : "lapsed",
+      "subscriptionAutoRenewStatus" : "autoRenewOff",
+      "originalPurchaseDate" : "09:23 08/02/2023"
     }
-  ]
+  ],
+  "chargingPlatform" : "iOS AppStore",
+  "receiptStatus" : "ok",
+  "bundleId" : "com.readdle.PDFExpert5.subscription.year50BMI",
+  "receiptId" : 1675847933000,
+  "statisticsInfo" : {
+    "events" : [
+
+    ]
+  },
+  "externalId" : "67c642be-e86d-43d2-a67a-5ae71c93d0cd"
 }
-
-$done({body: JSON.stringify(responseBody)})
-
-body = 
 
 $done({'body':JSON['stringify'](body)});
