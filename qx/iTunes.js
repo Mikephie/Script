57a2877859c9416cd03f1fb3b00723106a702788
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-23 09:43:48 +0800

    👌

--- qx/iTunes.js
+++ qx/iTunes.js
@@ -56,7 +56,6 @@ const appList = {
   'PhotoCollagePro': { method: 'v1', product_id: 'PHOTABLE_PREMIUM' },
   'iscreen': { method: 'v1', product_id: 'com.zerone.hidesktop.forever' },
   'pollykann': { method: 'v1', product_id: 'vip.forever.pollykann' },
-  //'VLLO': { method: 'v1', product_id: 'com.darinsoft.vimo.yearly' },
   'iClear': { method: 'v1', product_id: 'com.youthpe.retake.yearly' },
   
   // 第二个脚本的应用列表
