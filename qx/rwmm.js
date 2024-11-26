813c357c2caa0a56565bb2fc10ddf5d1e7c9abe6
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-26 11:52:33 +0800

    👌

--- qx/rwmm.js
+++ qx/rwmm.js
@@ -1,1 +1,25 @@
-https://raw.githubusercontent.com/Mikephie/Script/main/qx/
+/*
+哎呀漫鸭
+新商店版：让我喵喵
+解锁会员权益
+
+[rewrite_local]
+https://www.pdreamer.com/aymyProd/api/getUserInfo url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/rwmm.js
+
+
+[mitm] 
+hostname = www.pdreamer.com
+*/
+
+var mikephie = JSON.parse($response.body);
+
+mikephie.data = {
+  ...mikephie.data,
+   "purchasedVIPExpiresAt": "2088-08-08",
+   "vipStatus": 1,
+   "inviteNum": 99,
+   "vipexpiresAt": "2088-08-08",
+   "purchasedVIP": "2088-08-08",
+};
+ 
+$done({body: JSON.stringify(mikephie)});
