039363f13df820dd7ee1d30ba8c19aff3d2f6db7
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-27 13:50:45 +0800

    👌

--- qx/zypg-afengye.js
+++ qx/zypg-afengye.js
@@ -17,20 +17,34 @@ hostname = appss.rhinoxlab.com
 *******************************/
 
 
-var aFengYe = $response.body;
-var obj =  JSON.parse(aFengYe);
 
-if($request.url.indexOf("/app/account/getAccountInfo") != -1) {
-    obj.result.type = "VIP";
-    obj.result.freeFlag = "YES";
+var mikephie = $response.body;
+var obj = JSON.parse(mikephie);
+
+if ($request.url.indexOf("/app/account/getAccountInfo") != -1) {
+    obj.result.headImg = "https://i.ibb.co/f1cgnGT/IMG-1215.jpg";
+    obj.result.dataId = "00000000000000000000";
+    obj.result.appleUserEmail = "mikephiemy@gmail.com";
+    obj.result.wordage = 7777777;
+    obj.result.mobile = "Mikephie";
+    obj.result.inviteCode = "000000";
     obj.result.vipGroupInfos = [
-       {
-        "groupType" : "TYPE_ONE",
-        "vipType" : "VIP",
-        "autoPay" : "YES"
-      }
+        {
+            "groupType": "TYPE_ONE",
+            "vipType": "VIP",
+            "autoPay": "NO"
+        }
     ];
+    obj.result.type = "VIP";
+    obj.result.vipExpireTime = "2088-08-08 08:08:08";
+    obj.result.vipExpireDays = 99999999;
+    obj.result.registerTime = "2022-09-09 03:20:32";
+    obj.result.nickname = "Mikephie";
+    obj.result.email = "mikephiemy@gmail.com";
+    obj.result.remainTimeSeconds = 99999;
+    obj.result.realnameStatus = "NO";
+    obj.result.times = 77777777;
 }
 
-aFengYe = JSON.stringify(obj);
-$done(aFengYe);
+mikephie = JSON.stringify(obj);
+$done(mikephie);
