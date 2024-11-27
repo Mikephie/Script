3151d4b7f2391b9eb884cd9df61cac7c32cf04c2
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-27 22:52:45 +0800

    👌

--- qx/zypg-afengye.js
+++ qx/zypg-afengye.js
@@ -24,7 +24,7 @@ try {
     // 解析响应体为 JSON 对象
     let obj = JSON.parse(mikephie);
 
-    if ($request.url.includes("getAccountInfo")) {
+    if ($request.url.includes("/getAccountInfo")) {
         // 修改或添加字段
         obj.result.type = "VIP"; // 设置用户类型为 VIP
         obj.result.freeFlag = "YES"; // 设置为免费用户
