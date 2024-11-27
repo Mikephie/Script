ddec5237e383c218028e97be5351e48650da66a9
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-27 19:54:50 +0800

    👌

--- qx/classdojo.js
+++ qx/classdojo.js
@@ -15,53 +15,23 @@ hostname = payments.classdojo.com
 
 *******************************/
 
-var mikephie = JSON.parse($response.body);
+let body = JSON.parse($response.body);
 
-    mikephie = {
-  "request_date_ms" : 1719384796877,
-  "request_date" : "2024-06-26T06:53:16Z",
-  "subscriber" : {
-    "non_subscriptions" : {
-
-    },
-    "first_seen" : "2021-06-27T09:08:42Z",
-    "original_application_version" : "6937",
-    "subscriber_attributes" : {
-      "$attConsentStatus" : {
-        "updated_at_ms" : 1719379318334,
-        "value" : "notDetermined"
-      }
-    },
-    "other_purchases" : {
-
-    },
-    "management_url" : "https://apps.apple.com/account/subscriptions",
-    "subscriptions" : {
-      "com.classdojo.storekit.afterschool.annual" : {
-        "store_transaction_id" : "300001899476419",
-        "is_sandbox" : false,
-        "ownership_type" : "PURCHASED",
-        "period_type" : "trial",
-        "unsubscribe_detected_at" : "2024-06-26T05:42:54Z",
-        "expires_date" : "2088-08-08T08:08:08Z",
-        "refunded_at" : null,
-        "original_purchase_date" : "2024-06-26T05:21:56Z",
-        "auto_resume_date" : null,
-        "purchase_date" : "2024-06-26T05:21:54Z",
-        "store" : "app_store"
-      }
-    },
-    "entitlements" : {
-      "Beyond" : {
-        "expires_date" : "2088-08-08T08:08:08Z",
-        "product_identifier" : "com.classdojo.storekit.afterschool.annual",
-        "purchase_date" : "2024-06-26T05:21:54Z"
-      }
-    },
-    "original_purchase_date" : "2024-06-26T05:21:56Z",
-    "original_app_user_id" : "5dc2e328587d3dcf1bee91e0",
-    "last_seen" : "2024-06-26T05:21:59Z"
-  }
+function modifyObject(obj) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (typeof obj[key] === 'object' && obj[key] !== null) {
+                modifyObject(obj[key]);
+            } else {
+                // 修改 expires_date 的值
+                if (key === 'expires_date') {
+                    obj[key] = "2088-08-08T08:08:08Z";
+                }
+            }
+        }
+    }
 }
 
-$done({body : JSON.stringify(mikephie)});
+// 修改对象
+modifyObject(body);
+$done({ body: JSON.stringify(body) });
