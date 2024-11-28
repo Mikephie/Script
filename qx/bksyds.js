19a8811f0b81d47c6c734885e7b334fb734ee7f5
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-29 06:54:48 +0800

    👌

--- qx/bksyds.js
+++ qx/bksyds.js
@@ -20,15 +20,15 @@ let mikephie = JSON.parse($response.body);
 if ($request.url.includes("/auth/member")) {
     Object.assign(mikephie.result, {
         memberExpire: 3742762088000,      // 更新或添加字段
-        uid: "f7d62252b11144ee8193f85fa95fcf0b",      // 更新或添加字段
+        uid: "f7d62252b11144ee8193f85fa95fcf0b",      // 更新添加
         });
 } else if ($request.url.includes("/clickEvent")) {
     Object.assign(mikephie.result, {
-        isVip: 1,      // 更新或添加字段
+        id: 38476625,      // 更新添加
         isVip: 1,      // 更新或添加字段
         vipTime: "2088-08-08 08:08:08"      // 更新或添加字段
-        uid: "C4957375-2D67-4728-B3E1-2696A3DFA5C8",      // 更新或添加字段
-        deviceId: "39B810B4-B42D-4208-90CF-2F1573394270",      // 更新或添加字段
+        uid: "C4957375-2D67-4728-B3E1-2696A3DFA5C8",      // 更新添加
+        deviceId: "39B810B4-B42D-4208-90CF-2F1573394270",      // 更新添加
     });
 }
 
