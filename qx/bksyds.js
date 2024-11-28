a3e870ef5096cba7a3b8570fdfee7c8ca148ac94
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-29 06:51:27 +0800

    👌

--- qx/bksyds.js
+++ qx/bksyds.js
@@ -26,7 +26,7 @@ if ($request.url.includes("/auth/member")) {
     Object.assign(mikephie.result, {
         isVip: 1,      // 更新或添加字段
         vipTime: "2088-08-08 08:08:08"      // 更新或添加字段
-        uid: 3742762088000,      // 更新或添加字段
+        uid: "C4957375-2D67-4728-B3E1-2696A3DFA5C8",      // 更新或添加字段
         memberExpire: 3742762088000,      // 更新或添加字段
     });
 }
