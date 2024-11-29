60dbc6c9857435904bbf88fed531edfea8295032
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-29 21:23:14 +0800

    👌

--- qx/bizhi.js
+++ qx/bizhi.js
@@ -22,20 +22,21 @@ hostname = leancloud.emotionwp.com
 let mikephie = JSON.parse($response.body);
 
 if ($request.url.includes("/wpf_account?")) {
-    Object.assign(mikephie.result, {
-        nickName: "Mikephie",          // 添加字段
-        isSVIP: 1,                     // 添加字段
-        svipType: "year_pro",          // 添加字段
-        svipEndTime: 3742761600,       // 添加字段
-        svipEndTimeFormat: "2088-08-08", // 添加字段
-        loginType: 3,                  // 添加字段
-        coin: 88888888,                // 添加字段
-        isVIP: 1,                      // 添加字段
-        vipType: "year_pro",           // 添加字段
-        vipEndTime: 3742761600,        // 添加字段
-        vipEndTimeFormat: "2088-08-08", // 添加字段
-        headImageUrl: "https://i.ibb.co/wM5z10N/IMG-1287.jpg" // 添加字段
-    });  // 修正：添加缺失的右大括号
+    mikephie.results.forEach(user => {
+        Object.assign(user, {
+            nickName: "Mikephie",          // 添加字段
+            isSVIP: 1,                     // 添加字段
+            svipType: "year_pro",          // 添加字段
+            svipEndTime: 3742761600,       // 添加字段
+            svipEndTimeFormat: "2088-08-08", // 添加字段
+            loginType: 3,                  // 添加字段
+            coin: 88888888,                // 添加字段
+            isVIP: 1,                      // 添加字段
+            vipType: "year_pro",           // 添加字段
+            vipEndTime: 3742761600,        // 添加字段
+            vipEndTimeFormat: "2088-08-08", // 添加字段
+            headImageUrl: "https://i.ibb.co/wM5z10N/IMG-1287.jpg" // 添加字段
+        });
 }
 
 $done({ body: JSON.stringify(mikephie) });
