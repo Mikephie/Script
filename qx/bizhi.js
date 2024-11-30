12c36a6dd8b3e1193a151128b3aaa62a5565c28c
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-11-30 10:21:46 +0800

    👌

--- qx/bizhi.js
+++ qx/bizhi.js
@@ -11,7 +11,7 @@ let mikephie = JSON.parse($response.body);
 
 [rewrite_local]
 # > 壁纸 - 精选高清墙纸图片和背景主题Vip&Svip
-^https?:\/\/leancloud.emotionwp.com\/.*\/classes\/wpf_account? url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/bizhi.js
+^https?:\/\/leancloud\.emotionwp\.com\/.+\/(classes|batch\/save) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/bizhi.js
 
 [mitm] 
 hostname = leancloud.emotionwp.com
@@ -19,52 +19,49 @@ hostname = leancloud.emotionwp.com
 *******************************/
 
 var mikephie = JSON.parse($response.body);
+const user = /classes\/wpf_account/;
+const xiazai = /classes/;
+const save = /batch\/save/;
 
-    mikephie = {
-  "results": [
-    {
-      "nickName": "Mikephie",
-      "svipType": "year_pro",
-      "isVIP": 1,
-      "sex": "1",
-      "vipEndTime": 3742761600,
-      "favCount": 0,
-      "createdAt": "2023-11-19T09:29:18.937Z",
-      "vipType": "year_pro",
-      "svipEndTimeFormat": "2088-08-08",
-      "svipEndTime": 3742761600,
-      "updatedAt": "2024-08-18T01:00:30.990Z",
-      "loginType": 3,
-      "downloadCount": 5,
-      "objectId": "6559d56eb87b3b5ada6602a7",
-      "coin": 88888888,
-      "vipEndTimeFormat": "1970-01-01",
-      "warmStartCount": 5,
-      "isSVIP": 1,
-      "coldStartCount": 0,
-      "headImageUrl": "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
-      "userId": "000213.21970f036fd0454a99d29d8cebe086cd.0929"
-    },
-    {
-      "coin": 0,
-      "updatedAt": "2023-11-19T09:35:30.531Z",
-      "vipEndTime": 0,
-      "sex": "1",
-      "isSVIP": 0,
-      "favCount": 0,
-      "warmStartCount": 0,
-      "userId": "000213.21970f036fd0454a99d29d8cebe086cd.0929",
-      "loginType": 3,
-      "nickName": "壁纸用户",
-      "isVIP": 0,
-      "headImageUrl": "http://emotion.emotionwp.com/profile_head.png",
-      "downloadCount": 0,
-      "objectId": "6559d6e271280637ee6efd63",
-      "createdAt": "2023-11-19T09:35:30.531Z",
-      "svipEndTime": 0,
-      "coldStartCount": 0
+if(user.test($request.url)){
+  mikephie.results = [{
+      "updatedAt" : "2024-11-11T11:11:11Z",
+      "vipEndTime" : 3742762088,
+      "vipEndTimeFormat" : "2088-08-08",
+      "sex" : "1",
+      "isSVIP" : 1,
+      "isVIP" : 1,
+      "userId" : "A88888888_B8888888888888-C88",
+      "loginType" : 1,
+      "nickName" : "Mikephie",
+      "headImageUrl" : "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
+      "objectId" : "888888888888888888888888",
+      "createdAt" : "2024-11-11T11:11:11Z",
+      "svipEndTime" : 3742762088,
+      "svipEndTimeFormat" : "2088-08-08",
+      "coin" : 88888888
+  }];
+}
+
+if(xiazai.test($request.url)){
+  mikephie.results.forEach(item => {
+    if ('needVIP' in item && 'needSVIP' in item && 'needCoin' in item && 'score' in item) {
+      item.score = 88888888;
+      item.needVIP = 0;
+      item.needSVIP = 0;
+      item.needCoin = 0;
     }
-  ]
+  });
+}
+
+if(save.test($request.url)){
+  mikephie = {
+  "888888888888888888888888" : {
+    "updatedAt" : "2024-11-11T11:11:11Z",
+    "objectId" : "888888888888888888888888"
+  },
+  "code" : 1
+  }
 }
 
 $done({body : JSON.stringify(mikephie)});
