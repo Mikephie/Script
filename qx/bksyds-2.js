95dd1cd3bcbeaf757334947b81542e48bc9e943f
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-12-10 20:39:42 +0800

    👌

--- qx/bksyds-2.js
+++ qx/bksyds-2.js
@@ -7,26 +7,10 @@
 
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
-[rewrite_local] 
-^https?:\/\/photoby\.hasmash\.com\/ url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/bksyds.js
+[Script]
+DS1 = type=http-response, pattern=^https?:\/\/photoby\.hasmash\.com\/app\/browseEvent, script-path=https://raw.githubusercontent.com/Mikephie/Script/main/qx/ds1.js, requires-body=true, max-size=-1, timeout=60
+
+DS2 = type=http-response, pattern=^https?:\/\/photoby\.hasmash\.com\/auth\/member, script-path=https://raw.githubusercontent.com/Mikephie/Script/main/qx/ds2.js, requires-body=true, max-size=-1, timeout=60
 
 [MITM]
-hostname = photoby.hasmash.com
-
-*******************************/
-
-
-let mikephie = JSON.parse($response.body);
-
-if ($request.url.includes("/auth/member")) {
-    Object.assign(mikephie.result, {
-        memberExpire: 3742762088000,      // 更新或添加字段
-        });
-} else if ($request.url.includes("/clickEvent")) {
-    Object.assign(mikephie.result, {
-        isVip: 1,      // 更新或添加字段
-        vipTime: "2088-08-08 08:08:08"      // 更新或添加字段
-    });
-}
-
-$done({ body: JSON.stringify(mikephie) });
+hostname = %APPEND% photoby.hasmash.com
