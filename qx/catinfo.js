2747152b806046a7196c6c320ce6c16098e34e16
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-12-17 10:41:45 +0800

    👌

--- qx/catinfo.js
+++ qx/catinfo.js
@@ -1,3 +1,21 @@
+/*        
+        ➪：脚本名称: 彩豆视频水印宝 （永久会员）
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local]
+# >彩豆视频水印宝 – 视频加水印制作视频剪辑（永久会员）
+^https?:\/\/appss.baomingding.com\/app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/cdsp.js
+
+[mitm] 
+hostname = appss.baomingding.com
+
+*******************************/
+
 const url = "https://api.revenuecat.com/v1/product_entitlement_mapping";
 const headers = { ...$request.headers };
 console.log(headers['user-agent']);
