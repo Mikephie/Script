acd8b9078777f7ac086ad1e29a247ff251746de3
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-12-24 21:03:49 +0800

    👌

--- qx/itranscreen.js
+++ qx/itranscreen.js
@@ -22,3 +22,36 @@ hostname = *.tencentcs.com,settings.itranscreen.com,api.itranscreen.com:8080
 *
 */
 
+
+var body = $response.body;
+var url = $request.url;
+
+const USER_QUOTA_PATH = "/api/v1/user/quota?user_id";
+const SETTINGS_PATH = "/settings.itranscreen.com/settings.json";
+
+var jsonData;
+
+try {
+  jsonData = JSON.parse(body);
+
+  if (url.indexOf(USER_QUOTA_PATH) !== -1) {
+    jsonData.data.coins = 999880;
+    jsonData.data.gift = 999880;
+    jsonData.data.subscribed = 1;
+    jsonData.status_code = 0;
+    body = JSON.stringify(jsonData);
+  }
+
+  if (url.indexOf(SETTINGS_PATH) !== -1) {
+    jsonData.cost.token.zhipu = "免费99";
+    jsonData.cost.request.baidu_pic = "免费999";
+    jsonData.cost.request.youdao_pic = "免费999";
+    jsonData.cost.char.google = "免费999";
+    jsonData.cost.char.baidu = "免费999";
+    body = JSON.stringify(jsonData);
+  }
+} catch (error) {
+  console.log("JSON 解析错误: " + error.message);
+}
+
+$done({ body: body });
