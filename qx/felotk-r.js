3a8c4acbf0cbefd6afdf0dd782f9fbc92f15790f
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-12-24 18:09:08 +0800

    OK

--- qx/felotk-r.js
+++ qx/felotk-r.js
@@ -13,7 +13,7 @@
 [rewrite_local]
 # >felo实时翻译-同传与语音字幕
 ^https?:\/\/(translator|accounts).felo.me\/api\/user.*$ url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/felosubs.js
-^https?:\/\/translator.felo.me\/api\/translation\/thirdToken\?token_channel url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/felotk.js
+^https?:\/\/translator.felo.me\/api\/translation\/thirdToken\?token_channel url script-request-header https://raw.githubusercontent.com/Mikephie/Script/main/qx/felotk-r.js
 
 [mitm] 
 hostname = accounts.felo.me,translator.felo.me
