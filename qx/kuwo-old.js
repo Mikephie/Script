82cb512edefaa2ccce88ac3a6b2c2e7f6d4e70e2
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-12-24 18:55:09 +0800

    OK

--- qx/kuwo-old.js
+++ qx/kuwo-old.js
@@ -268,3 +268,211 @@ function Env(name, opts) {
         return (this.data && this.data[key]) || null;
       }
     }
+    initGotEnv(opts) {
+      this.got = this.got ? this.got : require('got');
+      this.cktough = this.cktough ? this.cktough : require('tough-cookie');
+      this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar();
+      if (opts) {
+        opts.headers = opts.headers ? opts.headers : {};
+        if (undefined === opts.headers.Cookie && undefined === opts.cookieJar) {
+          opts.cookieJar = this.ckjar;
+        }
+      }
+    }
+
+    get(opts, callback = () => {}) {
+      if (opts.headers) {
+        delete opts.headers['Content-Type'];
+        delete opts.headers['Content-Length'];
+      }
+      if (this.isSurge() || this.isLoon()) {
+        if (this.isSurge() && this.isNeedRewrite) {
+          opts.headers = opts.headers || {};
+          Object.assign(opts.headers, { 'X-Surge-Skip-Scripting': false });
+        }
+        $httpClient.get(opts, (err, resp, body) => {
+          if (!err && resp) {
+            resp.body = body;
+            resp.statusCode = resp.status;
+          }
+          callback(err, resp, body);
+        });
+      } else if (this.isQuanX()) {
+        if (this.isNeedRewrite) {
+          opts.opts = opts.opts || {};
+          Object.assign(opts.opts, { hints: false });
+        }
+        $task.fetch(opts).then(
+          (resp) => {
+            const { statusCode: status, statusCode, headers, body } = resp;
+            callback(null, { status, statusCode, headers, body }, body);
+          },
+          (err) => callback(err)
+        );
+      } else if (this.isNode()) {
+        this.initGotEnv(opts);
+        this.got(opts)
+          .on('redirect', (resp, nextOpts) => {
+            try {
+              if (resp.headers['set-cookie']) {
+                const ck = resp.headers['set-cookie'].map(this.cktough.Cookie.parse).toString();
+                if (ck) {
+                  this.ckjar.setCookieSync(ck, null);
+                }
+                nextOpts.cookieJar = this.ckjar;
+              }
+            } catch (e) {
+              this.logErr(e);
+            }
+          })
+          .then(
+            (resp) => {
+              const { statusCode: status, statusCode, headers, body } = resp;
+              callback(null, { status, statusCode, headers, body }, body);
+            },
+            (err) => {
+              const { message: error, response: resp } = err;
+              callback(error, resp, resp && resp.body);
+            }
+          );
+      }
+    }
+
+    post(opts, callback = () => {}) {
+      const method = opts.method ? opts.method.toLocaleLowerCase() : 'post';
+      if (opts.body && opts.headers && !opts.headers['Content-Type']) {
+        opts.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+      }
+      if (opts.headers) delete opts.headers['Content-Length'];
+      if (this.isSurge() || this.isLoon()) {
+        if (this.isSurge() && this.isNeedRewrite) {
+          opts.headers = opts.headers || {};
+          Object.assign(opts.headers, { 'X-Surge-Skip-Scripting': false });
+        }
+        $httpClient[method](opts, (err, resp, body) => {
+          if (!err && resp) {
+            resp.body = body;
+            resp.statusCode = resp.status;
+          }
+          callback(err, resp, body);
+        });
+      } else if (this.isQuanX()) {
+        opts.method = method;
+        if (this.isNeedRewrite) {
+          opts.opts = opts.opts || {};
+          Object.assign(opts.opts, { hints: false });
+        }
+        $task.fetch(opts).then(
+          (resp) => {
+            const { statusCode: status, statusCode, headers, body } = resp;
+            callback(null, { status, statusCode, headers, body }, body);
+          },
+          (err) => callback(err)
+        );
+      } else if (this.isNode()) {
+        this.initGotEnv(opts);
+        const { url, ..._opts } = opts;
+        this.got[method](url, _opts).then(
+          (resp) => {
+            const { statusCode: status, statusCode, headers, body } = resp;
+            callback(null, { status, statusCode, headers, body }, body);
+          },
+          (err) => {
+            const { message: error, response: resp } = err;
+            callback(error, resp, resp && resp.body);
+          }
+        );
+      }
+    }
+
+    time(fmt, ts = null) {
+      const date = ts ? new Date(ts) : new Date();
+      let o = {
+        'M+': date.getMonth() + 1,
+        'd+': date.getDate(),
+        'H+': date.getHours(),
+        'm+': date.getMinutes(),
+        's+': date.getSeconds(),
+        'q+': Math.floor((date.getMonth() + 3) / 3),
+        'S': date.getMilliseconds()
+      };
+      if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+      for (let k in o)
+        if (new RegExp('(' + k + ')').test(fmt))
+          fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+      return fmt;
+    }
+
+    msg(title = name, subt = '', desc = '', opts) {
+      const toEnvOpts = (rawopts) => {
+        if (!rawopts) return rawopts;
+        if (typeof rawopts === 'string') {
+          if (this.isLoon()) return rawopts;
+          else if (this.isQuanX()) return { 'open-url': rawopts };
+          else if (this.isSurge()) return { url: rawopts };
+          else return undefined;
+        } else if (typeof rawopts === 'object') {
+          if (this.isLoon()) {
+            let openUrl = rawopts.openUrl || rawopts.url || rawopts['open-url'];
+            let mediaUrl = rawopts.mediaUrl || rawopts['media-url'];
+            return { openUrl, mediaUrl };
+          } else if (this.isQuanX()) {
+            let openUrl = rawopts['open-url'] || rawopts.url || rawopts.openUrl;
+            let mediaUrl = rawopts['media-url'] || rawopts.mediaUrl;
+            return { 'open-url': openUrl, 'media-url': mediaUrl };
+          } else if (this.isSurge()) {
+            let openUrl = rawopts.url || rawopts.openUrl || rawopts['open-url'];
+            return { url: openUrl };
+          }
+        } else {
+          return undefined;
+        }
+      };
+      if (!this.isMute) {
+        if (this.isSurge() || this.isLoon()) {
+          $notification.post(title, subt, desc, toEnvOpts(opts));
+        } else if (this.isQuanX()) {
+          $notify(title, subt, desc, toEnvOpts(opts));
+        }
+      }
+      if (!this.isMuteLog) {
+        let logs = ['', '==============📣系统通知📣=============='];
+        logs.push(title);
+        subt ? logs.push(subt) : '';
+        desc ? logs.push(desc) : '';
+        console.log(logs.join('\n'));
+        this.logs = this.logs.concat(logs);
+      }
+    }
+
+    log(...logs) {
+      if (logs.length > 0) {
+        this.logs = [...this.logs, ...logs];
+      }
+      console.log(logs.join(this.logSeparator));
+    }
+
+    logErr(err, msg) {
+      const isPrintSack = !this.isSurge() && !this.isQuanX() && !this.isLoon();
+      if (!isPrintSack) {
+        this.log('', `❗️${this.name}, 错误!`, err);
+      } else {
+        this.log('', `❗️${this.name}, 错误!`, err.stack);
+      }
+    }
+
+    wait(time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
+    done(val = {}) {
+      const endTime = new Date().getTime();
+      const costTime = (endTime - this.startTime) / 1000;
+      this.log('', `🔔${this.name}, 结束! 🕛 ${costTime} 秒`);
+      this.log();
+      if (this.isSurge() || this.isQuanX() || this.isLoon()) {
+        $done(val);
+      }
+    }
+  })(name, opts);
+}
