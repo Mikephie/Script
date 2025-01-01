1c0285b58226d7d342aa04f2ac324d7d1391ddf2
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-01-01 19:51:52 +0800

    👌

--- qx/kuwomod.js
+++ qx/kuwomod.js
@@ -73,3 +73,53 @@ async function getNotifyContent() {
   }
 }
 
+function handleMobiS() {
+  const currentId = $.getval("kuwo_free_current_id") || "";
+  const newId = obj.data?.rid;
+  let body = $.toStr(obj);
+  
+  if (currentId !== newId) {
+    try {
+      const { body: newBody } = await $.http.get(`https://mobi.kuwo.cn/mobi.s?f=web&source=kwplayer_ar_1.1.9_jiakong_118980_320.apk&type=convert_url_with_sign&rid=${currentId}&priority=bitrate&network=3G&mode=download&br=2000kflac`);
+      body = newBody;
+    } catch (e) {
+      $.logErr("解锁失败~", e);
+    }
+  }
+  
+  $.setval("", "kuwo_free_current_id");
+  $.done({ body });
+}
+
+function handleMusicPay() {
+  const songId = obj.songs[0].id;
+  if (typeof songId === "number") {
+    $.setval(songId.toString(), "kuwo_free_current_id");
+  }
+  
+  const pid = obj.songs[0].audio[0].pid;
+  const price = obj.songs[0].audio[0].price;
+  const policy = obj.songs[0].audio[0].policy;
+  const policyName = `${policy}_1`;
+  
+  for (let i = 0; i < obj.songs[0].audio.length; i++) {
+    obj.songs[0].audio[i].st = 0;
+  }
+  
+  const songInfo = obj.songs[0];
+  obj.user = [{
+    pid, type: policy, name: policyName, categray: policyName,
+    id: songId, order: 375787919, final: [], buy: 1657425321,
+    begin: 1657425321, end: 4180305321, CurEnd: 0, playCnt: 0,
+    playUpper: 300, downCnt: 0, downUpper: 300, playVideoCnt: 0,
+    playVideoUpper: 3000, downVideoCnt: 0, downVideoUpper: 3000,
+    price, period: 1000, feetype: 0, info: songInfo
+  }];
+  
+  $.done({ body: $.toStr(obj) });
+}
+
+function handleVipEnc() {
+  $.done({ body: "Vo4m6X2hTph/vfpPmau8PTT0sFN6JCgzxSLVH/u3sbEt7VniYsVHbRFgOgN+Uvs39rAI7R3C5HVpaSj8tr8U8dLYwYdDCjMILuUorh3z0BiQToiWxudHkcASIPHNrmZHZYC/yv3DP4b89hbzfqU5UUDUqaZpEBZr76sDF2wNPmYjUEFSVCMGyTl1F6j1DBmKJ1Tik0YuG/2UBa/Ilz12a1KneXsNs5x5EE41bXDke7EygIB3I+6SoITZXOLFAFQFZujdI0GzClNglDKtclpUxpjN3uVeJxHLU40FTwNWo3ZDNv8KSdZpYZ5BDEOCyZkifmHlf1wnocX2zTr2xRAM6JhAD2WaSSN
+  
+  
