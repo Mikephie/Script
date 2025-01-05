d0e624278e75c36a8d4c64406544ccecac1d7ec2
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-01-05 22:37:41 +0800

    ok

--- qx/sj100-2.js
+++ qx/sj100-2.js
@@ -2,7 +2,7 @@
 试卷100解锁VIP
 2024.1.12
 
-  "extra": "{\"products\":[{\"code\":\"premium_lifetime_prime_398\",\"trial\":false,\"subscription\":true,\"upgraded\":false,\"introductory\":false}]}",
+
   
 [rewrite_local]
 ^https:\/\/paper\.zjapp\.xyz\/api\/v1\/status\/list url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/sj100.js
@@ -18,7 +18,7 @@ obj.data.user_status_infos = [{
   "uid": "522546324428709888",
   "status": 1,
   "expire_at": 4092595200000,
-  "extra": "{\"products\":[{\"code\":\"premium_lifetime_prime_398\",\"trial\":false,\"subscription\":true,\"upgraded\":false,\"introductory\":false}]}",
+  "extra" : "{\"products\":[{\"code\":\"paper100_svip_weekly_38\",\"trial\":false,\"subscription\":true,\"upgraded\":false,\"introductory\":false,
   "count": 1,
   "name": "svip"
 }];
