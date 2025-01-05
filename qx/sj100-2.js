6121a1fcd8bda0d4924a6f3817429b611c936b8e
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-01-05 22:35:31 +0800

    👌

--- qx/sj100-2.js
+++ qx/sj100-2.js
@@ -2,6 +2,8 @@
 试卷100解锁VIP
 2024.1.12
 
+  "extra": "{\"products\":[{\"code\":\"premium_lifetime_prime_398\",\"trial\":false,\"subscription\":true,\"upgraded\":false,\"introductory\":false}]}",
+  
 [rewrite_local]
 ^https:\/\/paper\.zjapp\.xyz\/api\/v1\/status\/list url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/sj100.js
 
@@ -13,12 +15,12 @@ hostname = paper.zjapp.xyz
 let obj = JSON.parse($response.body);
 obj.data.user_status_infos = [{
   "serial": "",
-  "uid": "406280149991055360",
+  "uid": "522546324428709888",
   "status": 1,
   "expire_at": 4092595200000,
   "extra": "{\"products\":[{\"code\":\"premium_lifetime_prime_398\",\"trial\":false,\"subscription\":true,\"upgraded\":false,\"introductory\":false}]}",
   "count": 1,
-  "name": "vip"
+  "name": "svip"
 }];
 $done({
   "body": JSON.stringify(obj)
