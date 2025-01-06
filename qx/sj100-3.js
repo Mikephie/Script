91a98f51e362e4c721c9a6d71be697a7815bc2ec
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-01-06 09:12:47 +0800

    👌

--- qx/sj100-3.js
+++ qx/sj100-3.js
@@ -54,4 +54,16 @@ function modifyObject(obj) {
 body = modifyObject(body);
 $done({ body: JSON.stringify(body) });
  
-$done({body: JSON.stringify(mikephie)});
+let obj = JSON.parse($response.body);
+obj.data.user_status_infos = [{
+  "serial": "",
+  "uid": "406280149991055360",
+  "status": 1,
+  "expire_at": 4092595200000,
+  "extra": "{\"products\":[{\"code\":\"premium_lifetime_prime_398\",\"trial\":false,\"subscription\":true,\"upgraded\":false,\"introductory\":false}]}",
+  "count": 1,
+  "name": "vip"
+}];
+$done({
+  "body": JSON.stringify(obj)
+});
