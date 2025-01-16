d4a5c2dab777ff950d1e45f40cdffdba2f154968
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-01-16 18:08:18 +0800

    👌

--- qx/RevenueProducts.js
+++ qx/RevenueProducts.js
@@ -1,28 +1,362 @@
 {
-  "UA": {
-    "Watchly": {
-      "products": [
-        { "entitlement": "lifetime", "id": "watchface.lifetime", "type": "life", "name": "Watchly-表面" }
-      ]
-    },
-    "Journal": {
-      "products": [
-        { "entitlement": "PRO", "id": "com.pureformstudio.diary.yearly", "type": "sub", "name": "Journal App" },
-        { "entitlement": "AI+", "id": "com.pureformstudio.diarly.ai_plus_yearly", "type": "sub", "name": "Journal App" }
-      ]
-    }
-  },
-  "BUNDLE": {
-    "com.valo.reader": {
-      "products": [
-        { "entitlement": "com.valo.reader.vip1.forever", "id": "com.valo.reader.vip1.forever", "type": "sub", "name": "读不舍手" },
-        { "entitlement": "com.valo.reader.vip2.year", "id": "com.valo.reader.vip2.year", "type": "sub", "name": "读不舍手" }
-      ]
-    },
-    "io.innerpeace.yiye": {
-      "products": [
-        { "entitlement": "Premium", "id": "io.innerpeace.yiye.lifetime.forYearly", "type": "life", "name": "言外笔记" }
-      ]
+      'Watchly': {
+        products: [
+          { entitlement: 'lifetime', id: 'watchface.lifetime', type: 'life', name: 'Watchly-表面' }
+        ]
+      },
+      'ShellBean': {
+        products: [
+          { entitlement: 'pro', id: 'com.ningle.shellbean.subscription.year.discount', type: 'sub', name: 'ShellBean- SHH服务器' }
+        ]
+      }, 
+      'umai': {
+        products: [
+          { entitlement: 'umai_pro', id: 'umai_pro_yearly', type: 'sub', name: 'UmAI' }
+        ]
+      },    
+      'Art%20Generator': {
+        products: [
+          { entitlement: 'discount_pro', id: 'artx_yearly_option_2', type: 'sub', name: 'Art Generator' },
+          { entitlement: 'DalleCredit', id: 'artx_100Credit', type: 'sub', name: 'Art Generator' }
+        ]
+      },
+      'ASKAI': {
+        products: [
+          { entitlement: 'pro', id: 'askai_pro', type: 'life', name: 'ASKAI' },
+          { entitlement: 'pro_plan', id: 'token_pro_plan', type: 'life', name: 'ASKAI' }
+        ]
+      },
+      'Darkroom': {
+        products: [
+          { entitlement: 'co.bergen.Darkroom.entitlement.allToolsAndFilters', id: 'co.bergen.Darkroom.product.forever.everything', type: 'sub', name: 'Darkroom' },
+          { entitlement: 'co.bergen.Darkroom.entitlement.selectiveAdjustments', id: 'co.bergen.Darkroom.product.forever.everything', type: 'sub', name: 'Darkroom' }
+        ]
+      },
+      'flighttracker': {
+        products: [
+          { entitlement: 'unlimited_access', id: 'free_lifetime_access_2024', type: 'life', name: 'flighttracker航班查询' }
+        ]
+      },
+      'js_code_pad': {
+        products: [
+          { entitlement: 'developer', id: 'js_code_pad_developer_upgrade', type: 'life', name: 'js_code代码编辑' }
+        ]
+      },
+      'Loopsie': {
+        products: [
+          { entitlement: 'premium', id: 'com.gamelounge.loopsie.ios.one_time', type: 'life', name: 'Loopsie照片编辑' }
+        ]
+      },
+      'photography': {
+        products: [
+          { entitlement: 'premium', id: 'photography_sub_yearly_1', type: 'sub', name: 'PhotoX' }
+        ]
+      },
+      'chatbotapp': {
+        products: [
+          { entitlement: 'One Time Full Access', id: 'PAI_lifeTimeAccess_01', type: 'life', name: 'chatbotapp' }
+        ]
+      },
+      'XS%20VPN': {
+        products: [
+          { entitlement: 'goru_pro', id: 'xs_ios_yearly_sub_1', type: 'sub', name: 'XS VPN' }
+        ]
+      },
+      'Goru': {
+        products: [
+          { entitlement: 'goru_pro', id: 'com.ahmetserdarkaradeniz.goruyearlyalternative', type: 'sub', name: 'Goru' }
+        ]
+      },
+      'Remote': {
+        products: [
+          { entitlement: 'Pros', id: 'com.cherpake.macrc.mor', type: 'life', name: 'Remote' }
+        ]
+      },
+      'ChatPub': {
+        products: [
+          { entitlement: 'Unlimited Access', id: 'conversationai.year', type: 'sub', name: 'ChatPub' }
+        ]
+      },
+      'ChatGPT': {
+        products: [
+          { entitlement: 'premium', id: 'com.prod.gpt.1y40', type: 'sub', name: 'ChatGPT' }
+        ]
+      },
+      'Video%20Up!': {
+        products: [
+          { entitlement: 'Full_access_app', id: 'app.videoup.videoup.na.base.one_year', type: 'sub', name: 'Video Up!' }
+        ]
+      },
+      'Jellycuts': {
+        products: [
+          { entitlement: 'pro', id: 'com.zlineman.Jellyfish.tier.5.pro', type: 'sub', name: 'Jellycuts' }
+        ]
+      },
+      'Dream': {
+        products: [
+          { entitlement: 'premium', id: 'artx_yearly_option_2', type: 'sub', name: 'Dream' }
+        ]
+      },
+      'World%20Clock': {
+        products: [
+          { entitlement: 'pro', id: 'dream_yearly_jan_2024_3999', type: 'sub', name: 'World Clock' }
+        ]
+      },
+      'MorningAlarm': {
+        products: [
+          { entitlement: 'morning_premium', id: 'alarmify.yearly.1', type: 'sub', name: 'MorningAlarm' }
+        ]
+      },
+      'Genius%20AI': {
+        products: [
+          { entitlement: 'premium', id: 'genius_lifetime_59', type: 'life', name: 'Genius AI' }
+        ]
+      },
+      'Vocai-iOS': {
+        products: [
+          { entitlement: 'AI Pro', id: 'vocabAI_900_1m', type: 'sub', name: 'Vocai-iOS' }
+        ]
+      },
+      '1Blocker': {
+        products: [
+          { entitlement: 'premium', id: 'blocker.ios.iap.lifetime', type: 'life', name: '1Blocker' }
+        ]
+      },
+      'VidCap': {
+        products: [
+          { entitlement: 'io.fadel.vidcap.pro', id: 'subs.vcp_59.99_365_3', type: 'sub', name: 'VidCap' }
+        ]
+      },
+      'Color%20Widgets': {
+        products: [
+          { entitlement: 'pro', id: 'cw_1999_1y_3d0', type: 'sub', name: 'Color Widgets' }
+        ]
+      },
+      'ChatBot': {
+        products: [
+          { entitlement: 'chatbot_annual', id: 'chatbot_annual', type: 'sub', name: 'ChatBot' }
+        ]
+      },
+      'Infltr': {
+        products: [
+          { entitlement: 'com.Yooshr.infltr.subscriptionPremium', id: 'com.Yooshr.infltr.everythingForever', type: 'sub', name: 'Infltr' }
+        ]
+      },
+      'Currency': {
+        products: [
+          { entitlement: 'plus', id: 'com.jeffreygrossman.currencyapp.iap.plus', type: 'sub', name: 'Currency-汇率查询' }
+        ]
+      },
+      'Spark': {
+        products: [
+          { entitlement: 'premium', id: 'spark_5999_1y_1w0', type: 'sub', name: 'Spark_Mail-邮箱管理' }
+        ]
+      },
+      'ChatLLM': {
+        products: [
+          { entitlement: 'Pro', id: 'com.curiouscreatorsco.ChatLLM.pro.lifetime.notrial.150_00', type: 'life', name: 'AItText' }
+        ]
+      },
+      'Photomator': {
+        products: [
+          { entitlement: 'pixelmator_photo_pro_access', id: 'pixelmator_photo_lifetime_v1', type: 'life', name: 'Photomator' }
+        ]
+      },
+      'CountDuck': {
+        products: [
+          { entitlement: 'premium', id: 'Lifetime', type: 'life', name: '倒数鸭' }
+        ]
+      },
+      'ScreenRecordCase': {
+        products: [
+          { entitlement: 'Premium', id: 'me.fandong.ScreenRecordCase.Ultra', type: 'life', name: '屏幕套壳' }
+        ]
+      },
+      'ShellBoxKit': {
+        products: [
+          { entitlement: 'ssh_pro', id: 'ShellBoxKit.Year', type: 'sub', name: 'CareServer-服务器监控' }
+        ]
+      },
+      'IDM': {
+        products: [
+          { entitlement: 'premium', id: 'sub_yearly_idm', type: 'sub', name: 'IDM-下载' }
+        ]
+      },
+      'Whisper': {
+        products: [
+          { entitlement: 'all_features', id: 'whisperai_80_y', type: 'sub', name: 'Whisper' }
+        ]
+      },
+      'PhotoRoom': {
+        products: [
+          { entitlement: 'business', id: 'com.background.business.yearly', type: 'sub', name: 'PhotoRoom' }
+        ]
+      },
+      'TouchRetouchBasic': {
+        products: [
+          { entitlement: 'premium', id: 'tr5_yearlysubsc_30_and_20_dlrs', type: 'sub', name: 'TouchRetouch-水印清理' }
+        ]
+      },
+      'Drops': {
+        products: [
+          { entitlement: 'premium', id: 'forever_unlimited_time_discounted_80_int', type: 'life', name: 'Drops外语' }
+        ]
+      },
+      'UTC': {
+        products: [
+          { entitlement: 'Entitlement.Pro', id: 'tech.miidii.MDClock.subscription.month', type: 'sub', name: '花样文字' }
+        ]
+      },
+      'EraseIt': {
+        products: [
+          { entitlement: 'ProVersionLifeTime', id: 'com.uzero.cn.eraseit.premium1.fromyear', type: 'sub', name: 'EraseIt' }
+        ]
+      },
+      'DHWaterMarkManager': {
+        products: [
+          { entitlement: 'Vip', id: 'lifetimeVIP_001', type: 'life', name: 'DHWaterMarkManager' }
+        ]
+      },
+      '%E8%B5%84%E6%BA%90%E6%90%AC%E8%BF%90%E5%A4%A7%E5%B8%88': {
+        products: [
+          { entitlement: 'SaveTikYoutu_common', id: 'LifetimeSubscription', type: 'life', name: 'SaveTikYoutu' }
+        ]
+      },
+      'Yosum': {
+        products: [
+          { entitlement: 'Premium', id: 'yosum_999_1year', type: 'sub', name: 'Yosum订阅' }
+        ]
+      },
+      'iplayTV': {
+        products: [
+          { entitlement: 'com.ll.btplayer.12', id: 'com.ll.btplayer.12', type: 'life', name: 'ntPlayer' }
+        ]
+      },
+      'TQBrowser': {
+        products: [
+          { entitlement: 'pro_lt', id: 'com.tk.client.lifetime', type: 'life', name: 'Teak浏览器' }
+        ]
+      },
+      'Python3IDE': {
+        products: [
+          { entitlement: 'pro', id: 'python3ide_six_month', type: 'sub', name: 'Python3IDE' }
+        ]
+      },
+      'CallAnnie': {
+        products: [
+          { entitlement: 'ai.animato.callannie.entitlement.pro0', id: 'ai.animato.callannie.proyearly1', type: 'sub', name: 'CallAnnie' }
+        ]
+      },
+      'VideoToLive': {
+        products: [
+          { entitlement: 'Premium', id: 'video_to_live_premium', type: 'life', name: 'VideoToLive' }
+        ]
+      },
+      'Themy': {
+        products: [
+          { entitlement: 'fonts_premium', id: 'lifetime', type: 'life', name: 'Fonts-微信字体' }
+        ]
+      },
+      'BabyCare': {
+        products: [
+          { entitlement: 'pro', id: 'KiddoKeeper_38_LifeTime', type: 'life', name: '小守护' }
+        ]
+      },
+      'MenuBox': {
+        products: [
+          { entitlement: 'premium', id: 'com.menubox.premium', type: 'sub', name: 'MenuBox' }
+        ]
+      },
+      'Chatme': {
+        products: [
+          { entitlement: 'premium', id: 'chatme_premium_year_discount', type: 'sub', name: 'ChatMe' }
+        ]
+      },
+      'AI%C2%A0Chat': {
+        products: [
+          { entitlement: 'AI Plus', id: 'ai_plus_gpt_yearly', type: 'sub', name: 'AIChat' }
+        ]
+      },
+      'Days': {
+        products: [
+          { entitlement: 'premium', id: 'net.mattdavenport.daysuntil.dayspremiumlifetime', type: 'life', name: 'Days' }
+        ]
+      },
+      'Journal': {
+        products: [
+          { entitlement: 'PRO', id: 'com.pureformstudio.diary.yearly', type: 'sub', name: 'Journal App' },
+          { entitlement: 'AI+', id: 'com.pureformstudio.diarly.ai_plus_yearly', type: 'sub', name: 'Journal App' }
+        ]
+      }
+
+    },  // UA,BUNDLE分界线，保留此行。
+
+    BUNDLE: {
+       'com.valo.reader': {
+        products: [
+          { entitlement: 'com.valo.reader.vip1.forever', id: 'com.valo.reader.vip1.forever', type: 'sub', name: '读不舍手' },
+          { entitlement: 'com.valo.reader.vip2.year', id: 'com.valo.reader.vip2.year', type: 'sub', name: '读不舍手' }
+        ]
+      },
+      'com.runbuddy.prod': {
+        products: [
+          { entitlement: 'premium', id: 'rb_9999_1y_1y7999', type: 'sub', name: 'Runna-马拉松训练' }
+        ]
+      },
+      'com.reku.Counter': {
+        products: [
+          { entitlement: 'plus', id: 'com.reku.counter.plus.lifetime', type: 'life', name: 'Counter-计步器' }
+        ]
+      },
+      'TeleprompterX': {
+        products: [
+          { entitlement: 'Pro Upgrade', id: 'TPXOTP', type: 'life', name: 'Teleprompter' }
+        ]
+      },
+      'moonbox.co.il.grow': {
+        products: [
+          { entitlement: 'pro', id: 'moonbox.co.il.grow.lifetime.offer', type: 'life', name: '植物识别-PlantID' }
+        ]
+      },
+      'tech.miidii.MDClock': {
+        products: [
+          { entitlement: 'Entitlement.Pro', id: 'tech.miidii.MDClock.pro', type: 'life', name: '谜底时钟' }
+        ]
+      },
+      'com.voicedream.Voic': {
+        products: [
+          { entitlement: 'standard', id: 'vd_annual_79_3daytrial', type: 'sub', name: '声之梦' }
+        ]
+      },
+      'com.laser-focused.focus-ios': {
+        products: [
+          { entitlement: 'subscribed', id: 'iap.io.masterbuilders.focus.pro_one_year', type: 'sub', name: 'Focus-专注时间管理' }
+        ]
+      },
+      'com.roehl': {
+        products: [
+          { entitlement: 'Pro', id: 'habitkit_3499_lt', type: 'life', name: 'HabitKit/WinDiary-双件套' }
+        ]
+      },
+      'net.tengl.powertimer': {
+        products: [
+          { entitlement: 'plus', id: 'powertimer.plus', type: 'life', name: '元气计时-PowerTimer' }
+        ]
+      },
+      'com.reader.book': {
+        products: [
+          { entitlement: 'pro', id: 'reader.lifetimeFamily.pro', type: 'life', name: 'PureLibro' }
+        ]
+      },
+      'app.imone.OneWidget': {
+        products: [
+          { entitlement: 'pro', id: 'app.imone.OneWidget.Lifetime', type: 'life', name: 'OneWidget-小组件' }
+        ]
+      },
+      'io.innerpeace.yiye': {
+        products: [
+          { entitlement: 'Premium', id: 'io.innerpeace.yiye.lifetime.forYearly', type: 'life', name: '言外笔记' }
+        ]
+      }
     }
   }
-}
