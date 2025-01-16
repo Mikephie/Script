7bea00df711a88d3ca762ab6f39793e87fa7ea8c
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-01-16 18:06:14 +0800

    👌

---
+++ qx/RevenueProducts.js
@@ -0,0 +1,28 @@
+{
+  "UA": {
+    "Watchly": {
+      "products": [
+        { "entitlement": "lifetime", "id": "watchface.lifetime", "type": "life", "name": "Watchly-表面" }
+      ]
+    },
+    "Journal": {
+      "products": [
+        { "entitlement": "PRO", "id": "com.pureformstudio.diary.yearly", "type": "sub", "name": "Journal App" },
+        { "entitlement": "AI+", "id": "com.pureformstudio.diarly.ai_plus_yearly", "type": "sub", "name": "Journal App" }
+      ]
+    }
+  },
+  "BUNDLE": {
+    "com.valo.reader": {
+      "products": [
+        { "entitlement": "com.valo.reader.vip1.forever", "id": "com.valo.reader.vip1.forever", "type": "sub", "name": "读不舍手" },
+        { "entitlement": "com.valo.reader.vip2.year", "id": "com.valo.reader.vip2.year", "type": "sub", "name": "读不舍手" }
+      ]
+    },
+    "io.innerpeace.yiye": {
+      "products": [
+        { "entitlement": "Premium", "id": "io.innerpeace.yiye.lifetime.forYearly", "type": "life", "name": "言外笔记" }
+      ]
+    }
+  }
+}
