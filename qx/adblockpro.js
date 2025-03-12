17496e6a5adb0ebb08093246a4795193edad25fb
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-03-12 20:42:57 +0800

    o

--- qx/adblockpro.js
+++ qx/adblockpro.js
@@ -1,12 +1,12 @@
 /*
-📜 ✨ AdblockPro ✨
+📜 ✨ AdblockPRO ✨
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
 [rewrite_local] // Quantumult X
 ^https:\/\/api\.adblockpro\.app\/verify url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/adblockpro.js
 
 [Script] // Surge
-api_vip = type=http-response, pattern=^https:\/\/api\.adblockpro\.app\/verify, requires-body=true, max-size=0, script-path=https://raw.githubusercontent.com/Mikephie/Script/main/qx/adblockpro.js, timeout=60
+AdblockPRO = type=http-response, pattern=^https:\/\/api\.adblockpro\.app\/verify, requires-body=true, max-size=0, script-path=https://raw.githubusercontent.com/Mikephie/Script/main/qx/adblockpro.js, timeout=60
 
 [Script] // Loon
 http-response ^https:\/\/api\.adblockpro\.app\/verify script-path=https://raw.githubusercontent.com/Mikephie/Script/main/qx/adblockpro.js, requires-body=true, timeout=60
@@ -14,17 +14,15 @@ http-response ^https:\/\/api\.adblockpro\.app\/verify script-path=https://raw.gi
 [MITM]
 hostname = api.adblockpro.app
 
-*/
-
 /********** 会话通知模块 **********/
 function sNotify(a,b,c,d=60000){const e=`${a.replace(/[^a-zA-Z]/g,'').toLowerCase()}_session`;const f=typeof $prefs!=='undefined';const g=typeof $persistentStore!=='undefined'&&typeof $notify!=='undefined';const h=typeof $persistentStore!=='undefined'&&typeof $notification!=='undefined';const i=f?$prefs:$persistentStore;const j=f?$notification:(g?$notify:$notification);if(!i||!j)return false;try{const k=f?i.valueForKey(e):i.read(e);const l=Date.now();if(!k||(l-parseInt(k)>d)){j.post(a,b,c);f?i.setValueForKey(l.toString(),e):i.write(l.toString(),e);return true;}}catch(m){console.log(`[${a}] 错误: ${m}`);}return false;}
 
 /********** 应用配置信息 **********/
-const appName = "✨AdblockPro✨";
+const appName = "✨AdblockPRO✨";
 const author = "🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ";
-const message = "会员解锁至 0️⃣8️⃣0️⃣8️⃣2️⃣0️⃣8️⃣8️⃣";
+const message = "会员解锁至 ⓿❽-⓿❽-❷⓿❽❽";
 
-// 主脚本函数
+// 主脚本函数...
 let body = JSON.parse($response.body);
 function modifyObject(obj) {
     for (let key in obj) {
@@ -43,12 +41,11 @@ modifyObject(body);
 if (body.hasOwnProperty('v')) {
     body.v = true;  // Keeping 'v' as true since it's already true in the original
 }
-// 主脚本函数
+// 主脚本函数...
 
 sNotify(appName, author, message, 10 * 60 * 1000);
-// 根据条件选择返回方式
 if (typeof body === 'object') {
-    $done({ body: JSON.stringify(body) }); // JSON格式返回
+    $done({ body: JSON.stringify(body) });
 } else {
-    $done({ body }); // 字符串直接返回
+    $done({ body });
 }
