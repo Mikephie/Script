579e7e54db79e11bc83ac1c872885be347137a44
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-03-17 11:00:45 +0800

    o

--- qx/igv.js
+++ qx/igv.js
@@ -3,8 +3,10 @@
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
 [rewrite_local] // Quantumult X
+
 # 重写规则 - 拦截广告图片
-^https:\/\/media\.gv\.com\.sg\/cms\/images\/ads\/.*\.(jpg|jpeg|png|gif)$ url reject-img
+^https:\/\/media\.gv\.com\.sg\/cms\/images\/ads\/(CorpSales_NowAllowed_600x1200|OnlineF&BDeal)\.jpg url reject-img
+
 # 脚本 - 自动点击跳过广告按钮
 ^https:\/\/m\.gv\.com\.sg\/iGV2\/general\/advpage\.html url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/igv.js
 
