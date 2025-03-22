b3bce04e13a29c5bdbb442a83a7d64d2874d0195
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-03-22 08:29:54 +0800

    o

--- qx/embycrack.js
+++ qx/embycrack.js
@@ -1,81 +1,73 @@
-/*
-📜 ✨ EmbyUnlock ✨
+/*        
+        ➪：脚本名称: Embycrack （永久会员）
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： 🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
-[rewrite_local] // Quantumult X
-^https:\/\/mb3admin\.com\/admin\/service(\/registration\/validateDevice|\/appstore\/register|\/registration\/validate|\/registration\/getStatus|\/supporter\/retrievekey) url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/embycrack.js
+[rewrite_local]
 
-[Script] // Surge
-EmbyCrack = type=http-response, pattern=^https:\/\/mb3admin\.com\/admin\/service(\/registration\/validateDevice|\/appstore\/register|\/registration\/validate|\/registration\/getStatus|\/supporter\/retrievekey), requires-body=true, max-size=0, script-path=https://raw.githubusercontent.com/Mikephie/Script/main/qx/embycrack.js, timeout=60
+^https:\/\/mb3admin\.com\/admin\/service(\/registration\/validateDevice|\/appstore\/register|\/registration\/validate|\/registration\/getStatus|\/supporter\/retrievekey) url script-echo-response https://raw.githubusercontent.com/Mikephie/Script/main/qx/embycrack.js
 
-[Script] // Loon
-http-response ^https:\/\/mb3admin\.com\/admin\/service(\/registration\/validateDevice|\/appstore\/register|\/registration\/validate|\/registration\/getStatus|\/supporter\/retrievekey) script-path=https://raw.githubusercontent.com/Mikephie/Script/main/qx/embycrack.js, requires-body=true, timeout=60
+[mitm]
 
-[MITM]
-hostname = mb3admin.com
+hostname= mb3admin.com
+ 
+**************************************/
 
-*/
-
-// 主脚本函数...
-var obj = {};
-const url = $request.url;
-
-if (!url) { $done({}); }
+var url = $request.url;
 
 const myStatus = "HTTP/1.1 200 OK";
 const myHeaders = {
-    "Crack": "KS",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Allow-Method": "*",
+    "Crack": "KS", 
+    "Access-Control-Allow-Origin": "*", 
+    "Access-Control-Allow-Headers": "*", 
+    "Access-Control-Allow-Method": "*", 
     "Access-Control-Allow-Credentials": "true"
 };
 
+
 if (url.indexOf('/admin/service/registration/validateDevice') != -1) {
-    obj = {
-        "cacheExpirationDays": 365,
-        "message": "Device Valid",
-        "resultCode": "GOOD"
-    };
+  obj = {
+      "cacheExpirationDays": 365,
+      "message": "Device Valid",
+      "resultCode": "GOOD"
+	};
 } else if (url.indexOf('/admin/service/appstore/register') != -1) {
-    obj = {
-        "featId": "",
-        "registered": true,
-        "expDate": "2088-08-08",
-        "key": ""
-    };
+  obj = {
+      "featId":"",
+      "registered":true,
+      "expDate":"2099-09-09",
+      "key":""
+	};
 } else if (url.indexOf('/admin/service/registration/validate') != -1) {
-    obj = {
-        "featId": "",
-        "registered": true,
-        "expDate": "2088-08-08",
-        "key": ""
-    };
-} else if (url.indexOf('/admin/service/registration/getStatus') != -1) {
-    obj = {
-        "planType": "Cracked",
-        "deviceStatus": "",
-        "subscriptions": []
-    };
-} else if (url.indexOf('/admin/service/supporter/retrievekey') != -1) {
-    obj = {
-        "Success": false,
-        "ErrorMessage": "Supporter not found"
-    };
+	obj = {
+      "featId":"",
+      "registered":true,
+      "expDate":"2099-09-09",
+      "key":""
+	};
+} else if (url.indexOf('/admin/service/registration/getStatus') != -1){
+	obj = {
+      "planType":"Cracked",
+      "deviceStatus":"",
+      "subscriptions":[]
+	};
+} else if (url.indexOf('/admin/service/supporter/retrievekey') != -1){
+	obj = {
+      "Success":false,
+      "ErrorMessage":"Supporter not found"
+	};
 }
 
-var myData = JSON.stringify(obj);
-// 主脚本函数...
+myData = JSON.stringify(obj);
 
-/********** 应用配置信息 **********/
-const cooldownMs = 10 * 60 * 1000;
-const notifyKey = "EmbyUnlock_notify_key";
-const now = Date.now();
-let lastNotifyTime = $persistentStore.read(notifyKey) ? parseInt($persistentStore.read(notifyKey)) : 0;
+const myResponse = {
+    status: myStatus,
+    headers: myHeaders, // Optional.
+    body: myData // Optional.
+};
 
-if (now - lastNotifyTime > cooldownMs) {
-    $notification.post("✨EmbyUnlock✨", "🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ", "永久解锁或 ⓿❾-⓿❾-❷⓿❾❾");
-    $persistentStore.write(now.toString(), notifyKey);
-}
-
-$done({ status: myStatus, headers: myHeaders, body: myData });
+$done(myResponse);
