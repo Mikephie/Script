c3bf16f201c9c1567b5f2b2ae2801169fd274d6b
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-04-10 11:03:06 +0800

    o

--- qx/ime.js
+++ qx/ime.js
@@ -7,15 +7,18 @@
 >「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
 >「 额外说明 」         请勿传播或售卖此脚本
 
-**************************************
+**************************************/
 
-[rewrite_local]
-https:\/\/api\.imem\.app\/api\/premium\/getOwnStatus url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/iMe.js
-
-[mitm]
-hostname = api.imem.app
-
-*************************************/
+// -------- 通知（带冷却）逻辑开始 --------
+const cooldownMs = 10 * 60 * 1000;
+const notifyKey = "Filmix PRO+_notify_key";
+const now = Date.now();
+let lastNotifyTime = $persistentStore.read(notifyKey) ? parseInt($persistentStore.read(notifyKey)) : 0;
+if (now - lastNotifyTime > cooldownMs) {
+    $notification.post("✨Filmix PRO+✨", "🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ", "永久解锁或 ⓿❽-⓿❽-❷⓿❽❽");
+    $persistentStore.write(now.toString(), notifyKey);
+}
+// -------- 通知（带冷却）逻辑结束 --------
 
 var Mike = JSON.parse($response.body);
 Mike.payload.active = true;
