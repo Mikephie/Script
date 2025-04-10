d76ee34cf5ee2c32ecb578f7173a9b352216105a
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-04-10 12:13:39 +0800

    o

--- qx/ime.js
+++ qx/ime.js
@@ -1,13 +1,17 @@
-/*************************************
+/*
+📜 ✨ iMe ✨
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+#!desc=iMe
+#!category=🔐APP
+#!author=🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+#!icon=https://raw.githubusercontent.com/Mikephie/icons/main/icon/ime.png
 
->「 脚本名称 」         iMe解锁Premium
->「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
->「 更新时间 」         2024-09-21
->「 注意事项 」         如需引用请注明出处，谢谢合作！
->「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
->「 额外说明 」         请勿传播或售卖此脚本
+[rewrite_local]
+https:\/\/api\.imem\.app\/api\/premium\/getOwnStatus url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/ime.js
 
-**************************************/
+[mitm]
+hostname = api.imem.app
+*/
 
 // -------- 通知（带冷却）逻辑开始 --------
 const cooldownMs = 10 * 60 * 1000;
@@ -20,8 +24,8 @@ if (now - lastNotifyTime > cooldownMs) {
 }
 // -------- 通知（带冷却）逻辑结束 --------
 
-var Mike = JSON.parse($response.body);
-Mike.payload.active = true;
+var obj = JSON.parse($response.body);
+obj.payload.active = true;
 $done({
-  "body": JSON.stringify(Mike)
+  "body": JSON.stringify(obj)
 });
