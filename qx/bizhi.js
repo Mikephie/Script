39ff2863628dbd73af3b5236b3b1e211bd64c616
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-05-22 16:08:47 +0800

    o

--- qx/bizhi.js
+++ qx/bizhi.js
@@ -36,7 +36,33 @@ if (user.test(url)) {
     body.results = [{
         "nickName": "MIKEPHIE",
         "headImageUrl": "https://i.ibb.co/wM5z10N/IMG-1287.jpg",
-        "coin": 88888888
+        "svipType" : "year_pro",
+        "coin": 88888888,
+        "isVIP": 1,
+        "isSVIP": 1,
+        "vipType": 2,
+        "svipType": "season_pro",
+        "vipEndTime": 3742762088000,
+        "svipEndTime": 3742762088000,
+        "vipEndTimeFormat": "2088-08-08T08:08:08Z",
+        "svipEndTimeFormat": "2088-08-08T08:08:08Z",
+        "vipEndTimeByAdmin": 0,
+        "svipEndTimeByAdmin": 0,
+        "isVIPByAdmin": 0,
+        "isSVIPByAdmin": 0,
+        "sex": "1",
+        "appVersion": "4.8.7",
+        "systemVersion": "18.5",
+        "createdAt": "2023-11-19T09:29:18.937Z",
+        "updatedAt": "2025-05-22T07:35:33.274Z",
+        "loginType": 1,
+        "downloadCount": 24,
+        "favCount": 0,
+        "warmStartCount": 90,
+        "coldStartCount": 259,
+        "cost": 680.79999999999995,
+        "objectId": "6559d56eb87b3b5ada6602a7",
+        "userId": "000213.21970f036fd0454a99d29d8cebe086cd.0929"
     }];
 }
 
