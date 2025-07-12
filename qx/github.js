4a29ebc1ad9f9be84cf8d8ca5f36d3780fa86943
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-07-12 21:43:30 +0800

    o

---
+++ qx/github.js
@@ -0,0 +1,40 @@
+/*
+#!name= ✨ GitHub ✨
+#!desc=脚本仓库
+#!category=🔐APP
+#!author=🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ
+#!icon=https://raw.githubusercontent.com/Mikephie/icons/main/icon/filmix.png
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+[rewrite_local]
+https://api.github.com/graphql url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/github.js
+
+[MITM]
+hostname = api.github.com
+
+*/
+
+// -------- 通知（带冷却）逻辑开始 --------
+const cooldownMs = 10 * 60 * 1000;
+const notifyKey = "GitHub_notify_key";
+const now = Date.now();
+let lastNotifyTime = $persistentStore.read(notifyKey) ? parseInt($persistentStore.read(notifyKey)) : 0;
+if (now - lastNotifyTime > cooldownMs) {
+    $notification.post("✨GitHub✨", "🅜ⓘ🅚ⓔ🅟ⓗ🅘ⓔ", "永久解锁或 ⓿❽-⓿❽-❷⓿❽❽");
+    $persistentStore.write(now.toString(), notifyKey);
+}
+// -------- 通知（带冷却）逻辑结束 --------
+
+// 主脚本函数...
+let obj = JSON.parse($response.body);
+if (obj.data && obj.data.viewer) {
+  obj.data.viewer.isProPlan = true;
+  obj.data.viewer.isEmployee = true;
+  obj.data.viewer.hasAppleIapSubscription = true;
+
+  $done({
+    body: JSON.stringify(obj)
+  });
+} else {
+  $done({});
+}
+// 主脚本函数...
