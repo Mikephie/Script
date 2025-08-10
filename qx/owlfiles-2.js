451da298e2bdaf51580cf11adba6821dbc7f2ef0
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-08-10 18:46:59 +0800

    o

--- qx/owlfiles-2.js
+++ qx/owlfiles-2.js
@@ -6,7 +6,7 @@
 #!icon=https://raw.githubusercontent.com/Mikephie/icons/main/icon/owlfiles.png
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 [rewrite_local]
-^https?:\/\/www\.skyjos\.com(?::58080)?\/ws\/(?:validate|loadaccountinfo)\b url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/owlfiles.js
+^https?:\/\/www\.skyjos\.com(?::58080)?\/ws\/(?:validate|loadaccountinfo)\b url script-response-body https://raw.githubusercontent.com/Mikephie/Script/main/qx/owlfiles-2.js
 
 [mitm]
 hostname = www.skyjos.com
