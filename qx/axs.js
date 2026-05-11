9995db65b3900583114e1416d7e9f77702a514e5
Mikephie <131840160+Mikephie@users.noreply.github.com>
2026-05-11 20:52:39 +0800

    Update axs.js

--- qx/axs.js
+++ qx/axs.js
@@ -7,30 +7,25 @@
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
 [rewrite_local]
-# AXS 上游首页广告 banner，必须先清
-^https:\/\/prod-smarket\.hithink\.sg\/hexinifs\/rs\/cms\/ad\/overseaHomePage.* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/refs/heads/main/qx/axs_hithink_ad_clean.js
+# AXS e-station 动态弹窗系统，直接禁用 PopupLoader
+^https:\/\/e-station\.axs\.com\.sg\/html\/wSDK\/js\/popup_(?:config|manager|loader)\.js.* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/refs/heads/main/qx/axs_popup_noop.js
 
-# AXS InfoPage 漏网弹窗：HTML 注入静默关闭
-^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/InfoPage\/[^\/]+\/whatsnew\.php.* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/refs/heads/main/qx/axs_whatsnew_silent.js
+# AXS e-station 页面兜底：移除 PopupLoader.init + 注入隐藏规则
+^https:\/\/e-station\.axs\.com\.sg\/wSDK\/.*\.(?:php|html)(?:\?.*)?$ url script-response-body https://raw.githubusercontent.com/Mikephie/Script/refs/heads/main/qx/axs_estation_clean.js
 
-# AXS InfoPage CSS 兜底：加载前先隐藏，防闪
-^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/InfoPage\/general_display\.css.* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/refs/heads/main/qx/axs_infopage_hide_css.js
+# AXS 页面底部 marketing banner
+^https:\/\/e-station\.axs\.com\.sg\/wSDK\/image\/marketing_banner\/.* url reject-img
 
-# AXS 弹窗图，兜底透明化
-^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/InfoPage\/[^\/]+\/images\/.* url reject-img
+# Moovit 紫色 Moovit+ 订阅条
+^https:\/\/app4\.moovitapp\.com\/services-app\/services\/Subscriptions\/GetSubscriptions.* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/refs/heads/main/qx/moovit_empty_octet_list.js
 
-# AXS 首页广告背景图，按 HAR 命中
-^https:\/\/m-station\.axs\.com\.sg\/AXSMobile\/CT_include\/CT153_src\/banner_image\/.* url reject-img
-
-# Moovit 首页提示/横幅卡片，HAR 里有 Tip! Use Live Directions
+# Moovit 其他广告/推送卡片
 ^https:\/\/app5\.moovitapp\.com\/services-app\/services\/V4\/UserNotifications\/AllValidNotifications.* url script-response-body https://raw.githubusercontent.com/Mikephie/Script/refs/heads/main/qx/moovit_empty_octet_list.js
-
-# Moovit 广告 SDK
 ^https:\/\/app5\.moovitapp\.com\/services-app\/services\/ads\/mediator\/getProximityTagsInfo.* url reject
 ^https:\/\/(?:config|logs)\.ads\.vungle\.com\/.* url reject
 ^https:\/\/cdn2\.inner-active\.mobi\/.* url reject
 ^https:\/\/skadnetworks\.fyber\.com\/.* url reject
 
 [mitm]
-hostname = prod-smarket.hithink.sg, m-station.axs.com.sg, app5.moovitapp.com, config.ads.vungle.com, logs.ads.vungle.com, cdn2.inner-active.mobi, skadnetworks.fyber.com
-
+hostname = e-station.axs.com.sg, app4.moovitapp.com, app5.moovitapp.com, config.ads.vungle.com, logs.ads.vungle.com, cdn2.inner-active.mobi, skadnetworks.fyber.com
+axs_popup_noop.js
